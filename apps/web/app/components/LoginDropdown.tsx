71ab59f0990707e523c93585df6c25e0d2c0db54
Ernesto Joel Hdz <erhernandez@altimetrik.com>
2023-06-24 09:40:55 -0600

    Update LoginDropdown.tsx

--- apps/web/app/components/LoginDropdown.tsx
+++ apps/web/app/components/LoginDropdown.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+/* eslint-disable @next/next/no-img-element */
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useState } from "react";
