ee64d7b016c7799b7c0cd61bafe1609e641c2b08
Ernesto Joel Hdz <erhernandez@altimetrik.com>
2023-06-24 09:35:38 -0600

    clean and fix

--- apps/web/app/(auth)/signin/ProviderSigInBtn.tsx
+++ apps/web/app/(auth)/signin/ProviderSigInBtn.tsx
@@ -11,12 +11,11 @@ export const  ProviderSigInBtn = ({provider, num}:{provider:any, num:number}) =>
 
     return (
         <>
-        <button className="login-button" onClick={() => signIn(provider.id)}>
+          <button type="button" className="login-button" onClick={() => signIn(provider.id)}>
             <div>
               <Image src="./key.svg" width={22} height={12} alt="login" />
             </div>
             {num > 1 ? `Log in using ${provider.name}` : `Log in using SSO`}
-            
           </button>
         </>
     )
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 
 // component exports
-export * from "./Button";
 export * from "./Header";
 
 export * from "./components/Container"
