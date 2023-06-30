1c26745fe8231bca5bbba2670b38d045ded5c8a2
Diego Arturo Hernandez Fuentes <dhernandez@altimetrik.com>
2023-06-30 09:26:16 -0600

    style: update padding for side bar

--- apps/web/components/Snippet/Snippet.tsx
+++ apps/web/components/Snippet/Snippet.tsx
@@ -5,7 +5,7 @@ import { stackoverflowDark } from 'react-syntax-highlighter/dist/esm/styles/hljs
 
 const Snippet = ({value, language='json'}) => {
     
-    return (<div className='frame'>
+    return (<div className='frame snippet'>
         <SyntaxHighlighter style={stackoverflowDark} language={language}>{
             `${language === 'json' && JSON.stringify(value, null, 2)}`
         }</SyntaxHighlighter>
