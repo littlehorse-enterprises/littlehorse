65e1a1f237e804efc6b200f1dd7d463cfbaa337a
G-Linares <gerrylinares@gmail.com>
2023-07-04 15:42:23 -0600

    Cleared console warnings

--- packages/ui/components/LoadMoreButton/LoadMoreButton.tsx
+++ packages/ui/components/LoadMoreButton/LoadMoreButton.tsx
@@ -2,20 +2,31 @@ import * as React from "react";
 /* eslint-disable @next/next/no-img-element */
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  loading: boolean
+  loading?: boolean;
+  disabled?: boolean;
+  children: React.ReactNode;
 }
 
-const LoadMoreButton: React.ForwardRefRenderFunction<HTMLButtonElement, ButtonProps> = (props: ButtonProps) => {
+const LoadMoreButton: React.ForwardRefRenderFunction<
+  HTMLButtonElement,
+  ButtonProps
+> = ({ loading, disabled, children, ...props }: ButtonProps) => {
   // return <button disabled={props.disabled} className={`load_more_btn ${props.disabled && 'disabled'}`} {...props}>{props.children} <img src="/add.svg" alt="add" /></button>;
-  return <button disabled={props.disabled} className={`load_more_btn ${props.disabled && 'disabled'}`} {...props}>
-      {props.children} 
-      {props.loading ? 
-        <img className="spinner" src="/Spinner.svg" alt="Spinner" /> 
-        : 
-        <img src="/add.svg" alt="add" /> 
-      }
-  </button>;
-}
+  return (
+    <button
+      disabled={disabled}
+      className={`load_more_btn ${disabled ? "disabled" : null}`}
+      {...props}
+    >
+      {children}
+      {loading ? (
+        <img className="spinner" src="/Spinner.svg" alt="Spinner" />
+      ) : (
+        <img src="/add.svg" alt="add" />
+      )}
+    </button>
+  );
+};
 
 LoadMoreButton.displayName = "LoadMoreButton";
 
