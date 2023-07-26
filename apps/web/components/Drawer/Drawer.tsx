6d4f9f1aca6c99e93120c37cf83c01058ebe8dba
EmmanuelQuintal <emmanuel.qunital-c@progleasing.com>
2023-07-26 11:41:37 -0600

    Bugs fix from LHD-113 and LHD120

--- apps/web/components/Drawer/Drawer.tsx
+++ apps/web/components/Drawer/Drawer.tsx
@@ -1,30 +1,19 @@
-import React from 'react'
-import Image from 'next/image'
-import arrowRightSvg from './arrow-right.svg'
+import React from "react";
+import Image from "next/image";
+import arrowRightSvg from "./arrow-right.svg";
 
 interface DrawerProps {
-	title: string
-	children?: React.ReactNode
+  title: string;
+  children?: React.ReactNode;
 }
 
 export const Drawer = (props: DrawerProps) => {
-	return (
-		<div className='drawer scrollbar'>
-			<header className='drawer__header'>
-				<button
-					className='drawer__header__button'
-					onClick={() => console.warn('Missing clickHandler')}
-				>
-					<Image
-						src={arrowRightSvg}
-						alt={'arrow-right'}
-						width={24}
-						height={24}
-					/>
-				</button>
-				<p>{props.title}</p>
-			</header>
-			{props.children}
-		</div>
-	)
-}
+  return (
+    <div className="drawer scrollbar">
+      <header className="drawer__header">
+        <p>{props.title}</p>
+      </header>
+      {props.children}
+    </div>
+  );
+};
