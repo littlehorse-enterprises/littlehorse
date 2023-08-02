ee458c970ab67ca87c0be0bc2d3bce9c91b7cc42
Colt McNealy <colt@littlehorse.io>
2023-08-02 12:01:10 -0700

    Adds unit test for early return

--- sdk-java/src/test/java/io/littlehorse/sdk/wfsdk/internal/ThreadBuilderImplTest.java
+++ sdk-java/src/test/java/io/littlehorse/sdk/wfsdk/internal/ThreadBuilderImplTest.java
@@ -3,11 +3,15 @@ package io.littlehorse.sdk.wfsdk.internal;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import io.littlehorse.sdk.common.proto.ComparatorPb;
 import io.littlehorse.sdk.common.proto.NodePb;
+import io.littlehorse.sdk.common.proto.NodePb.NodeCase;
 import io.littlehorse.sdk.common.proto.PutWfSpecPb;
 import io.littlehorse.sdk.common.proto.SleepNodePb.SleepLengthCase;
 import io.littlehorse.sdk.common.proto.VariableTypePb;
 import io.littlehorse.sdk.wfsdk.WfRunVariable;
+import java.util.List;
+import java.util.Map;
 import net.datafaker.Faker;
 import org.junit.jupiter.api.Test;
 
@@ -37,5 +41,41 @@ public class ThreadBuilderImplTest {
             SleepLengthCase.TIMESTAMP
         );
         assertEquals(sleepNode.getSleep().getTimestamp().getVariableName(), "my-var");
+    }
+
+    @Test
+    void testEarlyReturn() {
+        WorkflowImpl wf = new WorkflowImpl(
+            "asdf",
+            thread -> {
+                WfRunVariable var = thread.addVariable("my-var", VariableTypePb.INT);
+                thread.doIf(
+                    thread.condition(var, ComparatorPb.LESS_THAN, 10),
+                    ifBody -> {
+                        ifBody.execute("foo");
+                        ifBody.complete();
+                    }
+                );
+                thread.execute("bar");
+            }
+        );
+
+        PutWfSpecPb wfSpec = wf.compileWorkflow();
+        List<Map.Entry<String, NodePb>> exitNodes = wfSpec
+            .getThreadSpecsOrThrow(wfSpec.getEntrypointThreadName())
+            .getNodesMap()
+            .entrySet()
+            .stream()
+            .filter(nodePair -> {
+                return (nodePair.getValue().getNodeCase() == NodeCase.EXIT);
+            })
+            .toList();
+
+        assertEquals(exitNodes.size(), 2);
+
+        exitNodes.forEach(entry -> {
+            NodePb node = entry.getValue();
+            assertThat(node.getOutgoingEdgesCount() == 0);
+        });
     }
 }
