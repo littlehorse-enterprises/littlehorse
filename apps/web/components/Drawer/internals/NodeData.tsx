667ee72fe9af21b60cd3d6056e8a68bde84dcb10
EmmanuelQuintal <emmanuel.qunital-c@progleasing.com>
2023-08-09 09:07:37 -0600

    Adding more css styles, bug fixes in taskRun

--- apps/web/components/Drawer/internals/NodeData.tsx
+++ apps/web/components/Drawer/internals/NodeData.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export interface NodeDataProps {
-	reachTime: string
-	completionTime: string
-	status: string
+  reachTime: string;
+  completionTime: string;
+  status: string;
 }
 
 export const NodeData = (props: NodeDataProps) => {
-	return (
-		<div className='drawer__nodeData'>
-			<div className='drawer__nodeData__label'>Node data</div>
-			<div className='grid-3'>
-				<p className='drawer__nodeData__header'>REACH TIME</p>
-				<p className='drawer__nodeData__data'>{props.reachTime}</p>
-				<p className='drawer__nodeData__header'>COMPLETION TIME</p>
-				<p className='drawer__nodeData__data'>{props.completionTime}</p>
-				<p className='drawer__nodeData__headerSimple'>STATUS</p>
-				<p className='drawer__nodeData__dataSimple'>{props.status}</p>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="drawer__nodeData">
+      <div className="drawer__nodeData__label">Node data</div>
+      <div className="grid-3">
+        <p className="drawer__nodeData__header">REACH TIME</p>
+        <p className="drawer__nodeData__data">{props.reachTime}</p>
+        <p className="drawer__nodeData__header">COMPLETION TIME</p>
+        <p className="drawer__nodeData__data">{props.completionTime}</p>
+        <p className="drawer__nodeData__header">STATUS</p>
+        <p className="drawer__nodeData__data">{props.status}</p>
+      </div>
+    </div>
+  );
+};
