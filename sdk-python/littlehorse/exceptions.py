27c54400f255fbf21d3d3f12c1c8b54e5e6cacca
Saúl Piña <saul@littlehorse.io>
2023-08-18 16:29:45 -0500

    Remove exception

--- sdk-python/littlehorse/exceptions.py
+++ sdk-python/littlehorse/exceptions.py
@@ -10,12 +10,6 @@ class InvalidTaskDefNameException(Exception):
         super().__init__(self.message)
 
 
-class InvalidCallException(Exception):
-    def __init__(self, message: str):
-        self.message = message
-        super().__init__(self.message)
-
-
 class OAuthException(Exception):
     def __init__(self, message: str):
         self.message = message
