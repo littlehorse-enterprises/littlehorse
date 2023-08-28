5c8efdd6c3f27f94a5228069ad2233b5a7ece0f6
Saúl Piña <saul@littlehorse.io>
2023-08-28 10:37:33 -0500

    Applying naming convention

--- local-dev/build.sh
+++ local-dev/build.sh
@@ -3,16 +3,16 @@
 set -e
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-LH_SERVER_WORK_DIR=$(cd "$SCRIPT_DIR/../docker/lh-server" && pwd)
-LH_STANDALONE_WORK_DIR=$(cd "$SCRIPT_DIR/../docker/lh-standalone" && pwd)
+LH_SERVER_WORK_DIR=$(cd "$SCRIPT_DIR/../docker/server" && pwd)
+LH_STANDALONE_WORK_DIR=$(cd "$SCRIPT_DIR/../docker/standalone" && pwd)
 CONTEXT_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
 
 cd "$CONTEXT_DIR"
 
-echo "Building lh-server"
+echo "Building server"
 docker build --file ${LH_SERVER_WORK_DIR}/Dockerfile \
-    --tag littlehorse/lh-server:latest ${CONTEXT_DIR}
+    --tag littlehorse/littlehorse-server:latest ${CONTEXT_DIR}
 
-echo "Building lh-standalone"
+echo "Building standalone"
 docker build --file ${LH_STANDALONE_WORK_DIR}/Dockerfile \
-    --tag littlehorse/lh-standalone:latest ${CONTEXT_DIR}
+    --tag littlehorse/littlehorse-standalone:latest ${CONTEXT_DIR}
