b7f0a0256eafa78f82546dcf82c9951768cc2823
Eduwer Camacaro <eduwerc@gmail.com>
2023-08-29 10:14:42 -0500

    handle exception

--- sdk-java/src/main/java/io/littlehorse/sdk/common/proto/NodeRunOuterClass.java
+++ sdk-java/src/main/java/io/littlehorse/sdk/common/proto/NodeRunOuterClass.java
@@ -109,24 +109,26 @@ public final class NodeRunOuterClass {
       "task_run_id\"\017\n\rEntrypointRun\"\t\n\007ExitRun\"" +
       "\\\n\016StartThreadRun\022\034\n\017child_thread_id\030\001 \001" +
       "(\005H\000\210\001\001\022\030\n\020thread_spec_name\030\002 \001(\tB\022\n\020_ch" +
-      "ild_thread_id\"\373\001\n\021WaitForThreadsRun\022=\n\007t" +
+      "ild_thread_id\"\253\002\n\021WaitForThreadsRun\022=\n\007t" +
       "hreads\030\001 \003(\0132,.littlehorse.WaitForThread" +
-      "sRun.WaitForThread\032\246\001\n\rWaitForThread\0228\n\017" +
+      "sRun.WaitForThread\032\326\001\n\rWaitForThread\0228\n\017" +
       "thread_end_time\030\001 \001(\0132\032.google.protobuf." +
       "TimestampH\000\210\001\001\022,\n\rthread_status\030\002 \001(\0162\025." +
       "littlehorse.LHStatus\022\031\n\021thread_run_numbe" +
-      "r\030\003 \001(\005B\022\n\020_thread_end_time\"\313\001\n\020External" +
-      "EventRun\022\037\n\027external_event_def_name\030\001 \001(" +
-      "\t\0223\n\nevent_time\030\002 \001(\0132\032.google.protobuf." +
-      "TimestampH\000\210\001\001\022<\n\021external_event_id\030\003 \001(" +
-      "\0132\034.littlehorse.ExternalEventIdH\001\210\001\001B\r\n\013" +
-      "_event_timeB\024\n\022_external_event_id\"C\n\014Sle" +
-      "epNodeRun\0223\n\017maturation_time\030\001 \001(\0132\032.goo" +
-      "gle.protobuf.Timestamp\"n\n\007Failure\022\024\n\014fai" +
-      "lure_name\030\001 \001(\t\022\017\n\007message\030\002 \001(\t\0220\n\007cont" +
-      "ent\030\003 \001(\0132\032.littlehorse.VariableValueH\000\210" +
-      "\001\001B\n\n\010_contentB,\n\037io.littlehorse.sdk.com" +
-      "mon.protoP\001Z\007.;modelb\006proto3"
+      "r\030\003 \001(\005\022.\n\014failure_mode\030\004 \001(\0162\030.littleho" +
+      "rse.FailureModeB\022\n\020_thread_end_time\"\313\001\n\020" +
+      "ExternalEventRun\022\037\n\027external_event_def_n" +
+      "ame\030\001 \001(\t\0223\n\nevent_time\030\002 \001(\0132\032.google.p" +
+      "rotobuf.TimestampH\000\210\001\001\022<\n\021external_event" +
+      "_id\030\003 \001(\0132\034.littlehorse.ExternalEventIdH" +
+      "\001\210\001\001B\r\n\013_event_timeB\024\n\022_external_event_i" +
+      "d\"C\n\014SleepNodeRun\0223\n\017maturation_time\030\001 \001" +
+      "(\0132\032.google.protobuf.Timestamp\"n\n\007Failur" +
+      "e\022\024\n\014failure_name\030\001 \001(\t\022\017\n\007message\030\002 \001(\t" +
+      "\0220\n\007content\030\003 \001(\0132\032.littlehorse.Variable" +
+      "ValueH\000\210\001\001B\n\n\010_content*\027\n\013FailureMode\022\010\n" +
+      "\004FAST\020\000B,\n\037io.littlehorse.sdk.common.pro" +
+      "toP\001Z\007.;modelb\006proto3"
     };
     descriptor = com.google.protobuf.Descriptors.FileDescriptor
       .internalBuildGeneratedFileFrom(descriptorData,
@@ -183,7 +185,7 @@ public final class NodeRunOuterClass {
     internal_static_littlehorse_WaitForThreadsRun_WaitForThread_fieldAccessorTable = new
       com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
         internal_static_littlehorse_WaitForThreadsRun_WaitForThread_descriptor,
-        new java.lang.String[] { "ThreadEndTime", "ThreadStatus", "ThreadRunNumber", "ThreadEndTime", });
+        new java.lang.String[] { "ThreadEndTime", "ThreadStatus", "ThreadRunNumber", "FailureMode", "ThreadEndTime", });
     internal_static_littlehorse_ExternalEventRun_descriptor =
       getDescriptor().getMessageTypes().get(7);
     internal_static_littlehorse_ExternalEventRun_fieldAccessorTable = new
