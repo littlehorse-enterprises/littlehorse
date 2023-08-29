d86c196332a52eb45d69207576f2e8de2a8376e0
Saúl Piña <saul@littlehorse.io>
2023-08-29 09:59:13 -0500

    Freezing python and java version protoc version

--- sdk-python/littlehorse/worker.py
+++ sdk-python/littlehorse/worker.py
@@ -80,7 +80,7 @@ class LHWorkerContext:
         Returns:
             str: An idempotency key.
         """
-        return f"{self.wf_run_id}/{self.task_guid}"
+        return self.task_run_id.task_guid
 
     @property
     def task_def_name(self) -> str:
@@ -127,11 +127,10 @@ class LHWorkerContext:
         return str(
             {
                 "wf_run_id": self.wf_run_id,
-                "task_guid": self.task_guid,
+                "idempotency_key": self.idempotency_key,
                 "task_def_name": self.task_def_name,
                 "scheduled_time": str(self.scheduled_time),
                 "attempt_number": self.attempt_number,
-                "idempotency_key": self.idempotency_key,
             }
         )
 
