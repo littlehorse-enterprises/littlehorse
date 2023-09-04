dc809c4565dee3c73eb2221a1cd49944f5f297d9
David Moreno <David.mesm@gmail.com>
2023-09-04 11:18:25 -0700

    Create required specific Exceptions

---
+++ sdk-dotnet/src/LittleHorseSDK/LittleHorse.Common/Exceptions/LHApiException.cs
@@ -0,0 +1,41 @@
+﻿using LittleHorseSDK.Common.proto;
+
+namespace LittleHorse.Common.Exceptions
+{
+    [Serializable]
+    public class LHApiException : Exception
+    {
+        public LHResponseCode Code { get; init; }
+
+        public LHApiException() : base(CreateBaseMessage()) { }
+
+        public LHApiException(string message) : base (CreateBaseMessage(message)) { }
+
+        public LHApiException(string message, Exception innerException) : base(CreateBaseMessage(message), innerException) {}
+
+        public LHApiException(string message, LHResponseCode code) : base(CreateBaseMessage(message, code))
+        {
+            Code = code;
+        }
+
+        public LHApiException(string message, LHResponseCode code, Exception innerException) : base(CreateBaseMessage(message, code), innerException)
+        {
+            Code = code;
+        }
+
+        private static string CreateBaseMessage()
+        {
+            return $"Failed contacting LH API.";
+        }
+
+        private static string CreateBaseMessage(string message)
+        {
+            return $"Failed contacting LH API: {message}";
+        }
+
+        private static string CreateBaseMessage(string message, LHResponseCode code)
+        {
+            return $"Failed contacting LH API: {code} : {message}";
+        }
+    }
+}
---
+++ sdk-dotnet/src/LittleHorseSDK/LittleHorse.Common/Exceptions/LHSerdeException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleHorse.Common.Exceptions
+{
+    public class LHSerdeException : Exception
+    {
+        public LHSerdeException() : base() { }
+
+        public LHSerdeException(string message) : base(message) { }
+
+        public LHSerdeException(string message, Exception innerException) : base(message, innerException) { }
+
+    }
+}
