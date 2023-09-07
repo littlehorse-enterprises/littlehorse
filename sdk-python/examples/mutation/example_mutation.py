c5b6cd86003dea56876c184ee90076090a841be7
Saúl Piña <saul@littlehorse.io>
2023-09-07 18:12:36 -0500

    Rename python utils (#90)

--- sdk-python/examples/mutation/example_mutation.py
+++ sdk-python/examples/mutation/example_mutation.py
@@ -11,8 +11,8 @@ from littlehorse.workflow import ThreadBuilder, Workflow
 
 logging.basicConfig(level=logging.INFO)
 
-TASK_NAME_SPIDER_BITE = "spider-bite"
-TASK_NAME_DAILY_BUGLE = "daily-bugle"
+SPIDER_BITE = "spider-bite"
+DAILY_BUGLE = "daily-bugle"
 
 
 def get_config() -> LHConfig:
@@ -29,13 +29,13 @@ def get_workflow() -> Workflow:
         name = thread.add_variable("name", VariableType.STR)
 
         # it sends the name to a task worker
-        output = thread.execute(TASK_NAME_SPIDER_BITE, name)
+        output = thread.execute(SPIDER_BITE, name)
 
         # it assigns the result to the variable name
         thread.mutate(name, VariableMutationType.ASSIGN, output)
 
         # it pass the variable to another task
-        thread.execute(TASK_NAME_DAILY_BUGLE, name)
+        thread.execute(DAILY_BUGLE, name)
 
     return Workflow("example-mutation", my_entrypoint)
 
@@ -57,13 +57,13 @@ async def main() -> None:
     config = get_config()
     wf = get_workflow()
 
-    littlehorse.create_task_def(spider_bite, TASK_NAME_SPIDER_BITE, config)
-    littlehorse.create_task_def(daily_bugle, TASK_NAME_DAILY_BUGLE, config)
+    littlehorse.create_task_def(spider_bite, SPIDER_BITE, config)
+    littlehorse.create_task_def(daily_bugle, DAILY_BUGLE, config)
     littlehorse.create_workflow_spec(wf, config)
 
     await littlehorse.start(
-        LHTaskWorker(spider_bite, TASK_NAME_SPIDER_BITE, config),
-        LHTaskWorker(daily_bugle, TASK_NAME_DAILY_BUGLE, config),
+        LHTaskWorker(spider_bite, SPIDER_BITE, config),
+        LHTaskWorker(daily_bugle, DAILY_BUGLE, config),
     )
 
 
--- sdk-python/littlehorse/__init__.py
+++ sdk-python/littlehorse/__init__.py
@@ -1,3 +1,4 @@
+from littlehorse.proto_utils import to_variable_value as _to_variable_value
 from littlehorse.user_utils import create_workflow_spec as _create_workflow_spec
 from littlehorse.user_utils import (
     create_external_event_def as _create_external_event_def,
@@ -10,3 +11,4 @@ start = _start
 create_workflow_spec = _create_workflow_spec
 create_task_def = _create_task_def
 create_external_event_def = _create_external_event_def
+to_variable_value = _to_variable_value
