1b8101bffc1220ad27a5c161c6e6f1fdbed7e83a
Eduwer Camacaro <eduwerc@gmail.com>
2023-09-12 18:35:28 -0500

    fix unit tests

--- server/src/main/java/io/littlehorse/common/model/getable/core/usertaskrun/UserTaskRunModel.java
+++ server/src/main/java/io/littlehorse/common/model/getable/core/usertaskrun/UserTaskRunModel.java
@@ -452,7 +452,8 @@ public class UserTaskRunModel extends CoreGetable<UserTaskRun> {
     public List<GetableIndex<? extends AbstractGetable<?>>> getIndexConfigurations() {
         return List.of(
                 new GetableIndex<UserTaskRunModel>(
-                        List.of(Pair.of("wfRunId", GetableIndex.ValueType.SINGLE)), Optional.of(TagStorageType.LOCAL)),
+                        List.of(Pair.of("userTaskDefName", GetableIndex.ValueType.SINGLE)),
+                        Optional.of(TagStorageType.LOCAL)),
                 // Future: We will make this LOCAL if it's DONE or CANCELLED, and
                 // REMOTE if it's CLAIMED, UNASSIGNED, or ASSIGNED_NOT_CLAIMED.
                 new GetableIndex<UserTaskRunModel>(
