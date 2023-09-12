bbd5b7253fdaba887ac81255b0cbf647c4e0d71d
Eduwer Camacaro <eduwerc@gmail.com>
2023-09-12 18:10:53 -0500

    Handling any error or exception

--- lh-test/src/main/java/io/littlehorse/test/internal/step/AbstractStep.java
+++ lh-test/src/main/java/io/littlehorse/test/internal/step/AbstractStep.java
@@ -1,11 +1,16 @@
 package io.littlehorse.test.internal.step;
 
 import io.littlehorse.sdk.common.proto.LHPublicApiGrpc.LHPublicApiBlockingStub;
+import io.littlehorse.sdk.common.proto.WfRun;
+import io.littlehorse.sdk.common.proto.WfRunId;
 import io.littlehorse.test.internal.LHTestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class AbstractStep implements Step {
 
     protected final int id;
+    private Logger logger = LoggerFactory.getLogger(Step.class);
 
     public AbstractStep(int id) {
         this.id = id;
@@ -16,8 +21,15 @@ public abstract class AbstractStep implements Step {
         try {
             tryExecute(context, lhClient);
         } catch (Throwable throwable) {
+            printWfRun(context.toString(), lhClient);
             this.handleException(throwable);
         }
+    }
+
+    private void printWfRun(String id, LHPublicApiBlockingStub lhClient) {
+        WfRunId wfRunId = WfRunId.newBuilder().setId(id).build();
+        WfRun wfRun = lhClient.getWfRun(wfRunId);
+        logger.debug(wfRun.toString());
     }
 
     protected void handleException(Throwable ex) throws LHTestException {
