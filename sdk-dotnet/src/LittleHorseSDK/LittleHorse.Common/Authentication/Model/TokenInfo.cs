72342d26b91d4a595f88f270ed3896f23ca10e5f
David Moreno <David.mesm@gmail.com>
2023-09-13 11:27:31 -0600

    Add OAuth to SDK

---
+++ sdk-dotnet/src/LittleHorseSDK/LittleHorse.Common/Authentication/Model/TokenInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleHorse.Common.Authentication.Model
+{
+    public class TokenInfo
+    {
+        public string ClientId { get; init; }
+        public string Token { get; init; }
+        public DateTime Expiration { get; init; }
+
+        public bool IsExpired
+        {
+            get
+            {
+                return Expiration > DateTime.UtcNow;
+            }
+        }
+
+        public TokenInfo(string clientId, string token, DateTime expiration)
+        {
+            ClientId = clientId;
+            Token = token;
+            Expiration = expiration;
+        }
+    }
+}
---
+++ sdk-dotnet/src/LittleHorseSDK/LittleHorse.Common/Authentication/OAuthClient.cs
@@ -0,0 +1,61 @@
+﻿using LittleHorse.Common.Authentication.Model;
+using IdentityModel.Client;
+using System.Net;
+using LittleHorse.Common.Exceptions;
+using Microsoft.Extensions.Logging;
+
+namespace LittleHorse.Common.Authentication
+{
+    public class OAuthClient
+    {
+        private OAuthConfig _oAuthConfig;
+        private ILogger? _logger;
+
+        public OAuthClient (OAuthConfig oAuthConfig, ILogger? logger)
+        {
+            _oAuthConfig = oAuthConfig;
+            _logger = logger;
+        }
+
+        public async Task<TokenInfo> GetAccessTokenAsync()
+        {
+            try
+            {
+                var client = new HttpClient();
+
+                var response = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+                {
+                    Address = _oAuthConfig.TokenEndpointURI.AbsoluteUri,
+                    ClientId = _oAuthConfig.ClientId,
+                    ClientSecret = _oAuthConfig.ClientSecret
+                });
+
+                if (response.HttpStatusCode != HttpStatusCode.OK || response.AccessToken is null)
+                {
+                    var errorMessage = "";
+                    if (response.HttpErrorReason != null)
+                    {
+                        errorMessage = $"Error getting the token status: {response.HttpErrorReason}";
+                    }
+                    else
+                    {
+                        errorMessage = "Error getting the token status.";
+                    }
+
+                    throw new LHAuthorizationServerException(response.HttpStatusCode, errorMessage);
+                }
+
+                var currentDateTime = DateTime.UtcNow;
+
+                var expirationDateTime = currentDateTime.AddSeconds(response.ExpiresIn);
+
+                return new TokenInfo(_oAuthConfig.ClientId, response.AccessToken, expirationDateTime);
+            }
+            catch (Exception ex)
+            {
+                _logger?.LogError(ex, ex.Message);
+                throw;
+            }
+        }
+    }
+}
