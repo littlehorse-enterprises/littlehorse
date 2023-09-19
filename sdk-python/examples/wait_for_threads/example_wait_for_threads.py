10c25433812f884b64a876f5eb5e1447f5747084
Eduwer Camacaro <eduwerc@gmail.com>
2023-09-19 05:26:39 -0500

    Conditions for approval workflow

--- sdk-python/examples/wait_for_threads/example_wait_for_threads.py
+++ sdk-python/examples/wait_for_threads/example_wait_for_threads.py
@@ -5,7 +5,11 @@ from pathlib import Path
 import littlehorse
 from littlehorse.config import LHConfig
 from littlehorse.model.common_enums_pb2 import VariableType
-from littlehorse.model.common_wfspec_pb2 import IndexType, Comparator
+from littlehorse.model.common_wfspec_pb2 import (
+    IndexType,
+    Comparator,
+    VariableMutationType,
+)
 from littlehorse.worker import LHTaskWorker
 from littlehorse.workflow import ThreadBuilder, Workflow
 
@@ -23,21 +27,40 @@ def get_config() -> LHConfig:
 
 
 def get_workflow() -> Workflow:
-    def is_user_group(user_group_thread: ThreadBuilder) -> None:
-        pass
-
-    def is_user(user_thread: ThreadBuilder) -> None:
-        pass
-
     def one_approval(approval_thread: ThreadBuilder) -> None:
-        input_var = approval_thread.add_variable("INPUT", VariableType.JSON_OBJ)
-        (
+        approval = approval_thread.add_variable("INPUT", VariableType.JSON_OBJ)
+        is_approved = (
             approval_thread.add_variable("did-person-approve", VariableType.BOOL)
             .with_index(IndexType.LOCAL_INDEX)
             .persistent()
         )
+
+        def is_user_group(user_group_thread: ThreadBuilder) -> None:
+            user_task_output = approval_thread.assign_user_task(
+                "approve-task",
+                user_id=None,
+                user_group=approval.with_json_path("$.userGroup"),
+            )
+            user_group_thread.mutate(
+                is_approved,
+                VariableMutationType.ASSIGN,
+                user_task_output.with_json_path("$.isApproved"),
+            )
+
+        def is_user(user_thread: ThreadBuilder) -> None:
+            user_task_output = approval_thread.assign_user_task(
+                "approve-task",
+                user_id=approval.with_json_path("$.userId"),
+                user_group=approval.with_json_path("$.userGroup"),
+            )
+            user_thread.mutate(
+                is_approved,
+                VariableMutationType.ASSIGN,
+                user_task_output.with_json_path("$.isApproved"),
+            )
+
         condition = approval_thread.condition(
-            input_var.with_json_path("$.userId"), Comparator.EQUALS, None
+            approval.with_json_path("$.userId"), Comparator.EQUALS, None
         )
         approval_thread.do_if(
             condition=condition, if_body=is_user_group, else_body=is_user
