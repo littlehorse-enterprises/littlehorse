32d34b0dd9c69cddb70e8e4f66f3edd91619573e
Eduwer Camacaro <eduwerc@gmail.com>
2023-09-19 04:26:11 -0500

    fixed bug on starting multiple threads

--- sdk-python/examples/wait_for_threads/example_wait_for_threads.py
+++ sdk-python/examples/wait_for_threads/example_wait_for_threads.py
@@ -10,7 +10,7 @@ from littlehorse.workflow import ThreadBuilder, Workflow
 
 logging.basicConfig(level=logging.INFO)
 
-TASK_NAME = "task-1"
+TASK_NAME = "task-with-vars"
 
 
 def get_config() -> LHConfig:
@@ -23,24 +23,21 @@ def get_config() -> LHConfig:
 
 def get_workflow() -> Workflow:
     def thread_1(thread: ThreadBuilder) -> None:
-        thread.execute(TASK_NAME)
+        input_var = thread.add_variable("INPUT", VariableType.JSON_OBJ)
+        thread.execute(TASK_NAME, input_var.with_json_path("$.user"))
 
     def my_entrypoint(thread: ThreadBuilder) -> None:
         # it receives a name
         input = thread.add_variable("my-input", VariableType.JSON_ARR)
-        vars: dict[str, str] = {}
-        vars["var1"] = "mycontent"
-        spawned_thread_1 = thread.spawn_thread_for_each(
-            input, thread_1, "my-thread-1", vars
-        )
+        spawned_thread_1 = thread.spawn_thread_for_each(input, thread_1, "my-thread-1")
         thread.wait_for_threads(spawned_thread_1)
-        thread.execute(TASK_NAME)
+        thread.execute(TASK_NAME, "var")
 
-    return Workflow("example-wait-for-threads-v4", my_entrypoint)
+    return Workflow("example-wait-for-threads-v9", my_entrypoint)
 
 
-async def task() -> None:
-    print("This is an example")
+async def task(arg1: str) -> None:
+    print("This is an example var " + arg1)
 
 
 async def daily_bugle(news: str) -> None:
