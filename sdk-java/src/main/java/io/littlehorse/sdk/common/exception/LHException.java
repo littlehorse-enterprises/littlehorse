c652353d4e968d1fc8d55ecaa1c1e579e9986b36
Eduwer Camacaro <eduwerc@gmail.com>
2023-09-20 09:54:06 -0500

    add name to lh exceptions

--- sdk-java/src/main/java/io/littlehorse/sdk/common/exception/LHException.java
+++ sdk-java/src/main/java/io/littlehorse/sdk/common/exception/LHException.java
@@ -1,11 +1,17 @@
 package io.littlehorse.sdk.common.exception;
 
+import lombok.Getter;
+
 /**
  * Thrown to indicate that a Task method reached a client specific error.
  */
+@Getter
 public class LHException extends Exception {
 
-    public LHException(String message, Throwable cause) {
+    private final String name;
+
+    public LHException(String name, String message, Throwable cause) {
         super(message, cause);
+        this.name = name;
     }
 }
