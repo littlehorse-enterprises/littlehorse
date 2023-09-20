3a34f6f7e0df049bfc9453997de9207fc3c7644b
Eduwer Camacaro <eduwerc@gmail.com>
2023-09-20 10:02:07 -0500

    Add exception name to report task run subcommand

--- sdk-java/src/main/java/io/littlehorse/sdk/common/proto/ReportTaskRun.java
+++ sdk-java/src/main/java/io/littlehorse/sdk/common/proto/ReportTaskRun.java
@@ -17,6 +17,7 @@ private static final long serialVersionUID = 0L;
   }
   private ReportTaskRun() {
     status_ = 0;
+    exceptionName_ = "";
   }
 
   @java.lang.Override
@@ -173,6 +174,53 @@ private static final long serialVersionUID = 0L;
     return attemptNumber_;
   }
 
+  public static final int EXCEPTION_NAME_FIELD_NUMBER = 7;
+  @SuppressWarnings("serial")
+  private volatile java.lang.Object exceptionName_ = "";
+  /**
+   * <code>optional string exception_name = 7;</code>
+   * @return Whether the exceptionName field is set.
+   */
+  @java.lang.Override
+  public boolean hasExceptionName() {
+    return ((bitField0_ & 0x00000004) != 0);
+  }
+  /**
+   * <code>optional string exception_name = 7;</code>
+   * @return The exceptionName.
+   */
+  @java.lang.Override
+  public java.lang.String getExceptionName() {
+    java.lang.Object ref = exceptionName_;
+    if (ref instanceof java.lang.String) {
+      return (java.lang.String) ref;
+    } else {
+      com.google.protobuf.ByteString bs = 
+          (com.google.protobuf.ByteString) ref;
+      java.lang.String s = bs.toStringUtf8();
+      exceptionName_ = s;
+      return s;
+    }
+  }
+  /**
+   * <code>optional string exception_name = 7;</code>
+   * @return The bytes for exceptionName.
+   */
+  @java.lang.Override
+  public com.google.protobuf.ByteString
+      getExceptionNameBytes() {
+    java.lang.Object ref = exceptionName_;
+    if (ref instanceof java.lang.String) {
+      com.google.protobuf.ByteString b = 
+          com.google.protobuf.ByteString.copyFromUtf8(
+              (java.lang.String) ref);
+      exceptionName_ = b;
+      return b;
+    } else {
+      return (com.google.protobuf.ByteString) ref;
+    }
+  }
+
   private byte memoizedIsInitialized = -1;
   @java.lang.Override
   public final boolean isInitialized() {
@@ -204,6 +252,9 @@ private static final long serialVersionUID = 0L;
     }
     if (attemptNumber_ != 0) {
       output.writeInt32(6, attemptNumber_);
+    }
+    if (((bitField0_ & 0x00000004) != 0)) {
+      com.google.protobuf.GeneratedMessageV3.writeString(output, 7, exceptionName_);
     }
     getUnknownFields().writeTo(output);
   }
@@ -237,6 +288,9 @@ private static final long serialVersionUID = 0L;
     if (attemptNumber_ != 0) {
       size += com.google.protobuf.CodedOutputStream
         .computeInt32Size(6, attemptNumber_);
+    }
+    if (((bitField0_ & 0x00000004) != 0)) {
+      size += com.google.protobuf.GeneratedMessageV3.computeStringSize(7, exceptionName_);
     }
     size += getUnknownFields().getSerializedSize();
     memoizedSize = size;
@@ -276,6 +330,11 @@ private static final long serialVersionUID = 0L;
     }
     if (getAttemptNumber()
         != other.getAttemptNumber()) return false;
+    if (hasExceptionName() != other.hasExceptionName()) return false;
+    if (hasExceptionName()) {
+      if (!getExceptionName()
+          .equals(other.getExceptionName())) return false;
+    }
     if (!getUnknownFields().equals(other.getUnknownFields())) return false;
     return true;
   }
@@ -307,6 +366,10 @@ private static final long serialVersionUID = 0L;
     }
     hash = (37 * hash) + ATTEMPT_NUMBER_FIELD_NUMBER;
     hash = (53 * hash) + getAttemptNumber();
+    if (hasExceptionName()) {
+      hash = (37 * hash) + EXCEPTION_NAME_FIELD_NUMBER;
+      hash = (53 * hash) + getExceptionName().hashCode();
+    }
     hash = (29 * hash) + getUnknownFields().hashCode();
     memoizedHashCode = hash;
     return hash;
@@ -469,6 +532,7 @@ private static final long serialVersionUID = 0L;
         logOutputBuilder_ = null;
       }
       attemptNumber_ = 0;
+      exceptionName_ = "";
       return this;
     }
 
@@ -530,6 +594,10 @@ private static final long serialVersionUID = 0L;
       }
       if (((from_bitField0_ & 0x00000020) != 0)) {
         result.attemptNumber_ = attemptNumber_;
+      }
+      if (((from_bitField0_ & 0x00000040) != 0)) {
+        result.exceptionName_ = exceptionName_;
+        to_bitField0_ |= 0x00000004;
       }
       result.bitField0_ |= to_bitField0_;
     }
@@ -596,6 +664,11 @@ private static final long serialVersionUID = 0L;
       if (other.getAttemptNumber() != 0) {
         setAttemptNumber(other.getAttemptNumber());
       }
+      if (other.hasExceptionName()) {
+        exceptionName_ = other.exceptionName_;
+        bitField0_ |= 0x00000040;
+        onChanged();
+      }
       this.mergeUnknownFields(other.getUnknownFields());
       onChanged();
       return this;
@@ -660,6 +733,11 @@ private static final long serialVersionUID = 0L;
               bitField0_ |= 0x00000020;
               break;
             } // case 48
+            case 58: {
+              exceptionName_ = input.readStringRequireUtf8();
+              bitField0_ |= 0x00000040;
+              break;
+            } // case 58
             default: {
               if (!super.parseUnknownField(input, extensionRegistry, tag)) {
                 done = true; // was an endgroup tag
@@ -1234,6 +1312,85 @@ private static final long serialVersionUID = 0L;
     public Builder clearAttemptNumber() {
       bitField0_ = (bitField0_ & ~0x00000020);
       attemptNumber_ = 0;
+      onChanged();
+      return this;
+    }
+
+    private java.lang.Object exceptionName_ = "";
+    /**
+     * <code>optional string exception_name = 7;</code>
+     * @return Whether the exceptionName field is set.
+     */
+    public boolean hasExceptionName() {
+      return ((bitField0_ & 0x00000040) != 0);
+    }
+    /**
+     * <code>optional string exception_name = 7;</code>
+     * @return The exceptionName.
+     */
+    public java.lang.String getExceptionName() {
+      java.lang.Object ref = exceptionName_;
+      if (!(ref instanceof java.lang.String)) {
+        com.google.protobuf.ByteString bs =
+            (com.google.protobuf.ByteString) ref;
+        java.lang.String s = bs.toStringUtf8();
+        exceptionName_ = s;
+        return s;
+      } else {
+        return (java.lang.String) ref;
+      }
+    }
+    /**
+     * <code>optional string exception_name = 7;</code>
+     * @return The bytes for exceptionName.
+     */
+    public com.google.protobuf.ByteString
+        getExceptionNameBytes() {
+      java.lang.Object ref = exceptionName_;
+      if (ref instanceof String) {
+        com.google.protobuf.ByteString b = 
+            com.google.protobuf.ByteString.copyFromUtf8(
+                (java.lang.String) ref);
+        exceptionName_ = b;
+        return b;
+      } else {
+        return (com.google.protobuf.ByteString) ref;
+      }
+    }
+    /**
+     * <code>optional string exception_name = 7;</code>
+     * @param value The exceptionName to set.
+     * @return This builder for chaining.
+     */
+    public Builder setExceptionName(
+        java.lang.String value) {
+      if (value == null) { throw new NullPointerException(); }
+      exceptionName_ = value;
+      bitField0_ |= 0x00000040;
+      onChanged();
+      return this;
+    }
+    /**
+     * <code>optional string exception_name = 7;</code>
+     * @return This builder for chaining.
+     */
+    public Builder clearExceptionName() {
+      exceptionName_ = getDefaultInstance().getExceptionName();
+      bitField0_ = (bitField0_ & ~0x00000040);
+      onChanged();
+      return this;
+    }
+    /**
+     * <code>optional string exception_name = 7;</code>
+     * @param value The bytes for exceptionName to set.
+     * @return This builder for chaining.
+     */
+    public Builder setExceptionNameBytes(
+        com.google.protobuf.ByteString value) {
+      if (value == null) { throw new NullPointerException(); }
+      checkByteStringIsUtf8(value);
+      exceptionName_ = value;
+      bitField0_ |= 0x00000040;
       onChanged();
       return this;
     }
