4f18ebedca366e8c50e195cd6c45a1ffef5c9217
Eduwer Camacaro <eduwerc@gmail.com>
2023-10-18 10:11:15 -0500

    fixing tests

--- server/src/main/java/io/littlehorse/server/streams/topology/core/processors/RepartitionCommandProcessor.java
+++ server/src/main/java/io/littlehorse/server/streams/topology/core/processors/RepartitionCommandProcessor.java
@@ -5,23 +5,27 @@ import io.littlehorse.common.model.repartitioncommand.RepartitionCommand;
 import io.littlehorse.common.model.repartitioncommand.repartitionsubcommand.TaskMetricUpdate;
 import io.littlehorse.common.model.repartitioncommand.repartitionsubcommand.WfMetricUpdate;
 import io.littlehorse.common.util.LHUtil;
+import io.littlehorse.server.streams.ServerTopology;
 import io.littlehorse.server.streams.store.LHKeyValueIterator;
 import io.littlehorse.server.streams.store.LHStore;
 import java.time.Duration;
 import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.DateUtils;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.processor.PunctuationType;
 import org.apache.kafka.streams.processor.api.Processor;
 import org.apache.kafka.streams.processor.api.ProcessorContext;
 import org.apache.kafka.streams.processor.api.Record;
+import org.apache.kafka.streams.state.KeyValueStore;
 
 @Slf4j
 public class RepartitionCommandProcessor implements Processor<String, RepartitionCommand, Void, Void> {
 
-    private LHStore store;
     private LHServerConfig config;
     private ProcessorContext<Void, Void> ctx;
+
+    private KeyValueStore<String, Bytes> nativeStore;
 
     public RepartitionCommandProcessor(LHServerConfig config) {
         this.config = config;
@@ -29,55 +33,55 @@ public class RepartitionCommandProcessor implements Processor<String, Repartitio
 
     public void init(final ProcessorContext<Void, Void> ctx) {
         this.ctx = ctx;
-        // store = new RocksDBWrapper(ctx.getStateStore(ServerTopology.CORE_REPARTITION_STORE), config); TODO WIP
-
+        this.nativeStore = ctx.getStateStore(ServerTopology.CORE_REPARTITION_STORE);
         ctx.schedule(Duration.ofMinutes(1), PunctuationType.WALL_CLOCK_TIME, this::cleanOldMetrics);
     }
 
     public void process(final Record<String, RepartitionCommand> record) {
         if (record.value() != null) {
             log.debug("Received a metric update!");
-            record.value().process(store, ctx);
+            RepartitionCommand command = record.value();
+            record.value().process(LHStore.instanceFor(nativeStore, command.getTenantId()), ctx);
         }
     }
 
     public void cleanOldMetrics(long timestamp) {
+        final LHStore defaultStore = LHStore.defaultStore(nativeStore);
         Date thirtyDaysAgo = DateUtils.addDays(new Date(), -30);
-        cleanOldTaskMetrics(thirtyDaysAgo);
-        cleanOldWfMetrics(thirtyDaysAgo);
+        cleanOldTaskMetrics(defaultStore, thirtyDaysAgo);
+        cleanOldWfMetrics(defaultStore, thirtyDaysAgo);
     }
 
-    private void cleanOldTaskMetrics(Date daysAgo) {
+    private void cleanOldTaskMetrics(LHStore defaultStore, Date daysAgo) {
         try (LHKeyValueIterator<TaskMetricUpdate> iter =
-                store.range("", LHUtil.toLhDbFormat(daysAgo), TaskMetricUpdate.class)) {
+                defaultStore.range("", LHUtil.toLhDbFormat(daysAgo), TaskMetricUpdate.class)) {
             while (iter.hasNext()) {
                 log.trace("Skipping the cleaning of old metrics as they are currently not implemented.");
-                /*
-                 * LHIterKeyValue<TaskMetricUpdate> next = iter.next();
+
+                /** LHIterKeyValue<TaskMetricUpdate> next = iter.next();
                  * TaskMetricUpdate metric = next.getValue();
                  * store.delete(metric.getStoreKey());
                  * String taskDefMetricKey = TaskDefMetricsModel.getObjectId(metric.type,
                  * metric.windowStart,
                  * metric.taskDefName);
-                 * store.delete(taskDefMetricKey, TaskDefMetricsModel.class);
-                 */
+                 * store.delete(taskDefMetricKey, TaskDefMetricsModel.class);*/
             }
         }
     }
 
-    private void cleanOldWfMetrics(Date daysAgo) {
+    private void cleanOldWfMetrics(LHStore defaultStore, Date daysAgo) {
         try (LHKeyValueIterator<WfMetricUpdate> iter =
-                store.range("", LHUtil.toLhDbFormat(daysAgo), WfMetricUpdate.class)) {
+                defaultStore.range("", LHUtil.toLhDbFormat(daysAgo), WfMetricUpdate.class)) {
             while (iter.hasNext()) {
                 log.trace("Skipping the cleaning of old metrics as they are currently not implemented.");
-                /*
-                 * LHIterKeyValue<WfMetricUpdate> next = iter.next();
+
+                /* * LHIterKeyValue<WfMetricUpdate> next = iter.next();
                  * WfMetricUpdate metric = next.getValue();
                  * store.delete(metric.getStoreKey());
                  * String wfSpecMetricKey = WfSpecMetricsModel.getObjectId(
                  * metric.type, metric.windowStart, metric.wfSpecName, metric.wfSpecVersion);
-                 * store.delete(wfSpecMetricKey, WfSpecMetricsModel.class);
-                 */
+                 * store.delete(wfSpecMetricKey, WfSpecMetricsModel.class);*/
+
             }
         }
     }
---
+++ server/src/test/java/io/littlehorse/server/streams/topology/core/processors/CreateRemoteTagRepartitionCommandTest.java
@@ -0,0 +1,91 @@
+package io.littlehorse.server.streams.topology.core.processors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.littlehorse.TestUtil;
+import io.littlehorse.common.LHServerConfig;
+import io.littlehorse.common.model.repartitioncommand.RepartitionCommand;
+import io.littlehorse.common.model.repartitioncommand.repartitionsubcommand.CreateRemoteTag;
+import io.littlehorse.server.KafkaStreamsServerImpl;
+import io.littlehorse.server.streams.ServerTopology;
+import io.littlehorse.server.streams.store.LHStore;
+import io.littlehorse.server.streams.storeinternals.index.Tag;
+import io.littlehorse.server.streams.util.MetadataCache;
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.processor.api.MockProcessorContext;
+import org.apache.kafka.streams.processor.api.Record;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.Stores;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CreateRemoteTagRepartitionCommandTest {
+
+    private final Tag tagToStore = TestUtil.tag();
+    private final String commandId = UUID.randomUUID().toString();
+    private final RepartitionCommand commandToProcess =
+            new RepartitionCommand(new CreateRemoteTag(tagToStore), new Date(), commandId);
+
+    @Mock
+    private LHServerConfig config;
+
+    @Mock
+    private KafkaStreamsServerImpl server;
+
+    private final MetadataCache metadataCache = new MetadataCache();
+
+    private final KeyValueStore<String, Bytes> nativeInMemoryStore = Stores.keyValueStoreBuilder(
+                    Stores.inMemoryKeyValueStore(ServerTopology.CORE_REPARTITION_STORE),
+                    Serdes.String(),
+                    Serdes.Bytes())
+            .withLoggingDisabled()
+            .build();
+
+    private final MockProcessorContext<Void, Void> mockProcessorContext = new MockProcessorContext<>();
+
+    private RepartitionCommandProcessor commandProcessor;
+
+    private static final String TENANT_ID_A = "A", TENANT_ID_B = "B", DEFAULT_TENANT = "default";
+
+    private LHStore tenantAStore = LHStore.instanceFor(nativeInMemoryStore, TENANT_ID_A);
+    private LHStore tenantBStore = LHStore.instanceFor(nativeInMemoryStore, TENANT_ID_B);
+    private LHStore defaultStore = LHStore.instanceFor(nativeInMemoryStore, DEFAULT_TENANT);
+
+    @BeforeEach
+    public void setup() {
+        nativeInMemoryStore.init(mockProcessorContext.getStateStoreContext(), nativeInMemoryStore);
+        commandProcessor = new RepartitionCommandProcessor(config);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {TENANT_ID_A, TENANT_ID_B, DEFAULT_TENANT})
+    void shouldStoreRemoteTagWithTenantIsolation(final String tenantId) {
+        commandToProcess.setTenantId(tenantId);
+        commandProcessor.init(mockProcessorContext);
+        commandProcessor.process(new Record<>(commandId, commandToProcess, 0L));
+        assertThat(mockProcessorContext.scheduledPunctuators()).hasSize(1);
+        if (Objects.equals(tenantId, TENANT_ID_A)) {
+            ensureTenantIsolation(tagToStore.getStoreKey(), tenantAStore, tenantBStore, defaultStore);
+        } else if (Objects.equals(tenantId, TENANT_ID_B)) {
+            ensureTenantIsolation(tagToStore.getStoreKey(), tenantBStore, tenantAStore, defaultStore);
+        } else {
+            ensureTenantIsolation(tagToStore.getStoreKey(), defaultStore, tenantAStore, tenantBStore);
+        }
+    }
+
+    void ensureTenantIsolation(
+            String tagStoreKey, LHStore storeUnderTest, LHStore firstIsolatedStore, LHStore secondIsolatedStore) {
+        assertThat(storeUnderTest.get(tagStoreKey, Tag.class)).isNotNull();
+        assertThat(firstIsolatedStore.get(tagStoreKey, Tag.class)).isNull();
+        assertThat(secondIsolatedStore.get(tagStoreKey, Tag.class)).isNull();
+    }
+}
