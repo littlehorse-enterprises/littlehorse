972bcfe681965f481c0262559a96064fabf37a10
Eduwer Camacaro <eduwerc@gmail.com>
2023-10-27 16:18:35 -0500

    Authorizer refactors

--- server/src/main/java/io/littlehorse/server/auth/ServerAuthorizer.java
+++ server/src/main/java/io/littlehorse/server/auth/ServerAuthorizer.java
@@ -10,6 +10,4 @@ public interface ServerAuthorizer extends ServerInterceptor {
     Context.Key<PrincipalModel> PRINCIPAL = Context.key("principal");
 
     Metadata.Key<String> CLIENT_ID = Metadata.Key.of("clientId", Metadata.ASCII_STRING_MARSHALLER);
-
-    Metadata.Key<String> TENANT_ID = Metadata.Key.of("tenantId", Metadata.ASCII_STRING_MARSHALLER);
 }
