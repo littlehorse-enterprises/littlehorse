fe8c93fd598c2fa4cd3b7b51cf869deedd409563
Saúl Piña <saul@littlehorse.io>
2023-11-28 11:28:37 -0500

    Finish standalone docker
    Co-authored-by: Nelson Jumbo <nelson@littlehorse.io>

--- docker/standalone/dashboard-entrypoint.sh
+++ docker/standalone/dashboard-entrypoint.sh
@@ -2,3 +2,12 @@
 
 set -e
 
+export API_URL=${API_URL:-localhost:2023}
+export AUTHENTICATION_ENABLED=${AUTHENTICATION_ENABLED:-"false"}
+export AUTH_SECRET=${AUTH_SECRET:-$(uuidgen)}
+export NEXTAUTH_URL=${AUTH_CALLBACK_URL:-http://localhost:8080/}
+
+cd /lh/dashboard
+
+pnpm run build
+pnpm start
