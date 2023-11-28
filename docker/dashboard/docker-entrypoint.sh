13321d3748429d97cd97f57de964a84d826e1de5
Saúl Piña <saul@littlehorse.io>
2023-11-28 10:39:35 -0500

    Add env variables to docker
    Co-authored-by: Nelson Jumbo <nelson@littlehorse.io>

--- docker/dashboard/docker-entrypoint.sh
+++ docker/dashboard/docker-entrypoint.sh
@@ -4,6 +4,20 @@ set -e
 
 if [ "$1" = 'dashboard' ]; then
     shift
+
+    if [ -z "$API_URL" ]; then
+      echo "Provide the API_URL env variable"
+      exit 1
+    fi
+
+    if [ -z "$AUTHENTICATION_ENABLED" ]; then
+      echo "Provide the AUTHENTICATION_ENABLED env variable"
+      exit 1
+    fi
+
+    export AUTH_SECRET=${AUTH_SECRET:-$(uuidgen)}
+    export NEXTAUTH_URL=${AUTH_CALLBACK_URL:-http://localhost:8080/}
+
     pnpm run build
     pnpm start
 fi
