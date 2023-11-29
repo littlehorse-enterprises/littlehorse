afbcf3e7adb9f841e81d03777a0ade6ecc87cb5c
Nelson Jumbo <nelson@littlehorse.io>
2023-11-29 15:52:50 -0500

    LH-169 - Nelson - Fixed layouted graph logic to work when the auth enabled toggle is OFF

--- dashboard/apps/web/pages/api/visualization/workflowLayoutedGraph.ts
+++ dashboard/apps/web/pages/api/visualization/workflowLayoutedGraph.ts
@@ -8,11 +8,11 @@ import EdgeLabelExtractor from '../../../app/wfspec/[id]/[version]/components/vi
 import type { WfSpec } from '../../../littlehorse-public-api/wf_spec'
 import LHClient from '../LHClient'
 import { getServerSession } from 'next-auth/next'
+import type { SessionWithJWTExpireTime } from '../auth/[...nextauth]'
 import { authOptions } from '../auth/[...nextauth]'
 import { makeGrpcCall } from '../grpcMethodCallHandler'
 import { WfSpecId } from '../../../littlehorse-public-api/object_id'
 import { constants } from 'http2'
-
 
 const unauthorizedResponseContent = {
     status: constants.HTTP_STATUS_UNAUTHORIZED,
@@ -22,41 +22,48 @@ const unauthorizedResponseContent = {
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     if (req.method === 'POST') {
-        const session = await getServerSession(req, res, authOptions)
+        let session: SessionWithJWTExpireTime | null
+        let client: Client<LHPublicApiDefinition>
 
-        if (session) {
-            const client: Client<LHPublicApiDefinition> = LHClient.getInstance(session.accessToken)
-            const parsedRequestBody = JSON.parse(req.body)
-
-            const wfSpec: WfSpec = await makeGrpcCall('getWfSpec', req, res, WfSpecId.fromJSON({
-                name: parsedRequestBody.wfSpecName,
-                majorVersion: parsedRequestBody.version,
-                revision: 0 // TODO: OSS - bring this from the UI
-            }))
-
-            const elkInstance = new ElkConstructor()
-            let layoutedGraph: ReactFlowGraph
-
-            if (parsedRequestBody.isWfSpecVisualization) {
-                layoutedGraph = await new GraphLayouter(
-                    elkInstance,
-                    EdgeLabelExtractor.extract).getLayoutedGraph(wfSpec, parsedRequestBody.wfSpecName, parsedRequestBody.threadSpec)
-            } else {
-                layoutedGraph = await new GraphLayouter(
-                    elkInstance,
-                    EdgeLabelExtractor.extract,
-                    client).getLayoutedGraphForWfRun(wfSpec,
-                    parsedRequestBody.wfSpecName,
-                    parsedRequestBody.wfRunId,
-                    parsedRequestBody.threadRunNumber,
-                    parsedRequestBody.threadSpec
-                )
+        if (__AUTHENTICATION_ENABLED__) {
+            session = await getServerSession(req, res, authOptions)
+            if (!session) {
+                res.status(constants.HTTP_STATUS_UNAUTHORIZED)
+                    .json(unauthorizedResponseContent)
+                return
             }
-
-            res.send(layoutedGraph)
+            client = LHClient.getInstance(session.accessToken)
         } else {
-            res.status(constants.HTTP_STATUS_UNAUTHORIZED)
-                .json(unauthorizedResponseContent)
+            client = LHClient.getInstance()
         }
+
+        const parsedRequestBody = JSON.parse(req.body)
+
+        const wfSpec: WfSpec = await makeGrpcCall('getWfSpec', req, res, WfSpecId.fromJSON({
+            name: parsedRequestBody.wfSpecName,
+            majorVersion: parsedRequestBody.version,
+            revision: 0 // TODO: OSS - bring this from the UI
+        }))
+
+        const elkInstance = new ElkConstructor()
+        let layoutedGraph: ReactFlowGraph
+
+        if (parsedRequestBody.isWfSpecVisualization) {
+            layoutedGraph = await new GraphLayouter(
+                elkInstance,
+                EdgeLabelExtractor.extract).getLayoutedGraph(wfSpec, parsedRequestBody.wfSpecName, parsedRequestBody.threadSpec)
+        } else {
+            layoutedGraph = await new GraphLayouter(
+                elkInstance,
+                EdgeLabelExtractor.extract,
+                client).getLayoutedGraphForWfRun(wfSpec,
+                parsedRequestBody.wfSpecName,
+                parsedRequestBody.wfRunId,
+                parsedRequestBody.threadRunNumber,
+                parsedRequestBody.threadSpec
+            )
+        }
+
+        res.send(layoutedGraph)
     }
 }
