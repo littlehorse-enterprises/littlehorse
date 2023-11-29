edfca9a58c570a1e0c1d99b5d515bd0a89010d23
Nelson Jumbo <nelson@littlehorse.io>
2023-11-29 14:52:25 -0500

    LH-169 - Nelson - When getting wfSpecs in the BFF we build a version string which is the combination of both majorVersion and revision

--- dashboard/apps/web/__tests__/apps/pages/api/search/wfSpec.test.ts
+++ dashboard/apps/web/__tests__/apps/pages/api/search/wfSpec.test.ts
@@ -1,12 +1,17 @@
-import type { RequestMethod } from 'node-mocks-http'
 import { createMocks } from 'node-mocks-http'
 import handler from '../../../../../pages/api/search/wfSpec'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import * as grpcCallHandler from '../../../../../pages/api/grpcMethodCallHandler'
 import type { SearchWfSpecRequest } from '../../../../../littlehorse-public-api/service'
-jest.mock('../../../../../pages/api/grpcMethodCallHandler')
+import LHClient from '../../../../../pages/api/LHClient'
+import type { WfSpecIdList } from '../../../../../littlehorse-public-api/service'
+jest.mock('../../../../../pages/api/LHClient')
 
 describe('wfSpec API', () => {
+    beforeEach(() => {
+        __AUTHENTICATION_ENABLED__ = false
+    })
+
     afterEach(() => {
         jest.clearAllMocks()
     })
@@ -14,11 +19,23 @@ describe('wfSpec API', () => {
     it('should perform a grpc request to get the wfSpec sending the right request body', async () => {
         const { req, res }: { req: NextApiRequest; res: NextApiResponse } = createMocks({ method: 'POST' })
 
-        req.body = JSON.stringify({ prefix: 'any_prefix' })
+        req.body = JSON.stringify({ prefix: 'any_prefix' });
+
+        (LHClient.getInstance as unknown as jest.Mock).mockImplementation(() => ({
+            searchWfSpec: (_: SearchWfSpecRequest): Promise<WfSpecIdList> => {
+                return Promise.resolve({
+                    results: [ { name: 'ANY_SPEC', majorVersion: 1, revision: 2 } ]
+                } as WfSpecIdList)
+            }
+        }))
+
+        const mockedJson = jest.fn()
+        res.json = mockedJson
 
         await handler(req, res)
 
-        expect(grpcCallHandler.handleGrpcCallWithNext)
-            .toHaveBeenCalledWith('searchWfSpec', req, res, { prefix: 'any_prefix' } as SearchWfSpecRequest)
+        expect(mockedJson).toHaveBeenCalledWith({
+            results: [ { name: 'ANY_SPEC', majorVersion: 1, revision: 2, version: '1.2' } ]
+        })
     })
 })
--- dashboard/apps/web/pages/api/search/wfSpec.ts
+++ dashboard/apps/web/pages/api/search/wfSpec.ts
@@ -1,9 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { handleGrpcCallWithNext } from '../grpcMethodCallHandler'
+import { makeGrpcCall } from '../grpcMethodCallHandler'
+import type { WfSpecIdList } from '../../../littlehorse-public-api/service'
 import { SearchWfSpecRequest } from '../../../littlehorse-public-api/service'
+import type { WfSpecId } from '../../../littlehorse-public-api/object_id'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        await handleGrpcCallWithNext('searchWfSpec', req, res, SearchWfSpecRequest.fromJSON(JSON.parse(req.body)))
+        const response = await makeGrpcCall('searchWfSpec', req, res, SearchWfSpecRequest.fromJSON(JSON.parse(req.body)))
+
+        const resultsWithFormattedVersion = response.results.map((result: WfSpecId)  => ({
+            ...result, version: `${result.majorVersion}.${result.revision}`
+        }))
+
+        res.json({ ...response, results: resultsWithFormattedVersion } as WfSpecIdList)
     }
 }
