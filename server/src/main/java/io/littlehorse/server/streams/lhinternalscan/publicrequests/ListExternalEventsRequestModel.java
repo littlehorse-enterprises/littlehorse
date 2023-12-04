9200c319b6ad7f947b14291c3ea7da0409a2ee1a
Eduwer Camacaro <eduwerc@gmail.com>
2023-12-04 14:33:10 -0500

    Refactor Object id scan

--- server/src/main/java/io/littlehorse/server/streams/lhinternalscan/publicrequests/ListExternalEventsRequestModel.java
+++ server/src/main/java/io/littlehorse/server/streams/lhinternalscan/publicrequests/ListExternalEventsRequestModel.java
@@ -53,7 +53,7 @@ public class ListExternalEventsRequestModel
 
     @Override
     public SearchScanBoundaryStrategy getScanBoundary(String searchAttributeString) {
-        return new ObjectIdScanBoundaryStrategy(wfRunId);
+        return ObjectIdScanBoundaryStrategy.from(wfRunId, GetableClassEnum.EXTERNAL_EVENT);
     }
 
     @Override
