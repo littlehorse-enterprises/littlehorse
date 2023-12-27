af4c8209741cbeb1c57e7c1414b7067baf0bd0b1
Eduwer Camacaro <eduwerc@gmail.com>
2023-12-27 16:04:36 -0500

    Add unit test

--- server/src/test/java/io/littlehorse/server/streams/topology/core/processors/AggregateWfMetricsRepartitionCommandTest.java
+++ server/src/test/java/io/littlehorse/server/streams/topology/core/processors/AggregateWfMetricsRepartitionCommandTest.java
@@ -87,6 +87,7 @@ public class AggregateWfMetricsRepartitionCommandTest {
         firstUpdate.totalCompleted = 1L;
         firstUpdate.totalErrored = 2L;
         firstUpdate.totalStarted = 3L;
+        firstUpdate.startToCompleteTotal = 7;
         commandProcessor.init(mockProcessorContext);
         RepartitionCommand firstMetricUpdate = new RepartitionCommand(
                 new AggregateWfMetricsModel(wfSpecId, List.of(firstUpdate), tenantId), new Date(), commandId);
@@ -97,6 +98,7 @@ public class AggregateWfMetricsRepartitionCommandTest {
         secondUpdate.totalCompleted = 1L;
         secondUpdate.totalErrored = 2L;
         secondUpdate.totalStarted = 3L;
+        secondUpdate.startToCompleteTotal = 30;
         RepartitionCommand secondMetricUpdate = new RepartitionCommand(
                 new AggregateWfMetricsModel(wfSpecId, List.of(secondUpdate), tenantId), new Date(), commandId);
         commandProcessor.process(new Record<>(commandId, secondMetricUpdate, 0L, metadata));
@@ -108,6 +110,7 @@ public class AggregateWfMetricsRepartitionCommandTest {
         assertThat(wfSpecMetrics.totalCompleted).isEqualTo(2);
         assertThat(wfSpecMetrics.totalErrored).isEqualTo(4);
         assertThat(wfSpecMetrics.totalStarted).isEqualTo(6);
+        assertThat(wfSpecMetrics.startToCompleteAvg).isEqualTo(15);
     }
 
     @Test
