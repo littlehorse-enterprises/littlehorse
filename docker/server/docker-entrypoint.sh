43548a8fe79ece4ae1f74cb5e1ae7a6fda65e42f
Colt McNealy <colt@littlehorse.io>
2024-01-28 12:58:10 -0800

    Expose More Performance-Related Configurations

--- docker/server/docker-entrypoint.sh
+++ docker/server/docker-entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Uses jemalloc, which is much better for RocksDB memory management
+export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libjemalloc.so"
+
 if [ "$1" = 'server' ]; then
     shift
     exec java -Dlog4j2.configurationFile=/lh/log4j2.properties -jar /lh/server.jar "$@"
