707c571844c2ee6b7ca35e0bfcbf3d7f74ff47f0
Colt McNealy <colt@littlehorse.io>
2024-01-28 13:16:13 -0800

    revert changes related to jemalloc

--- docker/server/docker-entrypoint.sh
+++ docker/server/docker-entrypoint.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# Uses jemalloc, which is much better for RocksDB memory management
-export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libjemalloc.so"
-
 if [ "$1" = 'server' ]; then
     shift
     exec java -Dlog4j2.configurationFile=/lh/log4j2.properties -jar /lh/server.jar "$@"
