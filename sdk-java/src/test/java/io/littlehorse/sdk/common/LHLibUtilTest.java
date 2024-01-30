5d5f6a7545c1bab7646f7bc194106d10f49b8d9c
Colt McNealy <colt@littlehorse.io>
2024-01-30 10:50:43 -0800

    adds unit test

---
+++ sdk-java/src/test/java/io/littlehorse/sdk/common/LHLibUtilTest.java
@@ -0,0 +1,30 @@
+package io.littlehorse.sdk.common;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.littlehorse.sdk.common.proto.TaskRunId;
+import io.littlehorse.sdk.common.proto.WfRunId;
+
+public class LHLibUtilTest {
+    
+    @Test
+    void shouldToStringWfRunIdWhenNoParent() {
+        String idStr = "asdfasdf";
+        WfRunId id = WfRunId.newBuilder().setId(idStr).build();
+        Assertions.assertThat(LHLibUtil.wfRunIdToString(id)).isEqualTo(idStr);
+
+        String taskGuid = "task-guid";
+        TaskRunId taskRunid = TaskRunId.newBuilder().setWfRunId(id).setTaskGuid(taskGuid).build();
+        Assertions.assertThat(LHLibUtil.taskRunIdToString(taskRunid)).isEqualTo(idStr + "/" + taskGuid);
+    }
+
+    @Test
+    void shouldIncludeParentWfRunidOnToString() {
+        String parentId = "parent";
+        String childId = "child";
+        WfRunId id = WfRunId.newBuilder().setId(childId).setParentWfRunId(WfRunId.newBuilder().setId(parentId)).build();
+
+        Assertions.assertThat(LHLibUtil.wfRunIdToString(id)).isEqualTo(parentId + "_" + childId);
+    }
+}
