561f0defd0e68f511b677b3699b66ca1ec8b09b0
Saúl Piña <saul@littlehorse.io>
2024-01-30 17:41:22 -0500

    chore: some scripts improvements

--- local-dev/do-canary.sh
+++ local-dev/do-canary.sh
@@ -7,5 +7,5 @@ WORK_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
 
 cd "$WORK_DIR"
 
-./gradlew canary:clean canary:build -x test -x spotlessJavaCheck
-java -jar canary/build/libs/canary-*-all.jar canary/canary.properties
+./gradlew canary:installDist -x shadowJar -x test
+./canary/build/install/canary/bin/canary canary/canary.properties
