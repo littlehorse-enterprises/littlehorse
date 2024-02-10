05a17941fc378466184bf96ee9f7d9b8d0e527e6
Colt McNealy <colt@littlehorse.io>
2024-02-10 12:26:45 -0800

    fix javadoc

--- sdk-java/src/main/java/io/littlehorse/sdk/wfsdk/WaitForThreadsNodeOutput.java
+++ sdk-java/src/main/java/io/littlehorse/sdk/wfsdk/WaitForThreadsNodeOutput.java
@@ -9,10 +9,7 @@ import io.littlehorse.sdk.common.proto.LHErrorType;
  *
  * <p>When using this interface, you can set a policy that determines how the
  * node should handle waiting for threads' parallel executions:
- * {@link WaitForThreadsPolicy#STOP_ON_FAILURE}: Stops the entire execution for parent and children threads
- * when a single node fails
  *
- * @see WaitForThreadsPolicy
  * @author Eduwer Camacaro
  */
 public interface WaitForThreadsNodeOutput extends NodeOutput {
