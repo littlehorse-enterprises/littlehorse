0aef48bc9468b1ab40b911523218cae0d07af00b
Colt McNealy <colt@littlehorse.io>
2024-02-14 22:05:01 -0800

    Adds a test that verifies that we now store failure handler id on the Failure.
    Also this test fails because the NodeRun is not moved from HALTED back to EXCEPTION. We need to 'unhalt' the NodeRuns.

--- server/src/test/java/e2e/InterruptOnExternalEventTest.java
+++ server/src/test/java/e2e/InterruptOnExternalEventTest.java
@@ -1,23 +1,24 @@
 package e2e;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import io.littlehorse.sdk.common.proto.LHStatus;
 import io.littlehorse.sdk.wfsdk.Workflow;
 import io.littlehorse.test.LHTest;
 import io.littlehorse.test.LHWorkflow;
 import io.littlehorse.test.WorkflowVerifier;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 /*
- * Tests edge cases in the halting/unhalting logic of threadruns.
+ * Tests edge cases in the halting/unhalting logic of threadruns and noderuns.
  */
 @LHTest(
         externalEventNames = {
-            InterruptAndExternalEventTest.PARENT_EVENT,
-            InterruptAndExternalEventTest.INTERRUPT_TRIGGER,
-            InterruptAndExternalEventTest.CHILD_EVENT
+            InterruptOnExternalEventTest.PARENT_EVENT,
+            InterruptOnExternalEventTest.INTERRUPT_TRIGGER,
+            InterruptOnExternalEventTest.CHILD_EVENT
         })
-public class InterruptAndExternalEventTest {
+public class InterruptOnExternalEventTest {
 
     public static final String PARENT_EVENT = "iaeet-parent";
     public static final String INTERRUPT_TRIGGER = "iaeet-interupt-trigger";
@@ -53,6 +54,9 @@ public class InterruptAndExternalEventTest {
                     Assertions.assertThat(nodeRun.getStatus()).isEqualTo(LHStatus.HALTED);
                 })
                 .thenSendExternalEventJsonContent(CHILD_EVENT, null)
+                .thenVerifyWfRun(wfRun -> {
+                    Assertions.assertThat(wfRun.getThreadRuns(1).getStatus()).isEqualTo(LHStatus.COMPLETED);
+                })
                 .thenVerifyNodeRun(0, 1, nodeRun -> {
                     Assertions.assertThat(nodeRun.getStatus()).isEqualTo(LHStatus.RUNNING);
                 })
@@ -63,6 +67,46 @@ public class InterruptAndExternalEventTest {
                 .start();
     }
 
+    @Test
+    void parentShouldContinueAfterInterruptFinishesWhenEventArrivesWhileHalted() {
+        verifier.prepareRun(interruptOnExternalEvent)
+                .thenSendExternalEventJsonContent(INTERRUPT_TRIGGER, null)
+                .thenVerifyWfRun(wfRun -> {
+                    Assertions.assertThat(wfRun.getStatus()).isEqualTo(LHStatus.RUNNING);
+                    Assertions.assertThat(wfRun.getThreadRunsCount()).isEqualTo(2);
+                    Assertions.assertThat(wfRun.getThreadRuns(0).getStatus()).isEqualTo(LHStatus.HALTED);
+                    Assertions.assertThat(wfRun.getThreadRuns(1).getStatus()).isEqualTo(LHStatus.RUNNING);
+                })
+                .thenVerifyNodeRun(0, 1, nodeRun -> {
+                    Assertions.assertThat(nodeRun.getStatus()).isEqualTo(LHStatus.HALTED);
+                })
+                .thenSendExternalEventJsonContent(PARENT_EVENT, null)
+                .thenVerifyNodeRun(0, 1, nodeRun -> {
+                    Assertions.assertThat(nodeRun.getStatus()).isEqualTo(LHStatus.HALTED);
+                })
+                .thenVerifyWfRun(wfRun -> {
+                    Assertions.assertThat(wfRun.getStatus()).isEqualTo(LHStatus.RUNNING);
+                    Assertions.assertThat(wfRun.getThreadRuns(0).getStatus()).isEqualTo(LHStatus.HALTED);
+                })
+                .thenSendExternalEventJsonContent(CHILD_EVENT, null)
+                .thenVerifyWfRun(wfRun -> {
+                    Assertions.assertThat(wfRun.getStatus()).isEqualTo(LHStatus.COMPLETED);
+                })
+                .thenVerifyNodeRun(0, 1, nodeRun -> {
+                    Assertions.assertThat(nodeRun.getStatus()).isEqualTo(LHStatus.COMPLETED);
+                })
+                .start();
+    }
+
+    /*
+     * Main Thread: just waits for an external event. There is an interrupt trigger.
+     *
+     * Interrupt Thread: waits for a different external event.
+     *
+     * This allows us to test edge cases by "waking up" the interrupt and the main
+     * thread at different times. Because we control the advancing of the threads (by
+     * posting external events), we can inspect noderuns in detail at specific times.
+     */
     @LHWorkflow("interrupt-on-external-event")
     public Workflow getInterruptOnExternalEventWf() {
         return Workflow.newWorkflow("interrupt-on-external-event", parent -> {
