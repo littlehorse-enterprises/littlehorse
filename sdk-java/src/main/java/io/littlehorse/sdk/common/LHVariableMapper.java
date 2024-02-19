f347c9aec6cc04c14cc2dcbbb1e68b47887ae603
Eduwer Camacaro <eduwerc@gmail.com>
2024-02-19 14:55:03 -0500

    Convert variable value to java object

--- sdk-java/src/main/java/io/littlehorse/sdk/common/LHVariableMapper.java
+++ sdk-java/src/main/java/io/littlehorse/sdk/common/LHVariableMapper.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import io.littlehorse.sdk.common.proto.VariableValue;
-
+import java.util.Collection;
 import java.util.List;
 
 public final class LHVariableMapper {
@@ -16,34 +16,82 @@ public final class LHVariableMapper {
     }
 
     public static int asInt(VariableValue var) {
+        enforceType(var.getValueCase(), Integer.class);
         return (int) var.getInt();
     }
 
-    public static long asLong(VariableValue var){
+    public static long asLong(VariableValue var) {
+        enforceType(var.getValueCase(), Long.class);
         return var.getInt();
     }
 
     public static double asDouble(VariableValue var) {
+        enforceType(var.getValueCase(), Double.class);
         return var.getDouble();
     }
 
     public static boolean asBoolean(VariableValue var) {
+        enforceType(var.getValueCase(), Boolean.class);
         return var.getBool();
     }
+
     public static byte[] asBytes(VariableValue var) {
+        enforceType(var.getValueCase(), Byte.class);
         return var.getBytes().toByteArray();
     }
 
     public static String asString(VariableValue var) {
+        enforceType(var.getValueCase(), String.class);
         return var.getStr();
     }
 
-    public static <T> List<T> asList(VariableValue var, Class<T> clazz) {
+    public static <T> T as(VariableValue var, Class<T> clazz) {
         try {
-            return MAPPER.readValue(var.getJsonArr(), TypeFactory.defaultInstance().constructCollectionType(List.class, clazz));
-        } catch(JsonProcessingException exn) {
-            throw new RuntimeException(exn);
+            return MAPPER.readValue(var.getJsonArr(), clazz);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 
+    private static void enforceType(VariableValue.ValueCase valueCase, Class<?> targetType) {
+        List<Class<?>> compatibleTypes;
+        switch (valueCase) {
+            case INT:
+                compatibleTypes = List.of(Integer.class, Long.class);
+                break;
+            case STR:
+                compatibleTypes = List.of(String.class);
+                break;
+            case BOOL:
+                compatibleTypes = List.of(Boolean.class);
+                break;
+            case BYTES:
+                compatibleTypes = List.of(Byte.class);
+                break;
+            case DOUBLE:
+                compatibleTypes = List.of(Double.class);
+                break;
+            case JSON_ARR:
+                compatibleTypes = List.of(Collection.class);
+                break;
+            case JSON_OBJ:
+            case VALUE_NOT_SET:
+            default:
+                compatibleTypes = List.of();
+        }
+        if (!compatibleTypes.contains(targetType)) {
+            throw new IllegalArgumentException(String.format(
+                    "Not possible to convert variable type %s to %s", valueCase.name(), targetType.getSimpleName()));
+        }
+    }
+
+    public static <T> Collection<T> asList(VariableValue var, Class<T> clazz) {
+        try {
+            enforceType(var.getValueCase(), Collection.class);
+            return MAPPER.readValue(
+                    var.getJsonArr(), TypeFactory.defaultInstance().constructCollectionType(Collection.class, clazz));
+        } catch (JsonProcessingException exn) {
+            throw new RuntimeException(exn);
+        }
+    }
 }
--- sdk-java/src/test/java/io/littlehorse/sdk/common/LHVariableMapperTest.java
+++ sdk-java/src/test/java/io/littlehorse/sdk/common/LHVariableMapperTest.java
@@ -1,38 +1,38 @@
 package io.littlehorse.sdk.common;
-import com.google.protobuf.ByteString;
-import io.littlehorse.sdk.common.proto.VariableValue;
-import static org.assertj.core.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-import java.util.Objects;
 
 import static io.littlehorse.sdk.common.LHVariableMapper.*;
+import static org.assertj.core.api.Assertions.*;
+
+import com.google.protobuf.ByteString;
+import io.littlehorse.sdk.common.proto.VariableValue;
+import java.util.Collection;
+import java.util.Objects;
+import org.junit.jupiter.api.Test;
+
 public class LHVariableMapperTest {
 
     @Test
-    void shouldConvertJsonArrToList(){
-        String jsonArray =
-                "[" +
-                        "  {" +
-                        "    \"name\": \"John Doe\"," +
-                        "    \"age\": 30," +
-                        "    \"city\": \"New York\"" +
-                        "  }," +
-                        "  {" +
-                        "    \"name\": \"Jane Smith\"," +
-                        "    \"age\": 25," +
-                        "    \"city\": \"Los Angeles\"" +
-                        "  }," +
-                        "  {" +
-                        "    \"name\": \"Bob Johnson\"," +
-                        "    \"age\": 35," +
-                        "    \"city\": \"Chicago\"" +
-                        "  }" +
-                        "]";
+    void shouldConvertJsonArrToList() {
+        String jsonArray = "[" + "  {"
+                + "    \"name\": \"John Doe\","
+                + "    \"age\": 30,"
+                + "    \"city\": \"New York\""
+                + "  },"
+                + "  {"
+                + "    \"name\": \"Jane Smith\","
+                + "    \"age\": 25,"
+                + "    \"city\": \"Los Angeles\""
+                + "  },"
+                + "  {"
+                + "    \"name\": \"Bob Johnson\","
+                + "    \"age\": 35,"
+                + "    \"city\": \"Chicago\""
+                + "  }"
+                + "]";
         VariableValue jsonArr = VariableValue.newBuilder().setJsonArr(jsonArray).build();
-        List<Person> listOfPeople = asList(jsonArr, Person.class);
-        assertThat(listOfPeople).hasSize(3)
+        Collection<Person> listOfPeople = asList(jsonArr, Person.class);
+        assertThat(listOfPeople)
+                .hasSize(3)
                 .allMatch(Objects::nonNull)
                 .allMatch(person -> Objects.nonNull(person.name))
                 .allMatch(person -> Objects.nonNull(person.city))
@@ -55,27 +55,51 @@ public class LHVariableMapperTest {
     }
 
     @Test
-    void shouldExtractBooleanValue() {
+    void shouldConvertBooleanValue() {
         assertThat(asBoolean(VariableValue.newBuilder().setBool(true).build())).isEqualTo(true);
     }
 
     @Test
-    void shouldExtractBytesValue() {
+    void shouldConvertBytesValue() {
         ByteString bytes = ByteString.copyFrom("pedro".getBytes());
-        assertThat(asBytes(VariableValue.newBuilder().setBytes(bytes).build()))
-                .isEqualTo("pedro".getBytes());
+        assertThat(asBytes(VariableValue.newBuilder().setBytes(bytes).build())).isEqualTo("pedro".getBytes());
     }
 
     @Test
-    void shouldExtractStringValue() {
+    void shouldConvertStringValue() {
         assertThat(asString(VariableValue.newBuilder().setStr("pedro").build())).isEqualTo("pedro");
     }
 
-
+    @Test
+    void shouldVerifyVariableTypeBeforeConvertingTheValue() {
+        VariableValue stringVal = VariableValue.newBuilder().setStr("str").build();
+        VariableValue boolVal = VariableValue.newBuilder().setBool(true).build();
+        assertThatThrownBy(() -> asString(boolVal))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Not possible to convert variable type BOOL to String");
+        assertThatThrownBy(() -> asInt(boolVal))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Not possible to convert variable type BOOL to Integer");
+        assertThatThrownBy(() -> asLong(boolVal))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Not possible to convert variable type BOOL to Long");
+        assertThatThrownBy(() -> asDouble(boolVal))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Not possible to convert variable type BOOL to Double");
+        assertThatThrownBy(() -> asBoolean(stringVal))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Not possible to convert variable type STR to Boolean");
+        assertThatThrownBy(() -> asBytes(boolVal))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Not possible to convert variable type BOOL to Byte");
+        assertThatThrownBy(() -> asList(boolVal, Person.class))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Not possible to convert variable type BOOL to Collection");
+        assertThatThrownBy(() -> as(boolVal, Person.class)).isInstanceOf(RuntimeException.class);
+    }
 
     public static class Person {
         public String name, city;
         public int age;
     }
-
 }
