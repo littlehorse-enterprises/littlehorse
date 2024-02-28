07b2c5b0da4170d898960b7e53cbeded5a246809
Colt McNealy <colt@littlehorse.io>
2024-02-28 11:25:14 -0500

    EDU PLEASE REVIEW

--- server/src/main/java/io/littlehorse/server/streams/util/AsyncWaiters.java
+++ server/src/main/java/io/littlehorse/server/streams/util/AsyncWaiters.java
@@ -6,86 +6,49 @@ import io.littlehorse.common.proto.WaitForCommandResponse;
 import io.littlehorse.sdk.common.proto.WorkflowEvent;
 import io.littlehorse.sdk.common.proto.WorkflowEventId;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import lombok.extern.slf4j.Slf4j;
+import java.util.concurrent.ConcurrentHashMap;
 import org.apache.commons.lang3.NotImplementedException;
 
-@Slf4j
 public class AsyncWaiters {
 
-    private Lock lock;
-    private LinkedHashMap<String, AsyncWaiter> waiters;
+    // private Lock lock;
+    private ConcurrentHashMap<String, AsyncWaiter<?>> waiters;
 
     private static final long MAX_WAITER_AGE = 1000 * 60;
 
     public AsyncWaiters() {
-        lock = new ReentrantLock();
-        waiters = new LinkedHashMap<>();
+        // lock = new ReentrantLock();
+        waiters = new ConcurrentHashMap<>();
     }
 
+    @SuppressWarnings("unchecked")
     public void registerObserverWaitingForCommand(String commandId, StreamObserver<WaitForCommandResponse> observer) {
-        try {
-            lock.lock();
-            AsyncWaiter waiter = waiters.get(commandId);
-            if (waiter == null) {
-                waiters.put(commandId, new AsyncWaiter(commandId, observer));
-            } else {
-                if (waiter.getObserver() != null) {
-                    // this means the request has come in again...
-                    waiter.setObserver(observer);
-                    log.warn("Got a retry request before the event was processed");
-                } else {
-                    waiter.setObserver(observer);
-                    waiter.onMatched();
-                    waiters.remove(commandId);
-                }
-            }
-        } finally {
-            lock.unlock();
+        AsyncWaiter<WaitForCommandResponse> tmp = new AsyncWaiter<>(commandId);
+        AsyncWaiter<WaitForCommandResponse> waiter = (AsyncWaiter<WaitForCommandResponse>) waiters.putIfAbsent(commandId, tmp);
+        if (waiter == null) waiter = tmp;
+        if (waiter.setObserverAndMaybeComplete(observer)) {
+            waiters.remove(commandId);
         }
     }
 
+    @SuppressWarnings("unchecked")
     public void markCommandFailed(String commandId, Exception exception) {
-        // This happens when there is an unexpected error in the processing.
-        try {
-            lock.lock();
-            AsyncWaiter waiter = waiters.get(commandId);
-            if (waiter == null) {
-                waiters.put(commandId, new AsyncWaiter(commandId, exception));
-            } else {
-                waiter.setCaughtException(exception);
-                waiter.onMatched();
-                waiters.remove(commandId);
-            }
-        } finally {
-            lock.unlock();
+        AsyncWaiter<WaitForCommandResponse> tmp = new AsyncWaiter<>(commandId);
+        AsyncWaiter<WaitForCommandResponse> waiter = (AsyncWaiter<WaitForCommandResponse>) waiters.putIfAbsent(commandId, tmp);
+        if (waiter == null) waiter = tmp;
+        if (waiter.setExceptionAndMaybeComplete(exception)) {
+            waiters.remove(commandId);
         }
     }
 
+    @SuppressWarnings("unchecked")
     public void registerCommandProcessed(String commandId, WaitForCommandResponse response) {
-        try {
-            lock.lock();
-            AsyncWaiter waiter = waiters.get(commandId);
-            if (waiter == null) {
-                waiters.put(commandId, new AsyncWaiter(commandId, response));
-            } else {
-                if (waiter.getResponse() != null) {
-                    // this means that a duplicate Kafka event came through, but
-                    // they're idempotent, so we just take the first response.
-
-                    // Basically, just ignore this.
-                    log.warn("Just ignoring the second coming of this command id.");
-                } else {
-                    waiter.setResponse(response);
-                    waiter.onMatched();
-                    waiters.remove(commandId);
-                }
-            }
-        } finally {
-            lock.unlock();
+        AsyncWaiter<WaitForCommandResponse> tmp = new AsyncWaiter<>(commandId);
+        AsyncWaiter<WaitForCommandResponse> waiter = (AsyncWaiter<WaitForCommandResponse>) waiters.putIfAbsent(commandId, tmp);
+        if (waiter == null) waiter = tmp;
+        if (waiter.setResponseAndMaybeComplete(response)) {
+            waiters.remove(commandId);
         }
     }
 
@@ -98,26 +61,21 @@ public class AsyncWaiters {
     }
 
     public void cleanupOldWaiters() {
-        try {
-            lock.lock();
-            Iterator<Map.Entry<String, AsyncWaiter>> iter = waiters.entrySet().iterator();
-            long now = System.currentTimeMillis();
-            while (iter.hasNext()) {
-                Map.Entry<String, AsyncWaiter> pair = iter.next();
-                long age = now - pair.getValue().getArrivalTime().getTime();
-                if (age < MAX_WAITER_AGE) {
-                    break;
-                }
-                AsyncWaiter waiter = pair.getValue();
-                if (waiter.getObserver() != null) {
-                    waiter.getObserver()
-                            .onError(new RuntimeException(
-                                    "Request not processed on this worker, likely due to" + " rebalance"));
-                }
-                iter.remove();
+        Iterator<Map.Entry<String, AsyncWaiter<?>>> iter = waiters.entrySet().iterator();
+        long now = System.currentTimeMillis();
+        while (iter.hasNext()) {
+            Map.Entry<String, AsyncWaiter<?>> pair = iter.next();
+            long age = now - pair.getValue().getArrivalTime().getTime();
+            if (age < MAX_WAITER_AGE) {
+                break;
             }
-        } finally {
-            lock.unlock();
+            AsyncWaiter<?> waiter = pair.getValue();
+            if (waiter.getObserver() != null) {
+                waiter.getObserver()
+                        .onError(new RuntimeException(
+                                "Request not processed on this worker, likely due to" + " rebalance"));
+            }
+            iter.remove();
         }
     }
 }
