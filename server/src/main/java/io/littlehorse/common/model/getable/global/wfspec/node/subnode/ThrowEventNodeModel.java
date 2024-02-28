ad21c2fff8c6c51cc858727b51b1a42d5e53918e
Eduwer Camacaro <eduwerc@gmail.com>
2024-02-28 09:05:09 -0500

    E2e

--- server/src/main/java/io/littlehorse/common/model/getable/global/wfspec/node/subnode/ThrowEventNodeModel.java
+++ server/src/main/java/io/littlehorse/common/model/getable/global/wfspec/node/subnode/ThrowEventNodeModel.java
@@ -1,32 +1,47 @@
 package io.littlehorse.common.model.getable.global.wfspec.node.subnode;
 
-import com.google.protobuf.GeneratedMessageV3;
 import com.google.protobuf.Message;
+import io.littlehorse.common.LHSerializable;
 import io.littlehorse.common.exceptions.LHApiException;
 import io.littlehorse.common.model.getable.core.wfrun.SubNodeRun;
+import io.littlehorse.common.model.getable.core.wfrun.subnoderun.WorkflowEventRunModel;
 import io.littlehorse.common.model.getable.global.wfspec.node.SubNode;
+import io.littlehorse.common.model.getable.global.wfspec.variable.VariableAssignmentModel;
+import io.littlehorse.common.model.getable.objectId.WorkflowEventDefIdModel;
 import io.littlehorse.sdk.common.exception.LHSerdeError;
 import io.littlehorse.sdk.common.proto.ThrowEventNode;
 import io.littlehorse.server.streams.topology.core.ExecutionContext;
+import io.littlehorse.server.streams.topology.core.ProcessorExecutionContext;
 import java.util.Date;
 
 public class ThrowEventNodeModel extends SubNode<ThrowEventNode> {
+    private WorkflowEventDefIdModel workflowEventDefId;
+    private VariableAssignmentModel variableAssignment;
+    private ProcessorExecutionContext processorContext;
+
     @Override
-    public ThrowEventNode.Builder toProto() {
-        return null;
+    public void initFrom(Message proto, ExecutionContext context) throws LHSerdeError {
+        ThrowEventNode p = (ThrowEventNode) proto;
+        this.workflowEventDefId = LHSerializable.fromProto(p.getEventDefId(), WorkflowEventDefIdModel.class, context);
+        this.variableAssignment = LHSerializable.fromProto(p.getContent(), VariableAssignmentModel.class, context);
+        this.processorContext = context.castOnSupport(ProcessorExecutionContext.class);
     }
 
     @Override
-    public void initFrom(Message proto, ExecutionContext context) throws LHSerdeError {}
+    public ThrowEventNode.Builder toProto() {
+        return ThrowEventNode.newBuilder()
+                .setEventDefId(workflowEventDefId.toProto())
+                .setContent(variableAssignment.toProto());
+    }
 
     @Override
-    public Class<? extends GeneratedMessageV3> getProtoBaseClass() {
-        return null;
+    public Class<ThrowEventNode> getProtoBaseClass() {
+        return ThrowEventNode.class;
     }
 
     @Override
     public SubNodeRun<?> createSubNodeRun(Date time) {
-        return null;
+        return new WorkflowEventRunModel(workflowEventDefId, processorContext);
     }
 
     @Override
