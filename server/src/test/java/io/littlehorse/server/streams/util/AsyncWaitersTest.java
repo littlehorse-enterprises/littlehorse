2f88d2d8e639c01db9e5b5ac3fc5473e00711976
Eduwer Camacaro <eduwerc@gmail.com>
2024-03-16 13:07:45 -0400

    Concurrency model for async waiters (#719)
    * Concurrency model for async waiters
    ---------
    
    Co-authored-by: Colt McNealy <colt@littlehorse.io>

--- server/src/test/java/io/littlehorse/server/streams/util/AsyncWaitersTest.java
+++ server/src/test/java/io/littlehorse/server/streams/util/AsyncWaitersTest.java
@@ -8,16 +8,23 @@ import io.littlehorse.common.TestStreamObserver;
 import io.littlehorse.common.model.getable.core.events.WorkflowEventModel;
 import io.littlehorse.common.model.getable.objectId.WfRunIdModel;
 import io.littlehorse.common.proto.InternalWaitForWfEventRequest;
+import io.littlehorse.common.proto.WaitForCommandResponse;
 import io.littlehorse.sdk.common.proto.WorkflowEvent;
 import io.littlehorse.server.streams.topology.core.RequestExecutionContext;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Answers;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -57,5 +64,55 @@ public class AsyncWaitersTest {
         asyncWaiters.registerObserverWaitingForWorkflowEvent(mockRequest, clientObserver1, requestContext);
         Assertions.assertThat(clientObserver1.getValues()).hasSize(1).allMatch(Objects::nonNull);
         Assertions.assertThat(clientObserver1.isCompleted()).isTrue();
+    }
+
+    @Test
+    void shouldDeliverResponsesBackToWaitingClients() {
+        TestStreamObserver<WaitForCommandResponse> clientObserver1 = new TestStreamObserver<>();
+        String commandId = "command-1";
+        WaitForCommandResponse mockResponse = Mockito.mock();
+        asyncWaiters.registerObserverWaitingForCommand(commandId, clientObserver1);
+        asyncWaiters.registerCommandProcessed(commandId, mockResponse);
+        Assertions.assertThat(clientObserver1.isCompleted()).isTrue();
+        List<WaitForCommandResponse> commands = clientObserver1.getValues();
+        Assertions.assertThat(commands).hasSize(1);
+        Assertions.assertThat(commands.get(0)).isSameAs(mockResponse);
+    }
+
+    @Test
+    void shouldGuaranteeUniqueResponseForWaitingClients() {
+        TestStreamObserver<WaitForCommandResponse> clientObserver1 = new TestStreamObserver<>();
+        String commandId = "command-1";
+        WaitForCommandResponse mockResponse = Mockito.mock();
+        asyncWaiters.registerCommandProcessed(commandId, mockResponse);
+        asyncWaiters.registerObserverWaitingForCommand(commandId, clientObserver1);
+        // new clients should not get the previous response
+        asyncWaiters.registerObserverWaitingForCommand(commandId, clientObserver1);
+        Assertions.assertThat(clientObserver1.isCompleted()).isTrue();
+        List<WaitForCommandResponse> commands = clientObserver1.getValues();
+        Assertions.assertThat(commands).hasSize(1);
+        Assertions.assertThat(commands.get(0)).isSameAs(mockResponse);
+    }
+
+    @Test
+    void shouldHandleConcurrencyForWaitingClients() throws Exception {
+        ExecutorService executorService = Executors.newWorkStealingPool();
+        final Map<String, TestStreamObserver<WaitForCommandResponse>> commandObservers = new HashMap<>();
+        final int concurrentClients = 100_000;
+        for (int i = 0; i < concurrentClients; i++) {
+            String commandId = UUID.randomUUID().toString();
+            TestStreamObserver<WaitForCommandResponse> clientObserver = new TestStreamObserver<>();
+            commandObservers.put(commandId, clientObserver);
+            executorService.submit(() -> asyncWaiters.registerObserverWaitingForCommand(commandId, clientObserver));
+            executorService.submit(() -> asyncWaiters.registerCommandProcessed(commandId, Mockito.mock()));
+        }
+        executorService.shutdown();
+        boolean tasksTerminated = executorService.awaitTermination(3, TimeUnit.SECONDS);
+        Assertions.assertThat(tasksTerminated).isTrue();
+        Assertions.assertThat(commandObservers.keySet()).hasSize(concurrentClients);
+        commandObservers.forEach((commandId, observer) -> {
+            Assertions.assertThat(observer.getValues()).hasSize(1);
+            Assertions.assertThat(observer.getValues().get(0)).isNotNull();
+        });
     }
 }
