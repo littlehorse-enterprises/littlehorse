799364f05f4d5823b5f8839506672121d8989b1d
Mateo Rojas <mateo@littlehorse.io>
2024-03-21 10:39:31 -0500

    Setup proper baseURL for littlehorse

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: "https://littlehorse.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/littlehorse/",
 
   // // GitHub pages deployment config.
   // // If you aren't using GitHub pages, you don't need these.
