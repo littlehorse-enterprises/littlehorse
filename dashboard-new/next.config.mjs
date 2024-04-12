9faeb9ec82daef36145dc259da34e940e1ed5fb0
Mijail Rondon <mijail@riseup.net>
2024-04-12 12:30:24 -0500

    ci(dashboard): publish dashboard next image

--- dashboard-new/next.config.mjs
+++ dashboard-new/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   webpack: config => {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.('.svg'))
--- dashboard-new/src/app/(authenticated)/externalEventDef/[name]/components/Details.tsx
+++ dashboard-new/src/app/(authenticated)/externalEventDef/[name]/components/Details.tsx
@@ -1,17 +1,16 @@
 'use client'
 import { ExternalEventDef } from 'littlehorse-client/dist/proto/external_event'
-import { TaskDef } from 'littlehorse-client/dist/proto/task_def'
 import { FC } from 'react'
 
 type DetailsProps = {
   spec: ExternalEventDef
 }
 
-export const Details: FC<DetailsProps> = ({ spec: { name, retentionPolicy } }) => {
+export const Details: FC<DetailsProps> = ({ spec: { id, retentionPolicy } }) => {
   return (
     <div className="mb-4">
       <span className="italic">ExternalEventDef</span>
-      <h1 className="block text-2xl font-bold">{name}</h1>
+      <h1 className="block text-2xl font-bold">{id?.name}</h1>
       {retentionPolicy?.secondsAfterPut && (
         <div className="flex items-center gap-2">
           Retention Policy:
