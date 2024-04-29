f73a26c8e077fc69736b6ef4d16b910397345f00
Mateo Rojas <mateo@littlehorse.io>
2024-04-29 12:31:32 -0500

    Fixes baseURL and url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: "img/logo.jpg",
 
   // Set the production url of your site here
-  url: "https://littlehorse-enterprises.github.io",
+  url: "https://littlehorse.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/littlehorse/",
+  baseUrl: "/",
 
   // // GitHub pages deployment config.
   // // If you aren't using GitHub pages, you don't need these.
