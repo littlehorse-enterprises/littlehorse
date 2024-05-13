9f334ea3fe14965ba4b14102feeac4bc61d07f9d
Nelson Jumbo <nelson@littlehorse.io>
2024-05-13 15:41:21 -0500

    feature: Changes tailwind config to use black as default text color

--- dashboard/tailwind.config.ts
+++ dashboard/tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
         ...colors,
         black: '#242529',
         blue: { 500: '#7F7AFF' },
+        'text-default': 'black'
       }),
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
