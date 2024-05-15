74e8d734eaa30d0086102a35ba32ed5a64fdd328
Nelson Jumbo <nelson@littlehorse.io>
2024-05-15 10:34:40 -0500

    feature: allows to search by user id

--- dashboard/src/app/(authenticated)/userTaskDef/[...props]/components/UserTaskDef.tsx
+++ dashboard/src/app/(authenticated)/userTaskDef/[...props]/components/UserTaskDef.tsx
@@ -5,7 +5,7 @@ import React, { FC, Fragment, useState } from 'react'
 import { Details } from './Details'
 import { Navigation } from '@/app/(authenticated)/components/Navigation'
 import { Fields } from './Fields'
-import { Button } from '@headlessui/react'
+import { Button, Field, Input, Label } from '@headlessui/react'
 import { SEARCH_DEFAULT_LIMIT } from '@/app/constants'
 import Link from 'next/link'
 import { useInfiniteQuery } from '@tanstack/react-query'
@@ -14,6 +14,7 @@ import { useWhoAmI } from '@/contexts/WhoAmIContext'
 import { searchUserTaskRun } from '@/app/(authenticated)/userTaskDef/[...props]/actions/searchUserTaskRun'
 import { ArrowPathIcon } from '@heroicons/react/16/solid'
 import { concatWfRunIds } from '@/app/utils'
+import { useDebounce } from 'use-debounce'
 
 type Props = {
   spec: UserTaskDefProto
@@ -26,11 +27,13 @@ export const UserTaskDef: FC<Props> = ({ spec }) => {
     UserTaskRunStatus.UNASSIGNED,
   ]
   const [selectedStatus, setSelectedStatus] = useState(UserTaskRunStatus.UNASSIGNED)
+  const [userId, setUserId] = useState('')
   const { tenantId } = useWhoAmI()
-  const [limit, setLimit] = useState<number>(SEARCH_DEFAULT_LIMIT)
+  const [limit] = useState<number>(SEARCH_DEFAULT_LIMIT)
+  const [userIdToSearchFor] = useDebounce(userId, 1000)
 
   const { isPending, data, hasNextPage, fetchNextPage } = useInfiniteQuery({
-    queryKey: ['userTaskRun', selectedStatus, tenantId, limit],
+    queryKey: ['userTaskRun', selectedStatus, userIdToSearchFor, tenantId],
     initialPageParam: undefined,
     getNextPageParam: (lastPage: UserTaskRunIdList) => lastPage.bookmark?.toString('base64'),
     queryFn: async ({ pageParam }) => {
@@ -40,6 +43,7 @@ export const UserTaskDef: FC<Props> = ({ spec }) => {
         limit,
         status: selectedStatus,
         userTaskDefName: spec.name,
+        userId: userIdToSearchFor.trim() != '' ? userIdToSearchFor : undefined,
       })
     },
   })
@@ -64,6 +68,17 @@ export const UserTaskDef: FC<Props> = ({ spec }) => {
           ))}
         </div>
       </div>
+      <div className="mb-5 flex w-1/4 items-start justify-between">
+        <Field className="flex items-center justify-between">
+          <Label className="block w-1/2 font-bold">User Id</Label>
+          <Input
+            type="text"
+            value={userId}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUserId(e.target.value)}
+            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight shadow focus:outline-none"
+          />
+        </Field>
+      </div>
 
       {isPending ? (
         <div className="flex min-h-[360px] items-center justify-center text-center">
@@ -73,12 +88,14 @@ export const UserTaskDef: FC<Props> = ({ spec }) => {
         <div className="flex min-h-[360px] flex-col gap-4">
           <table className="text-surface min-w-full text-center text-sm font-light">
             <thead className="border-b border-neutral-200 bg-neutral-300 font-medium">
-              <th scope="col" className="px-6 py-4">
-                WfRun Id
-              </th>
-              <th scope="col" className="px-6 py-4">
-                User Task GUID
-              </th>
+              <tr>
+                <td scope="col" className="px-6 py-4">
+                  WfRun Id
+                </td>
+                <th scope="col" className="px-6 py-4">
+                  User Task GUID
+                </th>
+              </tr>
             </thead>
             <tbody>
               {data?.pages.map((page, i) => (
