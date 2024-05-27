e79d60b5c332432e825a69eb774438e7486b44ab
Eduwer Camacaro <eduwerc@gmail.com>
2024-05-27 15:00:13 -0500

    merge master

--- canary/src/main/java/io/littlehorse/canary/Main.java
+++ canary/src/main/java/io/littlehorse/canary/Main.java
@@ -43,7 +43,6 @@ public class Main {
         final CanaryConfig canaryConfig = args.length > 0 ? ConfigLoader.load(Paths.get(args[0])) : ConfigLoader.load();
 
         final PrometheusExporter prometheusExporter = new PrometheusExporter(canaryConfig.getCommonTags());
-        prometheusExporter.addMeasurable(lhClient);
         // create topics
         if (canaryConfig.isTopicCreationEnabled()) {
             final NewTopic topic = new NewTopic(
@@ -62,6 +61,7 @@ public class Main {
                     canaryConfig.getWorkflowName(),
                     canaryConfig.getWorkflowVersion(),
                     canaryConfig.getWorkflowRevision());
+            prometheusExporter.addMeasurable(lhClient);
             final BeatProducer producer = new BeatProducer(
                     lhConfig.getApiBootstrapHost(),
                     lhConfig.getApiBootstrapPort(),
