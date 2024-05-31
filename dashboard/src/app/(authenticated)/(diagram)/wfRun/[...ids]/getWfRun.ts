92ac58252ce95dad0b59894735a871093346e5d8
Hazim Arafa <hazimarafa80@gmail.com>
2024-05-31 11:15:58 -0700

    feat(dashboard): `WfSpec` var values are shown for current `ThreadRun`

--- dashboard/src/app/(authenticated)/(diagram)/wfRun/[...ids]/getWfRun.ts
+++ dashboard/src/app/(authenticated)/(diagram)/wfRun/[...ids]/getWfRun.ts
@@ -3,6 +3,7 @@
 import { lhClient } from '@/app/lhClient'
 import { NodeRun } from 'littlehorse-client/dist/proto/node_run'
 import { WfRunId } from 'littlehorse-client/dist/proto/object_id'
+import { Variable } from 'littlehorse-client/dist/proto/variable'
 import { ThreadRun, WfRun } from 'littlehorse-client/dist/proto/wf_run'
 import { WfSpec } from 'littlehorse-client/dist/proto/wf_spec'
 import { cookies } from 'next/headers'
@@ -17,6 +18,7 @@ export type WfRunResponse = {
   wfRun: WfRun & { threadRuns: ThreadRunWithNodeRuns[] }
   wfSpec: WfSpec
   nodeRuns: NodeRun[]
+  variables: Variable[]
 }
 export const getWfRun = async ({ ids }: Props): Promise<WfRunResponse> => {
   const tenantId = cookies().get('tenantId')?.value
@@ -25,15 +27,18 @@ export const getWfRun = async ({ ids }: Props): Promise<WfRunResponse> => {
     .reverse()
     .reduceRight<WfRunId | undefined>((parentWfRunId, id) => ({ id, parentWfRunId }), undefined)
   const wfRun = await client.getWfRun(wfRunId!)
-  const [wfSpec, { results: nodeRuns }] = await Promise.all([
+  const [wfSpec, { results: nodeRuns }, { results: variables }] = await Promise.all([
     client.getWfSpec({ ...wfRun.wfSpecId }),
     client.listNodeRuns({
+      wfRunId,
+    }),
+    client.listVariables({
       wfRunId,
     }),
   ])
 
   const threadRuns = wfRun.threadRuns.map(threadRun => mergeThreadRunsWithNodeRuns(threadRun, nodeRuns))
-  return { wfRun: { ...wfRun, threadRuns }, wfSpec, nodeRuns }
+  return { wfRun: { ...wfRun, threadRuns }, wfSpec, nodeRuns, variables }
 }
 
 const mergeThreadRunsWithNodeRuns = (threadRun: ThreadRun, nodeRuns: NodeRun[]): ThreadRunWithNodeRuns => {
