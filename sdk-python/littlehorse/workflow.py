d4d5fd97f695bac5de7c80141dda4b53d0a1ab0f
Jacob Snarr <jacobsnarr@gmail.com>
2024-06-04 18:46:41 -0400

    feat(sdk-python): Add customizable timeout options for TaskRuns (#858)

--- sdk-python/littlehorse/workflow.py
+++ sdk-python/littlehorse/workflow.py
@@ -740,6 +740,7 @@ class WorkflowThread:
         initializer: "ThreadInitializer",
         default_retries: Optional[int] = None,
         default_exponential_backoff: Optional[ExponentialBackoffRetryPolicy] = None,
+        default_timeout_seconds: Optional[int] = None,
     ) -> None:
         """This is used to define the logic of a ThreadSpec in a ThreadInitializer.
 
@@ -750,6 +751,7 @@ class WorkflowThread:
         self._default_exponential_backoff: Optional[ExponentialBackoffRetryPolicy] = (
             default_exponential_backoff
         )
+        self._default_timeout_seconds: Optional[int] = default_timeout_seconds
         self._default_retries: Optional[int] = default_retries
         self._wf_run_variables: list[WfRunVariable] = []
         self._wf_interruptions: list[WorkflowInterruption] = []
@@ -1016,6 +1018,7 @@ class WorkflowThread:
         self,
         task_name: Union[str, LHFormatString, WfRunVariable],
         *args: Any,
+        timeout_seconds: Optional[int] = None,
         retries: Optional[int] = None,
         exponential_backoff: Optional[ExponentialBackoffRetryPolicy] = None,
     ) -> NodeOutput:
@@ -1037,11 +1040,13 @@ class WorkflowThread:
         self._check_if_active()
         task_node: TaskNode
         readable_name: str
+
         if isinstance(task_name, str):
             readable_name = task_name
             task_node = TaskNode(
                 task_def_id=TaskDefId(name=task_name),
                 variables=[to_variable_assignment(arg) for arg in args],
+                timeout_seconds=timeout_seconds if timeout_seconds is not None else self._default_timeout_seconds,
                 retries=retries if retries is not None else self._default_retries,
                 exponential_backoff=(
                     exponential_backoff
@@ -1054,6 +1059,7 @@ class WorkflowThread:
             task_node = TaskNode(
                 dynamic_task=to_variable_assignment(task_name),
                 variables=[to_variable_assignment(arg) for arg in args],
+                timeout_seconds=timeout_seconds if timeout_seconds is not None else self._default_timeout_seconds,
                 retries=retries if retries is not None else self._default_retries,
                 exponential_backoff=(
                     exponential_backoff
@@ -1067,6 +1073,7 @@ class WorkflowThread:
             task_node = TaskNode(
                 dynamic_task=to_variable_assignment(task_name),
                 variables=[to_variable_assignment(arg) for arg in args],
+                timeout_seconds=timeout_seconds if timeout_seconds is not None else self._default_timeout_seconds,
                 retries=retries if retries is not None else self._default_retries,
                 exponential_backoff=(
                     exponential_backoff
@@ -1753,6 +1760,7 @@ class Workflow:
         self._allowed_updates: Optional[AllowedUpdateType] = None
         self._parent_wf: Optional[WfSpec.ParentWfSpecReference] = None
         self._retention_policy: Optional[WorkflowRetentionPolicy] = None
+        self._default_timeout_seconds: Optional[int] = None
         self._default_exponential_backoff: Optional[ExponentialBackoffRetryPolicy] = (
             None
         )
@@ -1824,6 +1832,7 @@ class Workflow:
                 initializer,
                 self._default_retries,
                 self._default_exponential_backoff,
+                self._default_timeout_seconds
             )
             thread_specs[name] = builder.compile()
 
@@ -1866,6 +1875,20 @@ class Workflow:
         """
         self._default_retries = retries
         self._default_exponential_backoff = exponential_backoff
+        return self
+    
+    def with_task_timeout_seconds(
+        self,
+        timeout_seconds: Optional[int] = None
+    ) -> Workflow:
+        """Configures the default timeout length (seconds) of the tasks.
+
+        Args:
+            timeout_seconds(Optional[int]): Length of time before the TaskRun times out.
+
+        Returns: This instance.
+        """
+        self._default_timeout_seconds = timeout_seconds
         return self
 
 
