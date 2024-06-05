307c1eb40a4a5202e49c553b3db245d9d22538cc
Eduwer Camacaro <eduwerc@gmail.com>
2024-06-05 12:03:43 -0500

    increase default inflight

--- sdk-java/src/main/java/io/littlehorse/sdk/common/config/LHConfig.java
+++ sdk-java/src/main/java/io/littlehorse/sdk/common/config/LHConfig.java
@@ -360,7 +360,7 @@ public class LHConfig extends ConfigBase {
     }
 
     public Integer getInflightTasks() {
-        return Integer.valueOf(getOrSetDefault(INFLIGHT_TASKS_KEY, "1"));
+        return Integer.valueOf(getOrSetDefault(INFLIGHT_TASKS_KEY, "2"));
     }
 
     public boolean isOauth() {
