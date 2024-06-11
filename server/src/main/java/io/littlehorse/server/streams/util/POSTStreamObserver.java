b888d09f2640b32324352c04658e794bc211f846
Eduwer Camacaro <eduwerc@gmail.com>
2024-06-11 12:05:41 -0500

    fix(server): retry wait for command before a rebalance

--- server/src/main/java/io/littlehorse/server/streams/util/POSTStreamObserver.java
+++ server/src/main/java/io/littlehorse/server/streams/util/POSTStreamObserver.java
@@ -2,10 +2,14 @@ package io.littlehorse.server.streams.util;
 
 import com.google.protobuf.ByteString;
 import com.google.protobuf.Message;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import io.littlehorse.common.model.AbstractCommand;
 import io.littlehorse.common.proto.WaitForCommandResponse;
 import io.littlehorse.server.streams.BackendInternalComms;
+import java.time.Duration;
+import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -16,6 +20,10 @@ public class POSTStreamObserver<U extends Message> implements StreamObserver<Wai
     private boolean shouldComplete;
     private final BackendInternalComms internalComms;
     private final AbstractCommand<?> command;
+    private final Date commandStartedAt;
+    private final Date timeoutAt;
+    // Wait until kafka streams rebalance finishes
+    private static final Duration successResponseTimeout = Duration.ofSeconds(30);
 
     public POSTStreamObserver(
             StreamObserver<U> responseObserver,
@@ -28,11 +36,20 @@ public class POSTStreamObserver<U extends Message> implements StreamObserver<Wai
         this.shouldComplete = shouldComplete;
         this.internalComms = internalComms;
         this.command = command;
+        this.commandStartedAt = new Date();
+        this.timeoutAt = new Date(commandStartedAt.getTime() + successResponseTimeout.toMillis());
     }
 
     @Override
     public void onError(Throwable t) {
-        ctx.onError(t);
+        final boolean isRetryable = t instanceof StatusRuntimeException grpcRuntimeException
+                && grpcRuntimeException.getStatus().getCode().equals(Status.UNAVAILABLE.getCode());
+        final boolean retryOneMoreTime = timeoutAt.compareTo(new Date()) > 0;
+        if (isRetryable && retryOneMoreTime) {
+            internalComms.waitForCommand(command, this);
+        } else {
+            ctx.onError(t);
+        }
     }
 
     @SuppressWarnings("unchecked")
