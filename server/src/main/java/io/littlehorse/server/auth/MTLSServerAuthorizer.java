f3c48c9c0f5c84aa71b2df8ffc006b627dac2063
Snarr <jacobsnarr@gmail.com>
2024-06-12 12:38:35 -0400

    Spotless apply

--- server/src/main/java/io/littlehorse/server/auth/MTLSServerAuthorizer.java
+++ server/src/main/java/io/littlehorse/server/auth/MTLSServerAuthorizer.java
@@ -3,31 +3,30 @@ package io.littlehorse.server.auth;
 import io.grpc.Metadata;
 import io.grpc.ServerCall;
 import io.grpc.ServerCall.Listener;
+import io.grpc.ServerCallHandler;
 import io.littlehorse.common.util.CertificateUtil;
 import io.littlehorse.server.listener.MTLSConfig;
-import io.grpc.ServerCallHandler;
 
 public class MTLSServerAuthorizer implements ServerAuthorizer {
 
-  private final String commonName;
+    private final String commonName;
 
-  public MTLSServerAuthorizer(MTLSConfig mtlsConfig) {
-    try {
-      this.commonName = CertificateUtil.getCommonNameFromCertificate(mtlsConfig.getCaCertificate());
-    } catch (Exception ex) {
-      throw new IllegalArgumentException("Certificate is not valid", ex);
+    public MTLSServerAuthorizer(MTLSConfig mtlsConfig) {
+        try {
+            this.commonName = CertificateUtil.getCommonNameFromCertificate(mtlsConfig.getCaCertificate());
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("Certificate is not valid", ex);
+        }
     }
-  }
 
-  @Override
-  public <ReqT, RespT> Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers,
-      ServerCallHandler<ReqT, RespT> next) {
-    headers.put(CLIENT_ID, this.commonName);
+    @Override
+    public <ReqT, RespT> Listener<ReqT> interceptCall(
+            ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+        headers.put(CLIENT_ID, this.commonName);
 
-    return next.startCall(call, headers);
+        return next.startCall(call, headers);
 
-    // throw new UnsupportedOperationException("Unimplemented method
-    // 'interceptCall'");
-  }
-
+        // throw new UnsupportedOperationException("Unimplemented method
+        // 'interceptCall'");
+    }
 }
