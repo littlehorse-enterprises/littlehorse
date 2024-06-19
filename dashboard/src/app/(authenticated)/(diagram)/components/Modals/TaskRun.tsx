8f466ce3684bd6c2b9e1539bd7b0e7cb3fb5b4be
Hazim Arafa <hazimarafa80@gmail.com>
2024-06-19 08:40:03 -0600

    fix(dashboard): account for `oneof` property on `TaskAttempt`

--- dashboard/src/app/(authenticated)/(diagram)/components/Modals/TaskRun.tsx
+++ dashboard/src/app/(authenticated)/(diagram)/components/Modals/TaskRun.tsx
@@ -1,7 +1,7 @@
 import { getVariableValue } from '@/app/utils'
 import { cn } from '@/lib/utils'
 import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
-import { TaskRun as LHTaskRun } from 'littlehorse-client/proto'
+import { TaskRun as LHTaskRun, TaskAttempt } from 'littlehorse-client/proto'
 import { ClipboardIcon } from 'lucide-react'
 import { FC, useMemo, useState } from 'react'
 import { Modal } from '../../context'
@@ -27,7 +27,10 @@ export const TaskRun: FC<Modal> = ({ data }) => {
           <Description>
             <div className="">
               <div
-                className={cn('flex items-center justify-between p-2', attempt.error ? 'bg-red-200' : 'bg-green-200')}
+                className={cn(
+                  'flex items-center justify-between p-2',
+                  attempt.exception || attempt.error ? 'bg-red-200' : 'bg-green-200'
+                )}
               >
                 <div className="flex items-center gap-1">
                   <div className="flex gap-1">
@@ -60,25 +63,30 @@ export const TaskRun: FC<Modal> = ({ data }) => {
                 </div>
               </div>
 
-              {attempt.error || attempt.exception ? (
-                <div className="mt-2 flex flex-col rounded bg-red-200 p-1">
-                  <h3 className="font-bold">{attempt.error ? 'Error' : attempt.exception ? 'Exception' : 'N/A'}</h3>
-                  <pre className="overflow-x-auto">
-                    {attempt.error ? attempt.error.message : attempt.exception ? attempt.exception.message : 'N/A'}
-                  </pre>
-                </div>
-              ) : (
-                attempt.output && (
-                  <div className="mt-2 flex flex-col rounded bg-zinc-500 p-1 text-white">
-                    <h3 className="font-bold">Output</h3>
-                    <pre className="overflow-x-auto">{getVariableValue(attempt.output)}</pre>
-                  </div>
-                )
-              )}
+              <AttemptErrorExceptionOutput attempt={attempt} />
             </div>
           </Description>
         </DialogPanel>
       </div>
     </Dialog>
+  )
+}
+
+function AttemptErrorExceptionOutput({ attempt }: { attempt: TaskAttempt }) {
+  if (!attempt.output && !attempt.error && !attempt.exception) return
+
+  return (
+    <div className={cn('mt-2 flex flex-col rounded p-1', attempt.output ? 'bg-zinc-500 text-white' : 'bg-red-200')}>
+      <h3 className="font-bold">
+        {attempt.error && 'Error'}
+        {attempt.exception && 'Exception'}
+        {attempt.output && 'Output'}
+      </h3>
+      <pre className="overflow-x-auto">
+        {attempt.error && attempt.error.message}
+        {attempt.exception && attempt.exception.message}
+        {attempt.output && getVariableValue(attempt.output)}
+      </pre>
+    </div>
   )
 }
