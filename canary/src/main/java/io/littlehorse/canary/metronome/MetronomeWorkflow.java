30f413e964ed53b2a7a0af7df9b2463d348e6b3d
Colt McNealy <colt@littlehorse.io>
2024-07-16 07:15:10 -0700

    fix(canary): sets workflow retention policy to 1 day

--- canary/src/main/java/io/littlehorse/canary/metronome/MetronomeWorkflow.java
+++ canary/src/main/java/io/littlehorse/canary/metronome/MetronomeWorkflow.java
@@ -2,6 +2,7 @@ package io.littlehorse.canary.metronome;
 
 import io.littlehorse.canary.util.LHClient;
 import io.littlehorse.sdk.common.proto.VariableType;
+import io.littlehorse.sdk.common.proto.WorkflowRetentionPolicy;
 import io.littlehorse.sdk.wfsdk.Workflow;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,12 +14,18 @@ public class MetronomeWorkflow {
     public static final String SAMPLE_ITERATION_VARIABLE = "sample-iteration";
 
     public MetronomeWorkflow(final LHClient lhClient, final String workflowName) {
-        final Workflow workflow = Workflow.newWorkflow(
+        Workflow workflow = Workflow.newWorkflow(
                 workflowName,
                 thread -> thread.execute(
                         TASK_NAME,
                         thread.addVariable(START_TIME_VARIABLE, VariableType.INT),
                         thread.addVariable(SAMPLE_ITERATION_VARIABLE, VariableType.BOOL)));
+
+        // TODO: This should be a configuration later.
+        workflow.withRetentionPolicy(WorkflowRetentionPolicy.newBuilder()
+                .setSecondsAfterWfTermination(60 * 60 * 24)
+                .build());
+
         lhClient.registerWorkflow(workflow);
 
         log.info("Workflow {} Registered", workflowName);
