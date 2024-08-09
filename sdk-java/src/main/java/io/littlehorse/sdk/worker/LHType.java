6e06ac6e7fb8f6b15d2121cfee6f0cdf9ff4413e
Eduwer Camacaro <eduwerc@gmail.com>
2024-08-09 09:27:04 -0500

    java doc

--- sdk-java/src/main/java/io/littlehorse/sdk/worker/LHType.java
+++ sdk-java/src/main/java/io/littlehorse/sdk/worker/LHType.java
@@ -5,8 +5,23 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation for adding metadata based on the target type.
+ * <p>
+ * This annotation can be applied to either a method or a method parameter:
+ * <p>
+ * - Method: When applied to a method, the metadata will be added to a `NodeOutput`.
+ * - Method Parameter: When applied to a method parameter, the metadata will be added to a `VariableDef`.
+ *
+ */
 @Target({ElementType.PARAMETER, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LHType {
+
+    /**
+     * Indicates whether the value should be masked.
+     *
+     * @return true if the value should be masked; false otherwise.
+     */
     boolean masked();
 }
