ccf869f27f30873273c709a3d197cf37bd6e00f2
Mijail Rondon <mijail@riseup.net>
2024-08-21 11:10:28 -0500

    ci(versioning): handle ci versions

--- lhctl/main.go
+++ lhctl/main.go
@@ -5,6 +5,13 @@ package main
 
 import "github.com/littlehorse-enterprises/littlehorse/lhctl/cmd"
 
+var (
+	version = "0.0.0-development"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+	cmd.SetVersionInfo(version, commit, date)
 	cmd.Execute()
 }
