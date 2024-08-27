94b076c90a031cefcfbdac809ce50acc6fabc477
Jacob Snarr <jacobsnarr@gmail.com>
2024-08-27 18:13:16 -0400

    feat(docs): Add social card preview image (#980)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -28,6 +28,25 @@ module.exports = {
   onBrokenMarkdownLinks: "throw",
   onBrokenAnchors: "throw",
 
+  headTags: [
+    {
+      tagName: "meta",
+      attributes: {
+        "name": "twitter:title",
+        "content": "LittleHorse Orchestrator",
+        "data-rh": "true"
+      }
+    },
+    {
+      tagName: "meta",
+      attributes: {
+        "name": "twitter:description",
+        "content": "The Missing Piece of the Microservice Architecture",
+        "data-rh": "true"
+      }
+    }
+  ],
+  
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
@@ -66,7 +85,7 @@ module.exports = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/social-card.png",
       colorMode: {
         disableSwitch: true,
       },
