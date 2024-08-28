8553132bd7ca31bfabe9f51e9d5cabf354ddac84
Eduwer Camacaro <eduwerc@gmail.com>
2024-08-28 09:33:50 -0500

    Post a events with null Payload

--- lhctl/internal/post_event.go
+++ lhctl/internal/post_event.go
@@ -31,24 +31,23 @@ lhctl postEvent <wfRunId> <externalEventName> NULL
 			log.Fatal("Required args: <wfRunId> <externalEventName> <varType> <payload> or  <wfRunId> <externalEventName> (to send a null payload)")
 		}
 
-		wfRunIdStr, eedName, varTypeStr := args[0], args[1], args[2]
-		varType, validVarType := lhproto.VariableType_value[varTypeStr]
+		wfRunIdStr, eedName := args[0], args[1]
 
 		wfRunId := littlehorse.StrToWfRunId(wfRunIdStr)
-
-		if !validVarType {
-			log.Fatal(
-				"Unrecognized varType. Valid options: INT, STR, BYTES, BOOL, JSON_OBJ, JSON_ARR, DOUBLE or NULL.",
-			)
-		}
-
-		varTypeEnum := lhproto.VariableType(varType)
 
 		content := &lhproto.VariableValue{}
 
 		if len(args) == 4 {
+			varTypeStr := args[2]
 			payloadStr := args[3]
 
+			varType, validVarType := lhproto.VariableType_value[varTypeStr]
+			if !validVarType {
+				log.Fatal(
+					"Unrecognized varType. Valid options: INT, STR, BYTES, BOOL, JSON_OBJ, JSON_ARR, DOUBLE or NULL.",
+				)
+			}
+			varTypeEnum := lhproto.VariableType(varType)
 			var err error
 			content, err = littlehorse.StrToVarVal(payloadStr, varTypeEnum)
 			if err != nil {
