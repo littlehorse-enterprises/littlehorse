683e4b936afa9ff316dc70765520408b8d6169e1
Eduwer Camacaro <eduwerc@gmail.com>
2024-09-16 13:23:12 -0500

    Unit tests

--- server/src/main/java/io/littlehorse/server/GlobalExceptionHandler.java
+++ server/src/main/java/io/littlehorse/server/GlobalExceptionHandler.java
@@ -6,11 +6,16 @@ import io.grpc.ServerCall;
 import io.grpc.ServerCallHandler;
 import io.grpc.ServerInterceptor;
 import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.littlehorse.common.exceptions.LHApiException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.errors.InvalidStateStoreException;
 
 @Slf4j
 public class GlobalExceptionHandler implements ServerInterceptor {
+
+    public static final String INTERNAL_ERROR_MESSAGE =
+            "An unexpected internal error occurred. Please contact support for assistance.";
 
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
@@ -18,7 +23,7 @@ public class GlobalExceptionHandler implements ServerInterceptor {
         return new InternalCallListener<>(call, next, headers);
     }
 
-    private class InternalCallListener<ReqT, RespT>
+    private static class InternalCallListener<ReqT, RespT>
             extends ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT> {
         private final ServerCall<ReqT, RespT> call;
         private final Metadata metadata;
@@ -35,20 +40,16 @@ public class GlobalExceptionHandler implements ServerInterceptor {
             try {
                 // Proceed with the call
                 super.onHalfClose();
-            } catch (Exception e) {
-                handleException(call, e, metadata);
+            } catch (LHApiException apiException) {
+                call.close(apiException.getStatus(), metadata);
+            } catch (InvalidStateStoreException ex) {
+                call.close(Status.UNAVAILABLE.withDescription(ex.getMessage()).withCause(ex), metadata);
+            } catch (StatusRuntimeException ex) {
+                call.close(Status.fromThrowable(ex), metadata);
+            } catch (Throwable ex) {
+                log.error("BOOM! ", ex);
+                call.close(Status.INTERNAL.withDescription(INTERNAL_ERROR_MESSAGE), metadata);
             }
-        }
-    }
-
-    public void handleException(ServerCall<?, ?> call, Throwable throwable, Metadata metadata) {
-        log.trace("Unrecognized exception: ", throwable);
-        Status status = Status.INTERNAL
-                .withDescription("Server Error: " + throwable.getMessage())
-                .withCause(throwable);
-        call.close(status, metadata);
-        if (throwable instanceof LHApiException myEx) {
-            call.close(myEx.getStatus(), metadata);
         }
     }
 }
