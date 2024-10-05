1b2ca2b2e51aed8f3074b614b8ca33f51c093248
Snarr <jacobsnarr@gmail.com>
2024-10-04 22:08:56 -0400

    Add dark mode theme 'Dracula'

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -178,6 +178,7 @@ module.exports = {
       },
       prism: {
         theme: lightCodeTheme,
+        darkTheme: themes.dracula,
         additionalLanguages: ["java", "go", "groovy", "protobuf"],
       },
       algolia: {
