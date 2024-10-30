ce25bb034309c257bb100b1e1b40028004e57e59
Muhammad Qamar <muhammadqamar111@gmail.com>
2024-10-30 16:45:17 +0500

    refactor(dashboard): added fixes on conditional node

--- dashboard/src/app/(authenticated)/(diagram)/components/NodeTypes/Nop/Nop.tsx
+++ dashboard/src/app/(authenticated)/(diagram)/components/NodeTypes/Nop/Nop.tsx
@@ -16,8 +16,14 @@ const NopNode: FC<NodeProps> = props => {
       <div className="">
         <div className="cursor-pointer1 ml-1 flex h-6 w-6 rotate-45 border-[1px] border-gray-500 bg-gray-200"></div>
         <Handle type="target" position={Position.Left} id="target-0" className="bg-transparent" />
-        {handlers?.map((itr: Position, index: number) => (
-          <Handle key={index} type="source" position={itr} className="bg-transparent" id={`source-${index}`} />
+        {handlers?.map((pos: Position, index: number) => (
+          <Handle
+            key={`custom-${index}`}
+            type="source"
+            position={pos}
+            className="bg-transparent"
+            id={`source-${index}`}
+          />
         ))}
       </div>
     </Fade>
