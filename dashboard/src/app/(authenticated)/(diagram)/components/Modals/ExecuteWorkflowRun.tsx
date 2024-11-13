4ecb4cf658469f74b98ec13c3482ca8fec83eecf
muhammadqamar <muhammadqamar111@gmail.com>
2024-11-13 07:13:32 -0500

    fix(dashboard):execute workflow form * add from using react hook form

--- dashboard/src/app/(authenticated)/(diagram)/components/Modals/ExecuteWorkflowRun.tsx
+++ dashboard/src/app/(authenticated)/(diagram)/components/Modals/ExecuteWorkflowRun.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useModal } from '../../hooks/useModal'
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog'
 import { Modal } from '../../context'
@@ -7,14 +7,17 @@ import { WfSpec } from 'littlehorse-client/proto'
 import { runWfSpec } from '../../wfSpec/[...props]/actions/runWfSpec'
 import { useWhoAmI } from '@/contexts/WhoAmIContext'
 import { useRouter } from 'next/navigation'
+import { WfForm } from '@/components/ui/wfForm'
 
 export const ExecuteWorkflowRun: FC<Modal> = ({ data }) => {
   const { showModal, setShowModal } = useModal()
   const lhWorkflowSpec = data as WfSpec
   const { tenantId } = useWhoAmI()
   const router = useRouter()
+  const formRef = useRef<any>(null);
   const handleFormSubmit = async (event: any) => {
-    event.preventDefault()
+    console.log('values', event)
+    // event.preventDefault()
     if (!lhWorkflowSpec.id) return
     const wfRun = await runWfSpec({
       ...lhWorkflowSpec.id,
@@ -34,6 +37,9 @@ export const ExecuteWorkflowRun: FC<Modal> = ({ data }) => {
     setShowModal(false)
     router.push(`/wfRun/${wfRun.id.id}`)
   }
+  const triggerSubmit = () => {
+    
+  };
   return (
     <Dialog open={showModal} onOpenChange={open => setShowModal(open)}>
       <DialogContent>
@@ -41,31 +47,9 @@ export const ExecuteWorkflowRun: FC<Modal> = ({ data }) => {
           <DialogTitle>Execute Workflow</DialogTitle>
         </DialogHeader>
         <div>
-          <form onSubmit={handleFormSubmit}>
-            {lhWorkflowSpec.threadSpecs?.entrypoint &&
+            {lhWorkflowSpec.threadSpecs?.entrypoint && 
             lhWorkflowSpec.threadSpecs?.entrypoint?.variableDefs?.some(def => def.required) ? (
-              lhWorkflowSpec.threadSpecs.entrypoint.variableDefs
-                .filter(def => def.required)
-                .map((def, index) => (
-                  <div key={index} className="my-4">
-                    <label className="mr-2">{def.varDef?.name}</label>
-                    {def.varDef?.type === 'DOUBLE' ? (
-                      <textarea
-                        name={def.varDef?.name}
-                        required={def.required}
-                        placeholder={def.varDef?.name}
-                        rows={5}
-                      />
-                    ) : (
-                      <input
-                        type={def.varDef?.type === 'INT' ? 'number' : 'text'}
-                        name={def.varDef?.name}
-                        required={def.required}
-                        placeholder={def.varDef?.name}
-                      />
-                    )}
-                  </div>
-                ))
+              <WfForm variableDefs={lhWorkflowSpec.threadSpecs.entrypoint.variableDefs} onSubmit={handleFormSubmit} formRef={formRef} />
             ) : (
               <p>Currently no variables required!</p>
             )}
@@ -74,12 +58,11 @@ export const ExecuteWorkflowRun: FC<Modal> = ({ data }) => {
                 <button className="rounded-sm bg-gray-100 px-4 py-1 text-gray-900" onClick={() => setShowModal(false)}>
                   Cancel
                 </button>
-                <button type="submit" className="flex items-center gap-1 rounded-sm bg-blue-500 p-1 px-4 text-white">
+                <button onClick={() => {formRef.current?.requestSubmit()}}  type="submit" className="flex items-center gap-1 rounded-sm bg-blue-500 p-1 px-4 text-white">
                   Run
                 </button>
               </div>
             </DialogFooter>
-          </form>
         </div>
       </DialogContent>
     </Dialog>
