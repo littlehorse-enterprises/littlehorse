d1f3af5bdfc63e4323ae0c854988f04aba1e2f96
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-11-17 20:33:51 -0800

    feat(dashboard): restructure under tenantId slug

---
+++ dashboard/src/app/[tenantId]/(diagram)/components/NodeTypes/Sleep/SleepDetails.tsx
@@ -0,0 +1,31 @@
+import { NodeRun, SleepNode } from 'littlehorse-client/proto'
+import { FC } from 'react'
+import { NodeDetails } from '../NodeDetails'
+import { formatDate } from '@/app/utils'
+import { getVariable, formatTime } from '@/app/utils'
+
+export const SleepDetails: FC<{ sleepNode?: SleepNode; nodeRun?: NodeRun }> = ({ sleepNode, nodeRun }) => {
+  if (!sleepNode) return
+  const timeValue = getVariable(sleepNode?.rawSeconds)
+  return (
+    <NodeDetails>
+      <div className="mb-2">
+        <div className="flex items-center gap-1 whitespace-nowrap text-nowrap">
+          <h3 className="font-bold">Sleep</h3>
+        </div>
+        <div className="flex flex-col gap-2 text-nowrap">
+          <div className="flex ">
+            {sleepNode.rawSeconds && (
+              <div>Time: {typeof timeValue === 'number' ? formatTime(timeValue) : timeValue}</div>
+            )}
+            {sleepNode.timestamp && <div>{getVariable(sleepNode.timestamp)}</div>}
+            {sleepNode.isoDate && <div>{getVariable(sleepNode.isoDate)}</div>}
+          </div>
+          {!!nodeRun?.sleep?.maturationTime && (
+            <div className="flex ">Maturation Time: {formatDate(new Date(nodeRun.sleep.maturationTime))}</div>
+          )}
+        </div>
+      </div>
+    </NodeDetails>
+  )
+}
