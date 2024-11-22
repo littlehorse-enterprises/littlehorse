1f634194be6e068f369d423896e23117d1580b66
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-11-21 21:55:13 -0800

    feat(dashboard): debloat wfspec view (#1123)
    - Added `getLatestWfSpecs` server actions
      - Uses `lhClient` RPC
    - Group unique `WfSpecs` with latest version tag
    
    ---------
    
    Co-authored-by: Hazim Arafa <hazimoarafa@gmail.com>

--- dashboard/src/app/(authenticated)/components/tables/WfSpecTable.tsx
+++ dashboard/src/app/(authenticated)/components/tables/WfSpecTable.tsx
@@ -1,30 +1,46 @@
-import { WfSpecId } from 'littlehorse-client/proto'
-import { TagIcon } from 'lucide-react'
-import Link from 'next/link'
-import { FC, Fragment } from 'react'
+import { FC, Fragment, useEffect, useMemo, useState } from 'react'
 import { SearchResultProps } from '.'
-
+import { TagIcon } from 'lucide-react'
+import { Separator } from '@/components/ui/separator'
+import { useRouter } from 'next/navigation'
+import { getLatestWfSpecs } from '@/app/actions/getLatestWfSpec'
+import { useWhoAmI } from '@/contexts/WhoAmIContext'
+import { WfSpecData } from '@/types'
 export const WfSpecTable: FC<SearchResultProps> = ({ pages = [] }) => {
+  const router = useRouter()
+  const [wfSpecs, setWfSpecs] = useState<WfSpecData[]>([])
+  const { tenantId } = useWhoAmI()
+
   if (pages.length === 0) {
     return <div className="flex min-h-[360px] items-center justify-center text-center italic">No WfSpecs</div>
   }
 
+  useEffect(() => {
+    if (!tenantId) return
+    const wfSpecNames = pages.flatMap(page => page.results).map(wfSpec => wfSpec.name)
+    getLatestWfSpecs(tenantId, wfSpecNames).then(setWfSpecs)
+  }, [pages, tenantId])
+
   return (
     <div className="py-4">
-      {pages.map((page, i) => (
-        <Fragment key={i}>
-          {page.results.map(({ name, majorVersion, revision }: WfSpecId) => (
-            <div key={`${name}.${majorVersion}.${revision}`} className="my-2 flex gap-2">
-              <Link className="underline hover:no-underline" href={`/wfSpec/${name}/${majorVersion}.${revision}`}>
-                {name}
-              </Link>
+      <div className="flex max-h-[600px] flex-col overflow-auto">
+        {wfSpecs.map(wfSpec => (
+          <Fragment key={wfSpec.name}>
+            <button
+              className="flex items-center gap-3 rounded-md px-2 py-2 hover:bg-gray-100"
+              key={wfSpec.name}
+              onClick={() => router.push(`/wfSpec/${wfSpec.name}/${wfSpec.latestVersion}`)}
+            >
+              <p>{wfSpec.name}</p>
               <div className="flex items-center gap-2 rounded bg-blue-200 px-2 font-mono text-sm text-gray-500">
-                <TagIcon className="h-4 w-4 fill-none stroke-gray-500 stroke-1" />v{majorVersion}.{revision}
+                <TagIcon className="h-4 w-4 fill-none stroke-gray-500 stroke-1" />
+                Latest: v{wfSpec.latestVersion}
               </div>
-            </div>
-          ))}
-        </Fragment>
-      ))}
+            </button>
+            <Separator />
+          </Fragment>
+        ))}
+      </div>
     </div>
   )
 }
