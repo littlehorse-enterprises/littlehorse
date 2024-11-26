de2eb21358a6e8820438723d8f0547cf141a9661
Hazim Arafa <hazimoarafa@gmail.com>
2024-11-25 21:36:43 -0800

    fix(dashboard): accommodate new

--- dashboard/src/app/(authenticated)/(diagram)/components/Modals/Edge.tsx
+++ dashboard/src/app/(authenticated)/(diagram)/components/Modals/Edge.tsx
@@ -21,7 +21,7 @@ export const Edge: FC<Modal> = ({ data }) => {
             <span className="rounded bg-green-300 p-1 text-xs">{mutation.operation}</span>
             <NodeOutput nodeOutput={mutation.nodeOutput} />
             <LiteralValue literalValue={mutation.literalValue} />
-            <RhsAssignment RhsAssignment={mutation.RhsAssignment} />
+            <RhsAssignment rhsAssignment={mutation.rhsAssignment} />
           </div>
         ))}
       </DialogContent>
@@ -41,12 +41,12 @@ const NodeOutput: FC<Pick<VariableMutation, 'nodeOutput'>> = ({ nodeOutput }) =>
   )
 }
 
-const RhsAssignment: FC<Pick<VariableMutation, 'RhsAssignment'>> = ({ RhsAssignment }) => {
-  if (!RhsAssignment) return <></>
+const RhsAssignment: FC<Pick<VariableMutation, 'rhsAssignment'>> = ({ rhsAssignment }) => {
+  if (!rhsAssignment) return <></>
   return (
     <>
-      <span className="rounded bg-gray-200 p-1 text-xs">Source Variable</span>
-      <span className="rounded bg-gray-100 p-1 font-mono text-xs text-orange-500">{getVariable(RhsAssignment)}</span>
+      <span className="rounded bg-gray-200 p-1 text-xs">RHS Assignment</span>
+      <span className="rounded bg-gray-100 p-1 font-mono text-xs text-orange-500">{getVariable(rhsAssignment)}</span>
     </>
   )
 }
