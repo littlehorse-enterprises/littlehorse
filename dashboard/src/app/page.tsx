60e0885b057a85fafa459c7acd98c29be840c7e1
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-11-25 23:12:05 -0800

    refactor(dashboard): whoAmI tenants default handling

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -2,19 +2,16 @@ import { redirect } from 'next/navigation'
 import getWhoAmI from './getWhoami'
 
 export default async function Home() {
-
-    if (process.env.LHD_OAUTH_ENABLED === 'true') {
-        const whoAmI = await getWhoAmI()
-        if (whoAmI.tenants.length === 0) {
-            return <div className="flex flex-col items-center justify-center">
-                <h1 className="font-bold text-2xl">No tenants found for the current principal</h1>
-                <p className="text-center mt-3">
-                    Try signing into a different principal. <br />
-                    Please contact your administrator if you think this is a mistake.
-                </p>
-            </div>
-        }
-        return redirect(`/${whoAmI.tenants[0]}`)
-    }
-    return redirect(`/default`)
+    const whoAmI = await getWhoAmI()
+    return whoAmI.tenants.length === 0 ? (
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="font-bold text-2xl">No tenants found for the current principal</h1>
+            <p className="text-center mt-3">
+                Try signing into a different principal. <br />
+                Please contact your administrator if you think this is a mistake.
+            </p>
+        </div>
+    ) : (
+        redirect(`/${whoAmI.tenants[0] ?? 'default'}`)
+    )
 }
