e93c772339efe54541aec669702d6adf44a65a3b
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-11-25 23:48:33 -0800

    refactor(dashboard): whoAmI tenantId logo redirect

--- dashboard/src/app/[tenantId]/components/Header.tsx
+++ dashboard/src/app/[tenantId]/components/Header.tsx
@@ -6,14 +6,15 @@ import LhLogo from '@/littlehorse.svg'
 import { Principal } from './Principal'
 import { TenantSelector } from './TenantSelector'
 import { useParams } from 'next/navigation'
+import { useWhoAmI } from '@/contexts/WhoAmIContext'
 
 export const Header: FC = () => {
-  const { tenantId } = useParams()
+  const whoAmI = useWhoAmI()
 
   return (
     <nav className="mb-4 border-gray-200 bg-black px-8 dark:bg-gray-900">
       <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between py-4">
-        <a href={`/${tenantId}`} className="flex items-center space-x-1 rtl:space-x-reverse">
+        <a href={`/${whoAmI.tenants[0]}`} className="flex items-center space-x-1 rtl:space-x-reverse">
           <LhLogo className="h-8 fill-white" />
           <div className="hidden flex-col gap-0 space-y-[-10px] text-xl font-bold text-white md:flex">
             <span>LITTLE</span>
