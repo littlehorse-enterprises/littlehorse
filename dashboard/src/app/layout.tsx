78bc188f708c9b18dc247008c1b0fc34dd128dde
Mateo Rojas <mateo@littlehorse.io>
2025-01-16 12:12:50 -0500

    Fix

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Toaster } from '@/components/ui/sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,8 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="mx-auto max-w-screen-xl px-8">{children}</div>
+        {children}
+        <Toaster position="top-center" richColors />
       </body>
     </html>
   )
