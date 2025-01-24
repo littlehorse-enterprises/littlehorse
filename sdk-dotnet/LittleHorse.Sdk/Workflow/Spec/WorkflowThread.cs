c179caee2620710d02f3e2ae1eb7edf07d7ecb6d
KarlaCarvajal <karlis.carvajalv@gmail.com>
2025-01-24 16:58:46 -0500

    feat(sdk-dotnet): Add declare methods

--- sdk-dotnet/LittleHorse.Sdk/Workflow/Spec/WorkflowThread.cs
+++ sdk-dotnet/LittleHorse.Sdk/Workflow/Spec/WorkflowThread.cs
@@ -144,7 +144,6 @@ public class WorkflowThread
 
     private TaskNode CreateTaskNode(TaskNode taskNode, params object[] args)
     {
-
         foreach (var arg in args)
         {
             taskNode.Variables.Add(AssignVariable(arg));
@@ -164,5 +163,89 @@ public class WorkflowThread
         }
 
         return taskNode;
+    }
+    
+    /// <summary>
+    /// Creates a variable of type BOOL in the ThreadSpec.
+    /// </summary>
+    /// <param name="name">
+    /// It is the name of the variable.
+    /// </param>
+    /// <returns>The value of <paramref name="WfRunVariable" /> </returns>
+    public WfRunVariable DeclareBool(string name) 
+    {
+        return AddVariable(name, VariableType.Bool);
+    }
+    
+    /// <summary>
+    /// Creates a variable of type INT in the ThreadSpec.
+    /// </summary>
+    /// <param name="name">
+    /// It is the name of the variable.
+    /// </param>
+    /// <returns>The value of <paramref name="WfRunVariable" /> </returns>
+    public WfRunVariable DeclareInt(string name) 
+    {
+        return AddVariable(name, VariableType.Int);
+    }
+    
+    /// <summary>
+    /// Creates a variable of type STR in the ThreadSpec.
+    /// </summary>
+    /// <param name="name">
+    /// It is the name of the variable.
+    /// </param>
+    /// <returns>The value of <paramref name="WfRunVariable" /> </returns>
+    public WfRunVariable DeclareStr(string name) 
+    {
+        return AddVariable(name, VariableType.Str);
+    }
+    
+    /// <summary>
+    /// Creates a variable of type DOUBLE in the ThreadSpec.
+    /// </summary>
+    /// <param name="name">
+    /// It is the name of the variable.
+    /// </param>
+    /// <returns>The value of <paramref name="WfRunVariable" /> </returns>
+    public WfRunVariable DeclareDouble(string name) 
+    {
+        return AddVariable(name, VariableType.Double);
+    }
+    
+    /// <summary>
+    /// Creates a variable of type BYTES in the ThreadSpec.
+    /// </summary>
+    /// <param name="name">
+    /// It is the name of the variable.
+    /// </param>
+    /// <returns>The value of <paramref name="WfRunVariable" /> </returns>
+    public WfRunVariable DeclareBytes(string name) 
+    {
+        return AddVariable(name, VariableType.Bytes);
+    }
+    
+    /// <summary>
+    /// Creates a variable of type JSON_ARR in the ThreadSpec.
+    /// </summary>
+    /// <param name="name">
+    /// It is the name of the variable.
+    /// </param>
+    /// <returns>The value of <paramref name="WfRunVariable" /> </returns>
+    public WfRunVariable DeclareJsonArr(string name) 
+    {
+        return AddVariable(name, VariableType.JsonArr);
+    }
+    
+    /// <summary>
+    /// Creates a variable of type JSON_OBJ in the ThreadSpec.
+    /// </summary>
+    /// <param name="name">
+    /// It is the name of the variable.
+    /// </param>
+    /// <returns>The value of <paramref name="WfRunVariable" /> </returns>
+    public WfRunVariable DeclareJsonObj(string name) 
+    {
+        return AddVariable(name, VariableType.JsonObj);
     }
 }
