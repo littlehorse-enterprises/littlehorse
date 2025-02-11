9025b2df9083e7b7bebdadbe0ff04733e8b9746c
Snarr <jacobsnarr@gmail.com>
2025-02-11 12:22:42 -0500

    test: Add tests for LHLibUtil json serialization

--- sdk-java/src/test/java/io/littlehorse/sdk/common/LHLibUtilTest.java
+++ sdk-java/src/test/java/io/littlehorse/sdk/common/LHLibUtilTest.java
@@ -1,7 +1,12 @@
 package io.littlehorse.sdk.common;
 
 import io.littlehorse.sdk.common.proto.TaskRunId;
+import io.littlehorse.sdk.common.proto.VariableValue;
 import io.littlehorse.sdk.common.proto.WfRunId;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -52,5 +57,57 @@ public class LHLibUtilTest {
                 .build();
 
         Assertions.assertThat(LHLibUtil.wfRunIdToString(id)).isEqualTo(parentId + "_" + childId);
+    }
+
+    @Test
+    void shouldConvertUUIDToString() {
+        UUID uuid = UUID.randomUUID();
+
+        VariableValue varVal = LHLibUtil.objToVarVal(uuid);
+
+        Assertions.assertThat(varVal.getStr()).isEqualTo(uuid.toString());
+    }
+
+    @Test
+    void validateMapToStringObjectConversion() {
+        UUID uuid = UUID.fromString("fc03087-d265-11e7-b8c6-83e29cd24f4c");
+
+        Map<String, Object> myMap = new HashMap<String, Object>();
+        myMap.put("key1", 5);
+        myMap.put("key2", "hello");
+        myMap.put("key3", 7.0);
+        myMap.put("key4", Map.of("key5", 2000.0));
+        myMap.put("key4", Map.of("key5", uuid));
+        
+
+        VariableValue varVal = LHLibUtil.objToVarVal(myMap);
+
+        Assertions.assertThat(varVal.getJsonObj()).isEqualTo("{\"key1\":5,\"key2\":\"hello\",\"key3\":7.0,\"key4\":{\"key5\":\"0fc03087-d265-11e7-b8c6-83e29cd24f4c\"}}");
+    }
+
+    @Test
+    void validateCollectionToStringArrayConversion() {
+
+    }
+
+    @Test
+    void validatePojoToStringObjectConversion() {
+        Book myBook = new Book("Neuromancer", 20, 5);
+        
+        VariableValue varVal = LHLibUtil.objToVarVal(myBook);
+
+        Assertions.assertThat(varVal.getJsonObj()).isEqualTo("{\"title\":\"Neuromancer\",\"soldUnits\":20,\"cost\":5.0}");
+    }
+
+    private class Book {
+        private String title;
+        private int soldUnits;
+        private double cost;
+    
+        public Book(String title, int soldUnits, double cost) {
+            this.title = title;
+            this.soldUnits = soldUnits;
+            this.cost = cost;
+        }
     }
 }
