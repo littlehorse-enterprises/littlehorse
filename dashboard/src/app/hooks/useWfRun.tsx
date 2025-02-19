07f411eeca117bc782ea9268c18aa62468d77eab
Bryson G. <bryson.glembin@gmail.com>
2025-02-19 08:36:12 -0800

    fix(dashboard): wfrun blank page

--- dashboard/src/app/hooks/useWfRun.tsx
+++ dashboard/src/app/hooks/useWfRun.tsx
@@ -1,19 +1,25 @@
 "use client";
 import useSWR from "swr";
-import { getWfRun } from "../actions/getWfRun";
+import { getWfRun, WfRunResponse } from "../actions/getWfRun";
+import { useState } from "react";
+import { WfRunId } from "littlehorse-client/proto";
 
 type Props = {
-    id: string
+    wfRunId: WfRunId
     tenantId: string
 }
 
-export function useWfRun({ id, tenantId }: Props) {
-    const { data, error, isLoading } = useSWR(`wfRun/${tenantId}/${id}`, () => {
-        return getWfRun({ ids: [id], tenantId })
+export function useWfRun({ wfRunId, tenantId }: Props) {
+    const [dataCache, setDataCache] = useState<WfRunResponse | null>(null)
+
+    const { error, isLoading } = useSWR(`wfRun/${tenantId}/${wfRunId.id}/${wfRunId.parentWfRunId?.id}`, async () => {
+        if (!wfRunId.id) return dataCache
+        const data = await getWfRun({ wfRunId, tenantId })
+        setDataCache(data)
     })
 
     return {
-        wfRunData: data,
+        wfRunData: dataCache,
         isLoading,
         isError: error
     }
