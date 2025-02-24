9c68b34ae052201023efab70950c685c0e3bd3f5
KarlaCarvajal <karlis.carvajalv@gmail.com>
2025-02-24 18:19:59 -0500

    feat(sdk-dotnet): add exception hablers in wf thread (#1325)

--- sdk-dotnet/LittleHorse.Sdk.Tests/Workflow/Spec/WorkflowThreadErrorsAndExceptionsTest.cs
+++ sdk-dotnet/LittleHorse.Sdk.Tests/Workflow/Spec/WorkflowThreadErrorsAndExceptionsTest.cs
@@ -44,51 +44,12 @@ public class WorkflowThreadErrorsAndExceptionsTest
         
         var compiledWfThread = workflowThread.Compile();
         
-        var expectedSpec = new ThreadSpec();
-        var entrypoint = new Node
-        {
-            Entrypoint = new EntrypointNode(),
-            OutgoingEdges =
+        var expectedSpec = GetExpectedThreadSpec(
+            new FailureHandlerDef
             {
-                new Edge { SinkNodeName = "1-fail-TASK" }
-            }
-        };
-
-        var failTask = new Node
-        {
-            Task = new TaskNode
-            {
-                TaskDefId = new TaskDefId { Name = "fail" }
-            },
-            OutgoingEdges = { new Edge { SinkNodeName = "2-my-task-TASK" } },
-            FailureHandlers =
-            {
-                new FailureHandlerDef
-                {
-                    HandlerSpecName = "exn-handler-1-fail-TASK-FAILURE_TYPE_ERROR",
-                    AnyFailureOfType = FailureHandlerDef.Types.LHFailureType.FailureTypeError
-                }
-            }
-        };
-        
-        var myTask = new Node
-        {
-            Task = new TaskNode
-            {
-                TaskDefId = new TaskDefId { Name = "my-task" }
-            },
-            OutgoingEdges = { new Edge { SinkNodeName = "3-exit-EXIT" } }
-        };
-
-        var exitNode = new Node
-        {
-            Exit = new ExitNode()
-        };
-        
-        expectedSpec.Nodes.Add("0-entrypoint-ENTRYPOINT", entrypoint);
-        expectedSpec.Nodes.Add("1-fail-TASK", failTask);
-        expectedSpec.Nodes.Add("2-my-task-TASK", myTask);
-        expectedSpec.Nodes.Add("3-exit-EXIT", exitNode);
+                HandlerSpecName = "exn-handler-1-fail-TASK-FAILURE_TYPE_ERROR",
+                AnyFailureOfType = FailureHandlerDef.Types.LHFailureType.FailureTypeError
+            });
 
         var expectedNumberOfNodes = numberOfEntrypointNodes + numberOfExitNodes + numberOfTasks;
         Assert.Equal(expectedNumberOfNodes, compiledWfThread.Nodes.Count);
@@ -121,6 +82,131 @@ public class WorkflowThreadErrorsAndExceptionsTest
         
         var compiledWfThread = workflowThread.Compile();
         
+        var expectedSpec = GetExpectedThreadSpec(
+            new FailureHandlerDef
+            {
+                HandlerSpecName = "exn-handler-1-fail-TASK-TIMEOUT",
+                SpecificFailure = "TIMEOUT"
+            });
+
+        var expectedNumberOfNodes = numberOfEntrypointNodes + numberOfExitNodes + numberOfTasks;
+        Assert.Equal(expectedNumberOfNodes, compiledWfThread.Nodes.Count);
+        Assert.Equal(expectedSpec, compiledWfThread);
+    }
+    
+    [Fact]
+    public void WfThread_WithExceptionName_ShouldCompileExceptionHandling()
+    {
+        var numberOfExitNodes = 1;
+        var numberOfEntrypointNodes = 1;
+        var numberOfTasks = 2;
+        var workflowName = "TestWorkflow";
+        var mockParentWorkflow = new Mock<Sdk.Workflow.Spec.Workflow>(workflowName, _action);
+
+        void EntryPointAction(WorkflowThread wf)
+        {
+            NodeOutput node = wf.Execute("fail");
+            wf.HandleException(
+                node,
+                "any-business-exception",
+                handler =>
+                {
+                    handler.Execute("my-task");
+                }
+            );
+            wf.Execute("my-task");
+        }
+        var workflowThread = new WorkflowThread(mockParentWorkflow.Object, EntryPointAction);
+        
+        var compiledWfThread = workflowThread.Compile();
+        
+        var expectedSpec = GetExpectedThreadSpec(
+            new FailureHandlerDef
+            {
+                HandlerSpecName = "exn-handler-1-fail-TASK-any-business-exception",
+                SpecificFailure = "any-business-exception"
+            });
+
+        var expectedNumberOfNodes = numberOfEntrypointNodes + numberOfExitNodes + numberOfTasks;
+        Assert.Equal(expectedNumberOfNodes, compiledWfThread.Nodes.Count);
+        Assert.Equal(expectedSpec, compiledWfThread);
+    } 
+    
+    [Fact]
+    public void WfThread_WithoutExceptionName_ShouldCompileExceptionHandling()
+    {
+        var numberOfExitNodes = 1;
+        var numberOfEntrypointNodes = 1;
+        var numberOfTasks = 2;
+        var workflowName = "TestWorkflow";
+        var mockParentWorkflow = new Mock<Sdk.Workflow.Spec.Workflow>(workflowName, _action);
+
+        void EntryPointAction(WorkflowThread wf)
+        {
+            NodeOutput node = wf.Execute("fail");
+            wf.HandleException(
+                node,
+                handler =>
+                {
+                    handler.Execute("my-task");
+                }
+            );
+            wf.Execute("my-task");
+        }
+        var workflowThread = new WorkflowThread(mockParentWorkflow.Object, EntryPointAction);
+        
+        var compiledWfThread = workflowThread.Compile();
+
+        var expectedSpec = GetExpectedThreadSpec(
+            new FailureHandlerDef
+            {
+                HandlerSpecName = "exn-handler-1-fail-TASK",
+                AnyFailureOfType = FailureHandlerDef.Types.LHFailureType.FailureTypeException
+            });
+
+        var expectedNumberOfNodes = numberOfEntrypointNodes + numberOfExitNodes + numberOfTasks;
+        Assert.Equal(expectedNumberOfNodes, compiledWfThread.Nodes.Count);
+        Assert.Equal(expectedSpec, compiledWfThread);
+    } 
+    
+    [Fact]
+    public void WfThread_WithBusinessException_ShouldCompileAnyFailureHandling()
+    {
+        var numberOfExitNodes = 1;
+        var numberOfEntrypointNodes = 1;
+        var numberOfTasks = 2;
+        var workflowName = "TestWorkflow";
+        var mockParentWorkflow = new Mock<Sdk.Workflow.Spec.Workflow>(workflowName, _action);
+
+        void EntryPointAction(WorkflowThread wf)
+        {
+            NodeOutput node = wf.Execute("fail");
+            wf.HandleAnyFailure(
+                node,
+                handler =>
+                {
+                    handler.Execute("my-task");
+                }
+            );
+            wf.Execute("my-task");
+        }
+        var workflowThread = new WorkflowThread(mockParentWorkflow.Object, EntryPointAction);
+        
+        var compiledWfThread = workflowThread.Compile();
+
+        var expectedSpec = GetExpectedThreadSpec(
+            new FailureHandlerDef
+            {
+                HandlerSpecName = "exn-handler-1-fail-TASK-any-failure",
+            });
+
+        var expectedNumberOfNodes = numberOfEntrypointNodes + numberOfExitNodes + numberOfTasks;
+        Assert.Equal(expectedNumberOfNodes, compiledWfThread.Nodes.Count);
+        Assert.Equal(expectedSpec, compiledWfThread);
+    }
+
+    private ThreadSpec GetExpectedThreadSpec(FailureHandlerDef failureHandlerDef)
+    {
         var expectedSpec = new ThreadSpec();
         var entrypoint = new Node
         {
@@ -140,11 +226,7 @@ public class WorkflowThreadErrorsAndExceptionsTest
             OutgoingEdges = { new Edge { SinkNodeName = "2-my-task-TASK" } },
             FailureHandlers =
             {
-                new FailureHandlerDef
-                {
-                    HandlerSpecName = "exn-handler-1-fail-TASK-TIMEOUT",
-                    SpecificFailure = "TIMEOUT"
-                }
+                failureHandlerDef
             }
         };
         
@@ -167,8 +249,6 @@ public class WorkflowThreadErrorsAndExceptionsTest
         expectedSpec.Nodes.Add("2-my-task-TASK", myTask);
         expectedSpec.Nodes.Add("3-exit-EXIT", exitNode);
 
-        var expectedNumberOfNodes = numberOfEntrypointNodes + numberOfExitNodes + numberOfTasks;
-        Assert.Equal(expectedNumberOfNodes, compiledWfThread.Nodes.Count);
-        Assert.Equal(expectedSpec, compiledWfThread);
+        return expectedSpec;
     }
 }
