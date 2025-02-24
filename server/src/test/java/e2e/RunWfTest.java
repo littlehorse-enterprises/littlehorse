ad1e77c5d274f43769eb95f403f31b4c2e3e4faf
Snarr <jacobsnarr@gmail.com>
2025-02-24 14:11:17 -0500

    spotless

--- server/src/test/java/e2e/RunWfTest.java
+++ server/src/test/java/e2e/RunWfTest.java
@@ -2,25 +2,24 @@ package e2e;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import io.grpc.Status.Code;
 import io.grpc.StatusRuntimeException;
 import io.littlehorse.sdk.common.proto.LittleHorseGrpc.LittleHorseBlockingStub;
 import io.littlehorse.sdk.common.proto.RunWfRequest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class RunWfTest {
     private LittleHorseBlockingStub client;
-  
+
     @Test
     public void runWfShouldFailWithInvalidId() {
         StatusRuntimeException caught = null;
         try {
             client.runWf(RunWfRequest.newBuilder()
-            .setId("my_workflow")
-            .setWfSpecName("basic-example")
-            .build());
+                    .setId("my_workflow")
+                    .setWfSpecName("basic-example")
+                    .build());
         } catch (StatusRuntimeException exn) {
             caught = exn;
         }
