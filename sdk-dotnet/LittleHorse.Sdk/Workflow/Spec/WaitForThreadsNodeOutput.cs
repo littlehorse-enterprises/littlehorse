3536c68cba2aee89879c4cf10aaa50e00b7ace63
KarlaCarvajal <karlis.carvajalv@gmail.com>
2025-03-04 00:35:03 -0500

    feat(sdk-dotnet): Refactor handler failures methods

--- sdk-dotnet/LittleHorse.Sdk/Workflow/Spec/WaitForThreadsNodeOutput.cs
+++ sdk-dotnet/LittleHorse.Sdk/Workflow/Spec/WaitForThreadsNodeOutput.cs
@@ -1,4 +1,5 @@
 using LittleHorse.Sdk.Common.Proto;
+using static LittleHorse.Sdk.Common.Proto.FailureHandlerDef.Types;
 
 namespace LittleHorse.Sdk.Workflow.Spec;
 
@@ -25,24 +26,31 @@ public class WaitForThreadsNodeOutput: NodeOutput
     /// <returns>This WaitForThreadsNodeOutput </returns>
     public WaitForThreadsNodeOutput HandleExceptionOnChild(string? exceptionName, Action<WorkflowThread> handler)
     {
-        string threadName = "exn-handler-" + NodeName + "-"
-                            + (exceptionName != null ? exceptionName : FailureHandlerDef.Types.LHFailureType.FailureTypeException);
+        string threadName = $"exn-handler-{NodeName}-" +
+                            $"{(exceptionName != null ? exceptionName : LHFailureType.FailureTypeException)}";
         threadName = Parent.Parent.AddSubThread(threadName, handler);
-        var handlerDef = new FailureHandlerDef
-        {
-            HandlerSpecName = threadName
-        };
-        if (exceptionName != null) {
-            handlerDef.SpecificFailure = exceptionName;
-        } else {
-            handlerDef.AnyFailureOfType = FailureHandlerDef.Types.LHFailureType.FailureTypeException;
-        }
+        var handlerDef = GetFailureHandlerDef(threadName, exceptionName, LHFailureType.FailureTypeException);
 
         Parent.AddFailureHandlerOnWaitForThreadsNode(this, handlerDef);
 
         return this;
     }
-    
+
+    private FailureHandlerDef GetFailureHandlerDef(string threadName, string? failureName, LHFailureType failureType)
+    {
+        var handlerDef = new FailureHandlerDef { HandlerSpecName = threadName };
+        if (failureName != null) 
+        {
+            handlerDef.SpecificFailure = failureName;
+        } 
+        else 
+        {
+            handlerDef.AnyFailureOfType = failureType;
+        }
+
+        return handlerDef;
+    }
+
     /// <summary>
     /// Specifies a Failure Handler to run in case any of the ThreadRun's that we are waiting for in this
     /// WaitForThreadsNode fails with a specific ERROR.
@@ -56,19 +64,9 @@ public class WaitForThreadsNodeOutput: NodeOutput
     /// <returns>This WaitForThreadsNodeOutput. </returns>
     public WaitForThreadsNodeOutput HandleErrorOnChild(LHErrorType? error, Action<WorkflowThread> handler)
     {
-        string threadName = "error-handler-" + NodeName + "-"
-                            + (error != null ? error : FailureHandlerDef.Types.LHFailureType.FailureTypeError);
+        string threadName = $"error-handler-{NodeName}-{(error != null ? error : LHFailureType.FailureTypeError)}";
         threadName = Parent.Parent.AddSubThread(threadName, handler);
-        var handlerDef = new FailureHandlerDef { HandlerSpecName = threadName };
-        if (error != null) 
-        {
-            handlerDef.SpecificFailure = error.ToString();
-        } 
-        else 
-        {
-            handlerDef.AnyFailureOfType = FailureHandlerDef.Types.LHFailureType.FailureTypeError;
-        }
-
+        var handlerDef = GetFailureHandlerDef(threadName, error?.ToString(), LHFailureType.FailureTypeError);
         Parent.AddFailureHandlerOnWaitForThreadsNode(this, handlerDef);
 
         return this;
