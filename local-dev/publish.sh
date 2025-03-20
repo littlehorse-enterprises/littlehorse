8578643fda35a4536500baf6cf513fc6188f55a5
Saúl Piña <sauljabin@gmail.com>
2025-03-20 16:24:25 -0500

    refactor: check tag

--- local-dev/publish.sh
+++ local-dev/publish.sh
@@ -10,10 +10,24 @@ cd "$WORK_DIR"
 publish_maven() {
     read -p "This script should only be used in special situations. Do you want to continue? [yes/no] " answer
 
-    if [ "yes" != "$answer" ]
-    then
-        echo "Terminated" 
+    if [ "yes" != "$answer" ]; then
+        echo "Terminated"
         exit 1
+    fi
+
+    if git describe --exact-match --tags >/dev/null 2>&1; then
+        TAG=$(git describe --exact-match --tags)
+        read -p "Tag '$TAG' was found. Do you want to continue? [yes/no] " answer
+
+        if [ "yes" != "$answer" ]; then
+            echo "Terminated"
+            exit 1
+        fi
+
+        sed -i "s/version=.*/version=${TAG}/g" gradle.properties
+    else
+      echo "No tags found"
+      exit 1
     fi
 
     read -p "GPG signing key location: " signingKey
@@ -22,7 +36,7 @@ publish_maven() {
     read -p "OSSRH username: " ossrhUsername
     read -s -p "OSSRH password: " ossrhPassword
     echo
-    
+
     export ORG_GRADLE_PROJECT_signingKey="$(<$signingKey)"
     export ORG_GRADLE_PROJECT_signingPassword="$signingPassword"
     export ORG_GRADLE_PROJECT_ossrhUsername="$ossrhUsername"
@@ -32,6 +46,6 @@ publish_maven() {
 }
 
 case $1 in
-  --maven) publish_maven;;
-  *) echo "Invalid option.";; 
+--maven) publish_maven ;;
+*) echo "Invalid option." ;;
 esac
