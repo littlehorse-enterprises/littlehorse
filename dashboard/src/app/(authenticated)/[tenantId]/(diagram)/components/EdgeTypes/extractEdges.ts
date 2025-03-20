cfd387bfc4c26b50b07435708b1d75da75a2653c
Bryson G. <bryson.glembin@gmail.com>
2025-03-19 19:23:58 -0700

    Update extractEdges.ts

--- dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/EdgeTypes/extractEdges.ts
+++ dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/EdgeTypes/extractEdges.ts
@@ -4,30 +4,17 @@ import { Edge as EdgeProto, ThreadSpec } from 'littlehorse-client/proto'
 import { Edge, MarkerType } from 'reactflow'
 
 export const extractEdges = (spec: ThreadSpec, threadName: string): Edge[] => {
-  const targetMap = new Map<string, number>()
-  const sourceMap = new Map<string, number>()
   return Object.entries(spec.nodes).flatMap(([source, node]) => {
     return node.outgoingEdges.map(edge => {
-      const sourceIndex = sourceMap.get(source) ?? 0
-      let targetIndex = targetMap.get(edge.sinkNodeName) ?? 0
-      const sourceTarget = sourceMap.get(edge.sinkNodeName) ?? 0
-
-      if (sourceTarget > 0 && targetIndex !== 0) targetIndex++
       const edgeId = `${source}-${edge.sinkNodeName}`
-      const id = sourceIndex === 0 && targetIndex === 0 ? edgeId : `${edgeId}-${sourceIndex}-${targetIndex}`
-      targetMap.set(edge.sinkNodeName, targetIndex + 1)
-      sourceMap.set(source, sourceIndex + 1)
-
       const label = extractEdgeLabel(edge)
       return {
-        id,
+        id: `${threadName}:${edgeId}`,
         source: `${threadName}:${source}`,
         type: 'default',
         target: `${threadName}:${edge.sinkNodeName}`,
         label,
         data: edge,
-        targetHandle: `target-${targetIndex}`,
-        sourceHandle: `source-${sourceIndex}`,
         markerEnd: {
           type: MarkerType.ArrowClosed,
         },
