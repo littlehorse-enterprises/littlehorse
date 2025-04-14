1bf44fb93e63db23c43677f0707e7a8eb530cdf8
Colti-Wan McNealy <colt@littlehorse.io>
2025-04-13 18:49:54 -0700

    fixes sdk-java tests

--- sdk-java/src/test/java/io/littlehorse/sdk/wfsdk/internal/WorkflowThreadImplTest.java
+++ sdk-java/src/test/java/io/littlehorse/sdk/wfsdk/internal/WorkflowThreadImplTest.java
@@ -132,7 +132,7 @@ public class WorkflowThreadImplTest {
 
         VariableDef objVar = varDefs.get(1).getVarDef();
         assertThat(objVar.getDefaultValue()).isNotEqualTo(null);
-        assertEquals(objVar.getTypeDef(), VariableType.JSON_OBJ);
+        assertEquals(objVar.getTypeDef().getType(), VariableType.JSON_OBJ);
     }
 
     @Test
