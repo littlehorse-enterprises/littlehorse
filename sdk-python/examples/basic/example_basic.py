5483ce1e427afb65ded24891cda1c233b16b0c1e
Snarr <jacobsnarr@gmail.com>
2025-04-25 17:16:19 -0400

    Restore basic example

--- sdk-python/examples/basic/example_basic.py
+++ sdk-python/examples/basic/example_basic.py
@@ -5,9 +5,9 @@ import random
 
 import littlehorse
 from littlehorse.config import LHConfig
-from littlehorse.model import VariableType, Comparator, VariableMutationType
+from littlehorse.model import VariableType
 from littlehorse.worker import LHTaskWorker, WorkerContext
-from littlehorse.workflow import WfRunVariable, WorkflowIfStatement, WorkflowThread, Workflow
+from littlehorse.workflow import WorkflowThread, Workflow
 
 logging.basicConfig(level=logging.INFO)
 
@@ -20,82 +20,29 @@ def get_config() -> LHConfig:
     return config
 
 
-def get_workflow1() -> Workflow:
+def get_workflow() -> Workflow:
     def my_entrypoint(wf: WorkflowThread) -> None:
-        amount = wf.declare_int("amount").required()
+        the_name = wf.add_variable("input-name", VariableType.STR)
+        wf.execute("greet", the_name)
 
-        def admin_case(wf: WorkflowThread) -> None:
-            wf.execute("admin-tasks")
-        def manager_case(wf: WorkflowThread) -> None:
-            wf.execute("manager-tasks")
-
-        wf.do_if(
-            wf.condition(amount, Comparator.EQUALS, "admin"),
-            admin_case
-        ).do_else_if(
-            wf.condition(my_role, Comparator.EQUALS, "manager"),
-            manager_case
-        ).do_else(
-            lambda wf: wf.execute("customer-tasks")
-        )
-
-    return Workflow("example-if-statement", my_entrypoint)
-
-def get_workflow2() -> Workflow:
-    def my_entrypoint(wf: WorkflowThread) -> None:
-        amount = wf.declare_int("amount").required()
-
-        wf.do_if(
-            wf.condition(amount, Comparator.GREATER_THAN, 20),
-            lambda wf: wf.execute("message", "You have more than $20")
-        ).do_else_if(
-            wf.condition(amount, Comparator.GREATER_THAN, 15),
-            lambda wf: wf.execute("message", "You have greater than $15 but no more than $20")
-        ).do_else(
-            lambda wf: wf.execute("message", "You have no more than $15")
-        )
+    return Workflow("example-basic", my_entrypoint)
 
 
-    return Workflow("example-if-statement-amount", my_entrypoint)
-
-async def adminTasks(ctx: WorkerContext) -> str:
-    msg = "I am doing admin tasks"
+async def greeting(name: str, ctx: WorkerContext) -> str:
+    msg = f"Hello {name}!. WfRun {ctx.wf_run_id.id}"
     print(msg)
     await asyncio.sleep(random.uniform(0.5, 1.5))
     return msg
 
-async def managerTasks(ctx: WorkerContext) -> str:
-    msg = "I am doing manager tasks"
-    print(msg)
-    await asyncio.sleep(random.uniform(0.5, 1.5))
-    return msg
-
-async def customerTasks(ctx: WorkerContext) -> str:
-    msg = "You are a customer, you may not perform these tasks"
-    print(msg)
-    await asyncio.sleep(random.uniform(0.5, 1.5))
-    return msg
-
-async def message(msg: str, ctx: WorkerContext) -> str:
-    print(msg)
-    await asyncio.sleep(random.uniform(0.5, 1.5))
-    return msg
 
 async def main() -> None:
     config = get_config()
-    wf = get_workflow2()
+    wf = get_workflow()
 
-    littlehorse.create_task_def(adminTasks, "admin-tasks", config)
-    littlehorse.create_task_def(managerTasks, "manager-tasks", config)
-    littlehorse.create_task_def(customerTasks, "customer-tasks", config)
-    littlehorse.create_task_def(message, "message", config)
-
+    littlehorse.create_task_def(greeting, "greet", config)
     littlehorse.create_workflow_spec(wf, config)
 
-    await littlehorse.start(LHTaskWorker(adminTasks, "admin-tasks", config),
-                            LHTaskWorker(managerTasks, "manager-tasks", config),
-                            LHTaskWorker(customerTasks, "customer-tasks", config),
-                            LHTaskWorker(message, "message", config))
+    await littlehorse.start(LHTaskWorker(greeting, "greet", config))
 
 
 if __name__ == "__main__":
