0f58d2dd8ad21ffc9602f97c27c301272da213e2
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2025-05-01 11:01:44 -0700

    Update Default.tsx

--- dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/EdgeTypes/Default.tsx
+++ dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/EdgeTypes/Default.tsx
@@ -1,22 +1,27 @@
-import React, { type FC } from 'react';
+import React, { type FC, useCallback } from 'react';
 import {
   getSmoothStepPath,
   EdgeLabelRenderer,
   BaseEdge,
   type EdgeProps,
-  type Edge,
+  Position,
 } from 'reactflow'
+import { CircleAlertIcon } from 'lucide-react'
+import { useModal } from '../../hooks/useModal'
+import { Edge as EdgeProto } from 'littlehorse-client/proto'
 
-const CustomEdge: FC<EdgeProps<Edge<{ label: string }>>> = ({
+const CustomEdge: FC<EdgeProps<EdgeProto>> = ({
   id,
   sourceX,
   sourceY,
   targetX,
   targetY,
-  sourcePosition,
-  targetPosition,
+  sourcePosition = Position.Bottom,
+  targetPosition = Position.Top,
   label,
   data,
+  style,
+  ...rest
 }) => {
   const [edgePath, labelX, labelY] = getSmoothStepPath({
     sourceX,
@@ -28,20 +33,35 @@ const CustomEdge: FC<EdgeProps<Edge<{ label: string }>>> = ({
     borderRadius: 20,
   });
 
+  const { setModal, setShowModal } = useModal()
+  const onClick = useCallback(() => {
+    if (!data) return
+    setModal({ type: 'edge', data })
+    setShowModal(true)
+  }, [data, setModal, setShowModal])
+
   return (
     <>
-      <BaseEdge id={id} path={edgePath} />
+      <BaseEdge id={id} path={edgePath} style={style} {...rest} />
       <EdgeLabelRenderer>
-        {label && (
-          <div
-            className="absolute bg-white px-2 py-1 rounded text-xs font-medium nodrag nopan"
-            style={{
-              transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
-            }}
-          >
-            {label}
+        <div
+          style={{
+            position: 'absolute',
+            transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+            pointerEvents: 'all',
+          }}
+        >
+          <div onClick={onClick} className="flex cursor-pointer flex-col items-center">
+            {(data?.variableMutations?.length ?? 0) > 0 && (
+              <CircleAlertIcon size={16} className={`fill-gray-200`} />
+            )}
+            {label && (
+              <div className="rounded-md bg-gray-200 px-2 text-center text-xs text-gray-600">
+                {label}
+              </div>
+            )}
           </div>
-        )}
+        </div>
       </EdgeLabelRenderer>
     </>
   );
