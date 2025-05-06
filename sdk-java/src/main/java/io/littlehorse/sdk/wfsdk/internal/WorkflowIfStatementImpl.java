93c25ffba0d80af458c138ed3aa257e5451e6397
Snarr <jacobsnarr@gmail.com>
2025-05-06 02:12:09 -0400

    Cover case where doElse is called twice

--- sdk-java/src/main/java/io/littlehorse/sdk/wfsdk/internal/WorkflowIfStatementImpl.java
+++ sdk-java/src/main/java/io/littlehorse/sdk/wfsdk/internal/WorkflowIfStatementImpl.java
@@ -13,11 +13,15 @@ public class WorkflowIfStatementImpl implements WorkflowIfStatement {
     private String firstNopNodeName;
     private String lastNopNodeName;
 
+    @Getter()
+    private boolean wasElseExecuted;
+
     public WorkflowIfStatementImpl(
             WorkflowThread parentWorkflowThread, String firstNopNodeName, String lastNopNodeName) {
         this.parentWorkflowThread = (WorkflowThreadImpl) parentWorkflowThread;
         this.firstNopNodeName = firstNopNodeName;
         this.lastNopNodeName = lastNopNodeName;
+        this.wasElseExecuted = false;
     }
 
     @Override
@@ -27,6 +31,11 @@ public class WorkflowIfStatementImpl implements WorkflowIfStatement {
 
     @Override
     public void doElse(IfElseBody body) {
+        if (this.wasElseExecuted) {
+            throw new IllegalStateException("doElse() can only be called once per WorkflowIfStatement.");
+        }
+
+        this.wasElseExecuted = true;
         this.parentWorkflowThread.doElseIf(this, null, body);
     }
 }
