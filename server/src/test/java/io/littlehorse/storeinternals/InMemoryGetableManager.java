515a9635d9cd3efef3b9f2f469d8fe8a740f6073
Eduwer Camacaro <eduwerc@gmail.com>
2025-05-12 08:44:48 -0500

    Add inmemory classes for getable manager implementations

---
+++ server/src/test/java/io/littlehorse/storeinternals/InMemoryGetableManager.java
@@ -0,0 +1,30 @@
+package io.littlehorse.storeinternals;
+
+import com.google.protobuf.Message;
+import io.littlehorse.common.model.AbstractGetable;
+import io.littlehorse.common.model.CoreGetable;
+import io.littlehorse.common.model.getable.CoreObjectId;
+import io.littlehorse.common.model.getable.ObjectIdModel;
+import io.littlehorse.server.streams.storeinternals.GetableManager;
+import io.littlehorse.server.streams.topology.core.ProcessorExecutionContext;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class InMemoryGetableManager extends GetableManager {
+
+    private final Map<ObjectIdModel<?, ?, ?>, AbstractGetable<?>> buffer = new ConcurrentHashMap<>();
+
+    public InMemoryGetableManager(ProcessorExecutionContext executionContext) {
+        super(null, null, null, null, executionContext);
+    }
+
+    @Override
+    public <U extends Message, T extends CoreGetable<U>> void put(T getable) throws IllegalStateException {
+        buffer.put(getable.getObjectId(), getable);
+    }
+
+    @Override
+    public <U extends Message, T extends CoreGetable<U>> T get(CoreObjectId<?, U, T> id) {
+        return (T) buffer.get(id);
+    }
+}
