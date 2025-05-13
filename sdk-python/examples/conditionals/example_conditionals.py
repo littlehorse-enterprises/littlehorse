d7afb075e2ed4618c666bb3df059c79ffe5d034d
KarlaCarvajal <karlis.carvajalv@gmail.com>
2025-05-13 12:29:26 -0500

    (sdk-python): refactor code to use object to call do_else

--- sdk-python/examples/conditionals/example_conditionals.py
+++ sdk-python/examples/conditionals/example_conditionals.py
@@ -52,8 +52,8 @@ class SaleService:
         wf.mutate(total_sales, VariableMutationType.ASSIGN, output)
 
         condition = wf.condition(total_sales, Comparator.GREATER_THAN_EQ, GOAL)
-        wf.do_if(condition, self.if_body
-                 ).do_else(self.else_body)
+        if_statement = wf.do_if(condition, self.if_body)
+        if_statement.do_else(self.else_body)
 
     def get_workflow(self) -> Workflow:
         return Workflow("example-conditionals", self.entrypoint)
