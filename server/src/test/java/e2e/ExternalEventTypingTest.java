8c45aa2dab9805f1569ebfc523bde69900580d21
Colt McNealy <colt@littlehorse.io>
2025-05-24 14:40:13 -0700

    linters

--- server/src/test/java/e2e/ExternalEventTypingTest.java
+++ server/src/test/java/e2e/ExternalEventTypingTest.java
@@ -2,28 +2,26 @@ package e2e;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
-
-import io.grpc.StatusRuntimeException;
 import io.grpc.Status.Code;
+import io.grpc.StatusRuntimeException;
 import io.littlehorse.sdk.common.LHLibUtil;
 import io.littlehorse.sdk.common.proto.ExternalEventDefId;
 import io.littlehorse.sdk.common.proto.LHStatus;
+import io.littlehorse.sdk.common.proto.LittleHorseGrpc.LittleHorseBlockingStub;
 import io.littlehorse.sdk.common.proto.PutExternalEventRequest;
 import io.littlehorse.sdk.common.proto.WfRunId;
-import io.littlehorse.sdk.common.proto.LittleHorseGrpc.LittleHorseBlockingStub;
-
-import org.junit.jupiter.api.Test;
 import io.littlehorse.sdk.wfsdk.ExternalEventNodeOutput;
 import io.littlehorse.sdk.wfsdk.WfRunVariable;
 import io.littlehorse.sdk.wfsdk.Workflow;
 import io.littlehorse.test.LHTest;
 import io.littlehorse.test.LHWorkflow;
 import io.littlehorse.test.WorkflowVerifier;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 @LHTest
 public class ExternalEventTypingTest {
-    
+
     @LHWorkflow("typed-external-event")
     public Workflow typedExternalEvent;
 
@@ -34,7 +32,8 @@ public class ExternalEventTypingTest {
     public Workflow getBasicExternalEventWorkflow() {
         return Workflow.newWorkflow("typed-external-event", thread -> {
             WfRunVariable evtOutput = thread.declareInt("evt-output");
-            ExternalEventNodeOutput payload = thread.waitForEvent("typed-as-int").registeredAs(Integer.class);
+            ExternalEventNodeOutput payload =
+                    thread.waitForEvent("typed-as-int").registeredAs(Integer.class);
             evtOutput.assign(payload);
         });
     }
@@ -44,12 +43,18 @@ public class ExternalEventTypingTest {
         WfRunId id = verifier.prepareRun(typedExternalEvent).start();
 
         assertThatThrownBy(() -> {
-            client.putExternalEvent(PutExternalEventRequest.newBuilder()
-                    .setExternalEventDefId(ExternalEventDefId.newBuilder().setName("typed-external-event"))
-                    .setWfRunId(id).setContent(LHLibUtil.objToVarVal("not-an-integer")).build());
-        }).matches(exn -> {
-            return exn instanceof StatusRuntimeException && ((StatusRuntimeException) exn).getStatus().getCode() == Code.INVALID_ARGUMENT && exn.getMessage().toLowerCase().contains("typed-external-event");
-        });
+                    client.putExternalEvent(PutExternalEventRequest.newBuilder()
+                            .setExternalEventDefId(
+                                    ExternalEventDefId.newBuilder().setName("typed-external-event"))
+                            .setWfRunId(id)
+                            .setContent(LHLibUtil.objToVarVal("not-an-integer"))
+                            .build());
+                })
+                .matches(exn -> {
+                    return exn instanceof StatusRuntimeException
+                            && ((StatusRuntimeException) exn).getStatus().getCode() == Code.INVALID_ARGUMENT
+                            && exn.getMessage().toLowerCase().contains("typed-external-event");
+                });
     }
 
     @Test
