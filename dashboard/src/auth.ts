2fc901208bf9c95e9f5476386261f5aef7b2e824
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2025-05-30 16:42:37 -0700

    update auth

--- dashboard/src/auth.ts
+++ dashboard/src/auth.ts
@@ -1,16 +1,19 @@
 import NextAuth from "next-auth";
 import Keycloak from "next-auth/providers/keycloak";
 import "next-auth/jwt";
+import { validateAccessToken, isTokenExpired } from "@/utils/authUtil";
 
 declare module "next-auth" {
   interface Session {
     accessToken?: string;
+    expiresAt?: number;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     accessToken?: string;
+    expiresAt?: number;
   }
 }
 
@@ -18,7 +21,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
     signIn: "/api/signin",
   },
-  providers: [
+  providers: process.env.LHD_OAUTH_ENABLED === "false" || !process.env.LHD_OAUTH_ENABLED ? [] : [
     Keycloak({
       issuer: process.env.KEYCLOAK_ISSUER_URI,
       clientId: process.env.KEYCLOAK_CLIENT_ID,
@@ -29,15 +32,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     jwt({ token, account }) {
       if (account?.provider === "keycloak") {
         token.accessToken = account.access_token;
+        token.expiresAt = account.expires_at;
       }
       return token;
     },
     async session({ session, token }) {
       session.accessToken = token.accessToken;
+      session.expiresAt = token.expiresAt;
       return session;
     },
     async authorized({ auth }) {
-      return !!auth?.user;
+      if (process.env.LHD_OAUTH_ENABLED === "false" || !process.env.LHD_OAUTH_ENABLED) 
+        return true;
+      
+      const token = auth?.accessToken;
+
+      return !!(
+        token &&
+        (await validateAccessToken(token)) &&
+        !isTokenExpired(auth?.expiresAt)
+      );
     },
   },
 });
