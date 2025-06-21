765912991799da76ce64b47f522fb88708bb0aa9
Bryson G <114206517+bryson-g@users.noreply.github.com>
2025-06-20 19:12:15 -0700

    chore: update Jest configuration and add Node engine requirements in package files

--- dashboard/jest.config.js
+++ dashboard/jest.config.js
@@ -9,7 +9,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapping: {
+  moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   testMatch: [
