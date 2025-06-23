86da1f6014937fc0c63e94b83568d62f5d0d37ec
Snarr <jacobsnarr@gmail.com>
2025-06-23 14:05:32 -0400

    add `struct-def` java example

---
+++ examples/struct-def/src/main/java/io/littlehorse/examples/StructDef.java
@@ -0,0 +1,73 @@
+package io.littlehorse.examples;
+
+import io.littlehorse.sdk.common.config.LHConfig;
+import io.littlehorse.sdk.common.proto.VariableType;
+import io.littlehorse.sdk.wfsdk.WfRunVariable;
+import io.littlehorse.sdk.wfsdk.Workflow;
+import io.littlehorse.sdk.wfsdk.internal.WorkflowImpl;
+import io.littlehorse.sdk.worker.LHTaskWorker;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Properties;
+/*
+ * This is a simple example, which does two things:
+ * 1. Declare an "input-name" variable of type String
+ * 2. Pass that variable into the execution of the "greet" task.
+ */
+public class StructDef {
+
+    public static Workflow getWorkflow() {
+        return new WorkflowImpl(
+            "example-basic",
+            wf -> {
+                WfRunVariable theName = wf.addVariable("input-name", VariableType.STR).searchable();
+                wf.execute("greet", theName);
+            }
+        );
+    }
+
+    public static Properties getConfigProps() throws IOException {
+        Properties props = new Properties();
+        File configPath = Path.of(
+            System.getProperty("user.home"),
+            ".config/littlehorse.config"
+        ).toFile();
+        if(configPath.exists()){
+            props.load(new FileInputStream(configPath));
+        }
+        return props;
+    }
+
+    public static LHTaskWorker getTaskWorker(LHConfig config) {
+        MyWorker executable = new MyWorker();
+        LHTaskWorker worker = new LHTaskWorker(executable, "greet", config);
+
+        // Gracefully shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread(worker::close));
+        return worker;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Let's prepare the configurations
+        Properties props = getConfigProps();
+        LHConfig config = new LHConfig(props);
+
+        // New workflow
+        Workflow workflow = getWorkflow();
+
+        // New worker
+        LHTaskWorker worker = getTaskWorker(config);
+
+        // Register task
+        worker.registerTaskDef();
+
+        // Register a workflow
+        workflow.registerWfSpec(config.getBlockingStub());
+
+        // Run the worker
+        worker.start();
+    }
+}
