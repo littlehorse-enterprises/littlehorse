10f3e90e71dcfa4286a6583bfef5f742ba54501d
Hazim O. Arafa <hazimoarafa@gmail.com>
2025-06-23 20:04:01 +0300

    feat(dashboard): show the current version (#1654)

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_VERSION: "v0.0.0-dev",
+  }
 }
 
 export default nextConfig
