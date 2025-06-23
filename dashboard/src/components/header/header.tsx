6f09cd1a294cde5e745c302f3b2e3f4b600dc6ad
Hazim Arafa <hazimoarafa@gmail.com>
2025-06-23 19:49:53 +0300

    Update header.tsx

--- dashboard/src/components/header/header.tsx
+++ dashboard/src/components/header/header.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { Badge } from '@littlehorse-enterprises/ui-library/badge'
-import { useParams } from 'next/navigation'
 import LinkWithTenant from '../link-with-tenant'
 import Logo from '../ui/logo'
 import TenantHeader from './TenantHeader'
@@ -10,19 +9,16 @@ export default function Header() {
   return (
     <header className="flex h-16 items-center justify-between border-b border-gray-200 px-4 bg-black">
       <div className="flex items-center space-x-8">
-        <LinkWithTenant href="/" className="text-white hover:text-[#3b81f5]">
+        <LinkWithTenant href="/" className="mr-2">
           <Logo />
         </LinkWithTenant>
 
-        <nav className="flex space-x-8">
-
-        </nav>
+        <Badge variant="secondary" className="text-xs opacity-60">
+          {process.env.NEXT_PUBLIC_VERSION}
+        </Badge>
       </div>
 
       <div className="flex items-center gap-4">
-        <Badge>
-          {process.env.NEXT_PUBLIC_VERSION}
-        </Badge>
         <TenantHeader />
       </div>
     </header>
