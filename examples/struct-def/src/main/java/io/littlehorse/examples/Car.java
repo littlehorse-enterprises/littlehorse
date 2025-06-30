13993db084c0404f2c1ae44c57bef4f27ae49ea5
Snarr <jacobsnarr@gmail.com>
2025-06-30 14:08:49 -0400

    feat(sdk-java): Add example for StructDefs

--- examples/struct-def/src/main/java/io/littlehorse/examples/Car.java
+++ examples/struct-def/src/main/java/io/littlehorse/examples/Car.java
@@ -1,13 +1,19 @@
 package io.littlehorse.examples;
 
 import io.littlehorse.sdk.worker.LHStructDef;
+import io.littlehorse.sdk.worker.LHStructField;
 
 @LHStructDef(name = "car")
 public class Car {
+    public Garage garage;
+    public Person owner;
     public String brand;
     public String model;
     public Integer price;
     public Integer year;
     public Boolean isElectric;
+
+    @LHStructField(masked = true)
     public String[] features;
+
 }
---
+++ examples/struct-def/src/main/java/io/littlehorse/examples/Person.java
@@ -0,0 +1,12 @@
+package io.littlehorse.examples;
+
+import io.littlehorse.sdk.worker.LHStructDef;
+
+@LHStructDef(name="person")
+public class Person {
+  public String firstName;
+  public String lastName;
+  public Integer accountId;
+  public String phoneNumber;
+  // public Car car;
+}
