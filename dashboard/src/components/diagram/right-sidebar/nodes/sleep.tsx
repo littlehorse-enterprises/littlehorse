d612cab57c74bf5d15e703b42556a5a72445eaf5
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2025-07-01 10:47:40 -0700

    Update sleep.tsx

--- dashboard/src/components/diagram/right-sidebar/nodes/sleep.tsx
+++ dashboard/src/components/diagram/right-sidebar/nodes/sleep.tsx
@@ -7,35 +7,31 @@ interface SleepNodeComponentProps {
 }
 
 export function SleepNodeComponent({ sleepNode }: SleepNodeComponentProps) {
-  const mainContent = (
-    <>
-      {sleepNode.sleep.sleepLength?.$case === 'rawSeconds' && (
-        <div className="flex justify-between">
-          <span className="text-[#656565]">Duration:</span>
-          <span className="font-mono">{getVariable(sleepNode.sleep.sleepLength.rawSeconds)} seconds</span>
-        </div>
-      )}
-      {sleepNode.sleep.sleepLength?.$case === 'timestamp' && (
-        <div className="flex justify-between">
-          <span className="text-[#656565]">Until Timestamp:</span>
-          <span className="font-mono">{getVariable(sleepNode.sleep.sleepLength.timestamp)}</span>
-        </div>
-      )}
-      {sleepNode.sleep.sleepLength?.$case === 'isoDate' && (
-        <div className="flex justify-between">
-          <span className="text-[#656565]">Until Date:</span>
-          <span className="font-mono">{getVariable(sleepNode.sleep.sleepLength.isoDate)}</span>
-        </div>
-      )}
-    </>
-  );
+  const nodeCase = sleepNode.sleep.sleepLength?.$case;
 
   return (
     <BaseNodeComponent
       title="Sleep Properties"
       type="SLEEP"
     >
-      {mainContent}
+      {nodeCase === 'rawSeconds' && sleepNode.sleep.sleepLength && (
+        <div className="flex justify-between">
+          <span className="text-[#656565]">Duration:</span>
+          <span className="font-mono">{getVariable(sleepNode.sleep.sleepLength.rawSeconds)} seconds</span>
+        </div>
+      )}
+      {nodeCase === 'timestamp' && sleepNode.sleep.sleepLength && (
+        <div className="flex justify-between">
+          <span className="text-[#656565]">Until Timestamp:</span>
+          <span className="font-mono">{getVariable(sleepNode.sleep.sleepLength.timestamp)}</span>
+        </div>
+      )}
+      {nodeCase === 'isoDate' && sleepNode.sleep.sleepLength && (
+        <div className="flex justify-between">
+          <span className="text-[#656565]">Until Date:</span>
+          <span className="font-mono">{getVariable(sleepNode.sleep.sleepLength.isoDate)}</span>
+        </div>
+      )}
     </BaseNodeComponent>
   )
 }
