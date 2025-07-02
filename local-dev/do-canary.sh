52abd6604787264ad138e167f9ccbcdab29abd9a
Mateo Rojas <mateo@littlehorse.io>
2025-07-02 18:43:23 -0500

    Fixes do-canary script to run dotnet worker too

--- local-dev/do-canary.sh
+++ local-dev/do-canary.sh
@@ -7,5 +7,25 @@ WORK_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
 
 cd "$WORK_DIR"
 
+# Checks if dotnet is installed
+command -v dotnet >/dev/null 2>&1 || { echo >&2 "dotnet version 8 is required but it's not installed.  Aborting."; exit 1; }
+
+# Run Core Canary (Aggregator, Metronome and Metronome Java Worker)
 ./gradlew canary:installDist
-./canary/core/build/install/canary/bin/canary canary/core/canary.properties
+./canary/core/build/install/canary/bin/canary canary/core/canary.properties &
+CORE_PID=$!
+
+# Run .NET Metronome Worker
+dotnet run --project canary/dotnet-worker/LittleHorse.Canary.Worker/LittleHorse.Canary.Worker.csproj &
+DOTNET_WORKER_PID=$!
+
+cleanup() {
+    kill "$CORE_PID"
+    kill "$DOTNET_WORKER_PID"
+    exit 0
+}
+
+# Stops both processes cleanly on ctrl+c or terminal closed
+trap cleanup INT
+
+wait
