7c43ca705f808dae12204aa5e26f3866c0b4cedd
Mateo Rojas <mateo@littlehorse.io>
2025-07-02 00:03:26 -0500

    feat(sdk-dotnet): adds native support for async Tasks in .NET (#1678)

--- sdk-dotnet/Examples/ConditionalsWhileExample/MyWorker.cs
+++ sdk-dotnet/Examples/ConditionalsWhileExample/MyWorker.cs
@@ -5,7 +5,7 @@ namespace ConditionalsWhileExample;
 public class MyWorker
 {
     [LHTaskMethod("eating-donut")]
-    public string EatingDonut(int donutsLeft)
+    public async Task<string> EatingDonut(int donutsLeft)
     {
         int left = donutsLeft - 1;
         string message = "eating donut, " + left + " left";
