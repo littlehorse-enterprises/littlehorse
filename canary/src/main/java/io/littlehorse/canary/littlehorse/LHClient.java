bf6dbe67a4639d4c0724079cdaf9cbb7282ce90f
Eduwer Camacaro <eduwerc@gmail.com>
2025-07-15 18:25:47 -0500

    Set deadline for canary lh client

--- canary/src/main/java/io/littlehorse/canary/littlehorse/LHClient.java
+++ canary/src/main/java/io/littlehorse/canary/littlehorse/LHClient.java
@@ -22,7 +22,7 @@ public class LHClient {
 
     public LHClient(
             final LHConfig lhConfig, final String workflowName, final int workflowVersion, final int workflowRevision) {
-        this.futureStub = lhConfig.getFutureStub().withDeadline(Deadline.after(1, TimeUnit.MINUTES));
+        this.futureStub = lhConfig.getFutureStub();
         this.blockingStub = lhConfig.getBlockingStub();
         this.workflowName = workflowName;
         this.workflowRevision = workflowRevision;
@@ -34,18 +34,24 @@ public class LHClient {
     }
 
     public ListenableFuture<WfRun> runCanaryWf(final String id, final Instant start, final boolean sampleIteration) {
-        return futureStub.runWf(RunWfRequest.newBuilder()
-                .setWfSpecName(workflowName)
-                .setId(id)
-                .setRevision(workflowRevision)
-                .setMajorVersion(workflowVersion)
-                .putVariables(
-                        START_TIME_VARIABLE,
-                        VariableValue.newBuilder().setInt(start.toEpochMilli()).build())
-                .putVariables(
-                        SAMPLE_ITERATION_VARIABLE,
-                        VariableValue.newBuilder().setBool(sampleIteration).build())
-                .build());
+        return futureStub
+                .withDeadline(Deadline.after(1, TimeUnit.MINUTES))
+                .runWf(RunWfRequest.newBuilder()
+                        .setWfSpecName(workflowName)
+                        .setId(id)
+                        .setRevision(workflowRevision)
+                        .setMajorVersion(workflowVersion)
+                        .putVariables(
+                                START_TIME_VARIABLE,
+                                VariableValue.newBuilder()
+                                        .setInt(start.toEpochMilli())
+                                        .build())
+                        .putVariables(
+                                SAMPLE_ITERATION_VARIABLE,
+                                VariableValue.newBuilder()
+                                        .setBool(sampleIteration)
+                                        .build())
+                        .build());
     }
 
     public WfRun getCanaryWfRun(final String id) {
