300a462a2a77b4b6c71ec37118d69324e46b2eef
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2025-07-22 14:41:45 -0700

    revert

--- dashboard/src/app/(authenticated)/[tenantId]/(diagram)/wfRun/[...ids]/components/WfRun.tsx
+++ dashboard/src/app/(authenticated)/[tenantId]/(diagram)/wfRun/[...ids]/components/WfRun.tsx
@@ -2,35 +2,19 @@
 import { Diagram } from '@/app/(authenticated)/[tenantId]/(diagram)/components/Diagram'
 import { Navigation } from '@/app/(authenticated)/[tenantId]/components/Navigation'
 import { useWfRun } from '@/app/hooks/useWfRun'
-import { WfRunId, WfRunVariableAccessLevel } from 'littlehorse-client/proto'
 import { useSearchParams } from 'next/navigation'
 import { FC } from 'react'
 import { Details } from './Details'
 import { Variables } from './Variables'
-
-export const WfRun: FC<{ ids: string[]; tenantId: string }> = ({ ids, tenantId }) => {
-  const wfRunId = ids.reduce((wfRunId, id, i) => (i === 0 ? { id } : { id, parentWfRunId: wfRunId }), {} as WfRunId)
-
+export const WfRun: FC<{ id: string, tenantId: string }> = ({ id, tenantId }) => {
   const searchParams = useSearchParams()
   const threadRunNumber = Number(searchParams.get('threadRunNumber'))
-  const { wfRunData } = useWfRun({ wfRunId, tenantId })
-  const { wfRunData: parentWfRunData } = useWfRun({
-    wfRunId: wfRunData?.wfRun?.id?.parentWfRunId ?? { id: '', parentWfRunId: undefined },
-    tenantId,
-  })
+  const { wfRunData, isLoading, isError } = useWfRun({ wfRunId: { id }, tenantId })
 
   if (!wfRunData) return null
-  const { wfRun, wfSpec, nodeRuns, variables } = wfRunData
+  const { wfRun, wfSpec, nodeRuns, variables } = wfRunData;
 
   if (!wfRun) return null
-
-  const variableDefs = wfSpec.threadSpecs[wfRun.threadRuns[threadRunNumber].threadSpecName].variableDefs
-  const inheritedVariables = variableDefs
-    .filter(vD => vD.accessLevel === WfRunVariableAccessLevel.INHERITED_VAR)
-    .map(vD => {
-      if (!wfRun.id?.parentWfRunId || !parentWfRunData) return null
-      return parentWfRunData.variables.find(v => v.id?.name === vD.varDef?.name)!
-    })
 
   return (
     <div className="mb-16">
@@ -42,9 +26,8 @@ export const WfRun: FC<{ ids: string[]; tenantId: string }> = ({ ids, tenantId }
       <Diagram spec={wfSpec} wfRun={wfRun} nodeRuns={nodeRuns} />
 
       <Variables
-        variableDefs={variableDefs}
+        variableDefs={wfSpec.threadSpecs[wfRun.threadRuns[threadRunNumber].threadSpecName].variableDefs}
         variables={variables.filter(v => v.id?.threadRunNumber == Number(searchParams.get('threadRunNumber')))}
-        inheritedVariables={inheritedVariables}
       />
     </div>
   )
