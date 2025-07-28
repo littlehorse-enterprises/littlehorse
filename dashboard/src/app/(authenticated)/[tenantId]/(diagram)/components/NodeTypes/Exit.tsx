217711a29994320fd0fe49390c47522cc57b00b8
Mijaíl Rondón <mijail@riseup.net>
2025-07-28 11:28:22 -0500

    revert(dashboard): child threads diagraming (#1422)
    This reverts commit c8f70639c9508bd37342ed8c45ec67bd84b99c18.

--- dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/NodeTypes/Exit.tsx
+++ dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/NodeTypes/Exit.tsx
@@ -27,8 +27,7 @@ const ExitNode: FC<NodeProps> = ({ data }) => {
       <div
         className={`flex h-6 w-6 cursor-pointer rounded-xl border-[3px] border-gray-500 ${failureDef ? 'bg-red-200' : 'bg-green-200'}`}
       >
-        <Handle type="target" position={Position.Left} className="bg-transparent" id="target-0" />
-        <Handle type="source" position={Position.Right} className="bg-transparent" id="source-0" />
+        <Handle type="target" position={Position.Left} className="bg-transparent" />
       </div>
     </Fade>
   )
