ea42acb42ecf9e84c127b7dd4532324c470ccb7b
Colt McNealy <colt@littlehorse.io>
2025-08-19 08:52:31 -0700

    adds go implemntation

--- sdk-go/littlehorse/wf_lib_internal.go
+++ sdk-go/littlehorse/wf_lib_internal.go
@@ -862,6 +862,26 @@ func (t *WorkflowThread) addVariable(
 	}
 }
 
+func (t *WorkflowThread) complete(result interface{}) {
+	t.checkIfIsActive()
+
+	_, node := t.createBlankNode("exit", "EXIT")
+
+	node.Node = &lhproto.Node_Exit{
+		Exit: &lhproto.ExitNode{},
+	}
+
+	if result != nil {
+		resultVarVal, err := t.assignVariable(result)
+		if err != nil {
+			t.throwError(tracerr.Wrap(err))
+		}
+		node.GetExit().Result = &lhproto.ExitNode_ReturnContent{
+			ReturnContent: resultVarVal,
+		}
+	}
+}
+
 func (t *WorkflowThread) condition(
 	lhs interface{}, op lhproto.Comparator, rhs interface{},
 ) *WorkflowCondition {
