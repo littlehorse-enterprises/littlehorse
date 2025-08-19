a9781f31d823279c4abd678e6bac1336181f4110
Christian CAicedo <christian@littlehorse.io>
2025-08-19 16:14:49 -0500

    feat(examples): consolidate  Go examples under examples/go

---
+++ examples/go/conditionals/ifelse/deploy/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/littlehorse-enterprises/littlehorse/sdk-go/littlehorse"
+
+	examples "github.com/littlehorse-enterprises/littlehorse/examples/go"
+	"github.com/littlehorse-enterprises/littlehorse/examples/go/conditionals/ifelse"
+)
+
+func main() {
+	_, client := examples.LoadConfigAndClient()
+	wf := littlehorse.NewWorkflow(ifelse.DonutWorkflow, ifelse.WorkflowName)
+	putWf, err := wf.Compile()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := (*client).PutWfSpec(context.Background(), putWf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	littlehorse.PrintProto(resp)
+}
