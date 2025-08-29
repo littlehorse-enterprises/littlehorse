8db509e596f6ee90d8cee93798640e9a641a268f
l4crito <34245597+l4crito@users.noreply.github.com>
2025-08-29 15:51:39 -0500

    feat(sdk-go): Register ExternalEventDef and WorkflowEventDef (#1731)
    Adds programmatic registration of external and workflow event definitions when registering a workflow spec in the Go SDK. Provides RegisteredAs methods on event node outputs and implements registerWfSpec to push definitions via the client

--- examples/go/externalevent/worker/main.go
+++ examples/go/externalevent/worker/main.go
@@ -12,32 +12,29 @@ import (
 func main() {
 	config, _ := examples.LoadConfigAndClient()
 
-	var askForNameWorker, greetWorker *littlehorse.LHTaskWorker
 	var err error
 
-	askForNameWorker, err = littlehorse.NewTaskWorker(config, externalevent.AskForName, externalevent.AskForNameTaskName)
+	greetWorker, err := littlehorse.NewTaskWorker(config, externalevent.SpecificGreeting, externalevent.SpecificGreetingTaskName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	summaryWorker, err := littlehorse.NewTaskWorker(config, externalevent.ShowSummary, externalevent.SummaryTaskName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	greetWorker, err = littlehorse.NewTaskWorker(config, externalevent.SpecificGreeting, externalevent.SpecificGreetingTaskName)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	askForNameWorker.RegisterTaskDef()
 	greetWorker.RegisterTaskDef()
+	summaryWorker.RegisterTaskDef()
 
 	defer func() {
 		log.Default().Print("Shutting down task workers")
-		askForNameWorker.Close()
 		greetWorker.Close()
+		summaryWorker.Close()
 	}()
 
-	// Start the workers.
 	log.Default().Print("Starting Task Workers")
 	go func() {
 		greetWorker.Start()
 	}()
-	askForNameWorker.Start()
+	summaryWorker.Start()
 }
