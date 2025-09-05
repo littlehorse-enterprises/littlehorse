7645c7a952149aaa5b1c4d19560598692f6c6946
Colt McNealy <colt@littlehorse.io>
2025-09-05 13:41:50 -0700

    chore(server): unify metrics configurations (#1816)
    - removes LHS_X_ROCKSDB_LOG_LEVEL
    - removes LHS_STREAMS_METRICS_LEVEL
    - relies on LHS_METRICS_LEVEL to configure all types of metrics. LHS_METRICS_LEVEL now translates exactly to kafka streams metrics level, and is translated one down to rocksdb.

--- sdk-java/src/main/java/io/littlehorse/sdk/common/config/ConfigBase.java
+++ sdk-java/src/main/java/io/littlehorse/sdk/common/config/ConfigBase.java
@@ -1,6 +1,7 @@
 package io.littlehorse.sdk.common.config;
 
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import lombok.extern.slf4j.Slf4j;
@@ -14,35 +15,41 @@ public abstract class ConfigBase {
         this.props = ConfigSource.newSource(getEnvKeyPrefixes())
                 .loadFromConfigSource(configSource)
                 .toProperties();
+        initialize();
     }
 
     public ConfigBase(String propLocation) {
         this.props = ConfigSource.newSource(getEnvKeyPrefixes())
                 .loadFromPropertiesFile(propLocation)
                 .toProperties();
+        initialize();
     }
 
     public ConfigBase(Path propLocation) {
         this.props = ConfigSource.newSource(getEnvKeyPrefixes())
                 .loadFromPropertiesFile(propLocation)
                 .toProperties();
+        initialize();
     }
 
     public ConfigBase(Properties props) {
         this.props = ConfigSource.newSource(getEnvKeyPrefixes())
                 .loadFromProperties(props)
                 .toProperties();
+        initialize();
     }
 
     public ConfigBase(Map<String, Object> configs) {
         this.props =
                 ConfigSource.newSource(getEnvKeyPrefixes()).loadFromMap(configs).toProperties();
+        initialize();
     }
 
     public ConfigBase() {
         this.props = ConfigSource.newSource(getEnvKeyPrefixes())
                 .loadFromEnvVariables()
                 .toProperties();
+        initialize();
     }
 
     protected abstract String[] getEnvKeyPrefixes();
@@ -56,6 +63,27 @@ public abstract class ConfigBase {
             return defaultVal;
         } else {
             return String.valueOf(props.get(key));
+        }
+    }
+
+    /**
+     * Override this to do setup in the constructor.
+     */
+    protected void doSetup() {}
+
+    /**
+     * Override this to allow warning for deprecated or removed configs.
+     */
+    protected List<String> deprecatedConfigs() {
+        return List.of();
+    }
+
+    private void initialize() {
+        doSetup();
+        for (String config : deprecatedConfigs()) {
+            if (props.containsKey(config)) {
+                log.warn("Detected deprecated or removed config {}.", config);
+            }
         }
     }
 }
