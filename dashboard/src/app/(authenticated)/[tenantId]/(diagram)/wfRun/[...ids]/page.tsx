901ef5ad92928b74e406c2cdd357d0009926d869
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2025-09-18 08:57:24 -0700

    Update page.tsx

--- dashboard/src/app/(authenticated)/[tenantId]/(diagram)/wfRun/[...ids]/page.tsx
+++ dashboard/src/app/(authenticated)/[tenantId]/(diagram)/wfRun/[...ids]/page.tsx
@@ -1,16 +1,9 @@
-import { lhClient } from '@/app/lhClient'
-import { WfRunId } from 'littlehorse-client/proto'
 import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 import { ClientError, Status } from 'nice-grpc-common'
 import { WfRun } from './components/WfRun'
 
 type Props = { params: { ids: string[]; tenantId: string } }
-
-async function getInheritedVariables(wfRunId: WfRunId, client: Awaited<ReturnType<typeof lhClient>>) {
-  const variables = []
-  client.getVariable({ wfRunId: wfRunId, threadRunNumber: 0 })
-}
 
 export default async function Page({ params: { ids, tenantId } }: Props) {
   try {
