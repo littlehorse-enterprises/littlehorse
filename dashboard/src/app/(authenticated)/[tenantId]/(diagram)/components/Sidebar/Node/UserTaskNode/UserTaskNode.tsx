28947ffc86f2b1269fc793c3cbd5026d8a151c5c
miguel guano <miguelguano@miguels-MacBook-Pro.local>
2025-10-14 15:38:54 -0500

    feat(diagram): fix spaces on user task nodes

--- dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/Sidebar/Node/UserTaskNode/UserTaskNode.tsx
+++ dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/Sidebar/Node/UserTaskNode/UserTaskNode.tsx
@@ -17,70 +17,70 @@ export const UserTaskNode: FC<{ node: UserTaskNodeProto }> = ({ node }) => {
   return (
     <div className="flex max-w-full flex-1 flex-col gap-2">
       <>
-        <div className=" gap-1">
+        <div>
           <small className="node-title">UserTask </small>
           <p>{userTaskDefName}</p>
         </div>
         {userGroup && (
-          <>
+          <div >
             <div className="flex items-center gap-1">
               <small className="node-title">User Group </small>
               {/* <InfoIcon size={16} color="gray" />   TODO: add information about the variable*/}
             </div>
             <VariableAssignment variableAssigment={userGroup} />
-          </>
+          </div>
         )}
 
         {userId && (
-          <>
+          <div>
             <div className="flex items-center gap-1">
               <small className="node-title">User Id </small>
               <InfoIcon size={16} color="gray" />
             </div>
             <VariableAssignment variableAssigment={userId} />
-          </>
+          </div>
         )}
         {userTaskDefVersion != undefined && (
-          <>
+          <div>
             <div className="flex items-center gap-1">
               <small className="node-title">User Task Definition Version</small>
               {/* <InfoIcon size={16} color="gray" /> */}
             </div>
             <p>{userTaskDefVersion}</p>
-          </>
+          </div>
         )}
         {notes && (
-          <>
+          <div>
             <div className="flex items-center gap-1">
               <small className="node-title">Notes </small>
               {/* <InfoIcon size={16} color="gray" /> */}
             </div>
             <VariableAssignment variableAssigment={notes} />
-          </>
+          </div>
         )}
         {onCancellationExceptionName && (
-          <>
+          <div>
             <div className="flex items-center gap-1">
               <small className="node-title">Cancel Exception </small>
               {/* <InfoIcon size={16} color="gray" /> */}
             </div>
             <VariableAssignment variableAssigment={onCancellationExceptionName} />
-          </>
+          </div>
         )}
-        <div className="pb-4">
+        <div className="pb-4 ">
           <small className="node-title"> Actions</small>
           <div>
             {actions.map((action, index) => {
               const nodeType = action.action?.$case ?? ''
               return (
-                <>
+                <div className="border-t-2 border-slate-100 ">
                   <Accordion type="single" collapsible>
                     <AccordionItem value={`action-${index}`}>
                       <AccordionTrigger>
                         <NodeTitleComponent title={nodeType} action={action.action?.value} />
                       </AccordionTrigger>
                       <AccordionContent>
-                        <div className="flex mt-2"  key={`hook-content-${index}`}>
+                        <div className="mt-2 flex" key={`hook-content-${index}`}>
                           <p className="flex-none truncate bg-blue-500 px-2 font-mono text-gray-200">Hook</p>
                           <p className=" flex-grow truncate bg-black pl-2 font-mono text-gray-200">{action.hook}</p>
                         </div>
@@ -94,7 +94,7 @@ export const UserTaskNode: FC<{ node: UserTaskNodeProto }> = ({ node }) => {
                       </AccordionContent>
                     </AccordionItem>
                   </Accordion>
-                </>
+                </div>
               )
             })}
           </div>
@@ -117,9 +117,9 @@ export const NodeTitleComponent: FC<{
   }
 
   return (
-    <div className="flex flex-1 items-center flex-start">
-      <span className='truncate pl-1'>{title}</span>
-      <span className="text-xs text-gray-500 ml-6">{summaryNodeTitle}</span>
+    <div className="flex-start flex flex-1 items-center">
+      <span className="truncate pl-1">{title}</span>
+      <span className="ml-6 text-xs text-gray-500">{summaryNodeTitle}</span>
     </div>
   )
 }
--- dashboard/src/components/ui/accordion.tsx
+++ dashboard/src/components/ui/accordion.tsx
@@ -24,7 +24,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        'flex flex-1 items-center justify-between py-2 font-medium transition-all hover:bg-gray-100 [&[data-state=open]>svg]:rotate-180',
+        'flex flex-1 items-center justify-between  font-medium transition-all hover:bg-gray-100 [&[data-state=open]>svg]:rotate-180',
         className
       )}
       {...props}
