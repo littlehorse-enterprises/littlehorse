f7bc02ce58db2768a291e7a68294d1d84d8d7534
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2025-10-20 08:11:32 -0700

    Update BasicExample.java

--- examples/java/basic/src/main/java/io/littlehorse/examples/BasicExample.java
+++ examples/java/basic/src/main/java/io/littlehorse/examples/BasicExample.java
@@ -1,21 +1,17 @@
 package io.littlehorse.examples;
 
+import io.littlehorse.sdk.common.config.LHConfig;
+import io.littlehorse.sdk.common.proto.VariableType;
+import io.littlehorse.sdk.wfsdk.WfRunVariable;
+import io.littlehorse.sdk.wfsdk.Workflow;
+import io.littlehorse.sdk.wfsdk.internal.WorkflowImpl;
+import io.littlehorse.sdk.worker.LHTaskWorker;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Properties;
-
-import io.littlehorse.sdk.common.config.LHConfig;
-import io.littlehorse.sdk.common.proto.InlineStructDef;
-import io.littlehorse.sdk.common.proto.LittleHorseGrpc.LittleHorseBlockingStub;
-import io.littlehorse.sdk.common.proto.PutStructDefRequest;
-import io.littlehorse.sdk.common.proto.StructDefCompatibilityType;
-import io.littlehorse.sdk.common.proto.StructFieldDef;
-import io.littlehorse.sdk.common.proto.TypeDefinition;
-import io.littlehorse.sdk.common.proto.VariableType;
-import io.littlehorse.sdk.common.proto.VariableValue;
-
 /*
  * This is a simple example, which does two things:
  * 1. Declare an "input-name" variable of type String
@@ -23,50 +19,55 @@ import io.littlehorse.sdk.common.proto.VariableValue;
  */
 public class BasicExample {
 
+    public static Workflow getWorkflow() {
+        return new WorkflowImpl(
+            "example-basic",
+            wf -> {
+                WfRunVariable theName = wf.addVariable("input-name", VariableType.STR).searchable();
+                wf.execute("greet", theName);
+            }
+        );
+    }
+
     public static Properties getConfigProps() throws IOException {
         Properties props = new Properties();
         File configPath = Path.of(
-                System.getProperty("user.home"),
-                ".config/littlehorse.config").toFile();
-        if (configPath.exists()) {
+            System.getProperty("user.home"),
+            ".config/littlehorse.config"
+        ).toFile();
+        if(configPath.exists()){
             props.load(new FileInputStream(configPath));
         }
         return props;
+    }
+
+    public static LHTaskWorker getTaskWorker(LHConfig config) {
+        MyWorker executable = new MyWorker();
+        LHTaskWorker worker = new LHTaskWorker(executable, "greet", config);
+
+        // Gracefully shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread(worker::close));
+        return worker;
     }
 
     public static void main(String[] args) throws IOException {
         // Let's prepare the configurations
         Properties props = getConfigProps();
         LHConfig config = new LHConfig(props);
-        LittleHorseBlockingStub client = config.getBlockingStub();
 
-        client.putStructDef(PutStructDefRequest.newBuilder()
-                .setName("car")
-                .setDescription("A car is a StructDef that blah blah")
-                .setAllowedUpdates(StructDefCompatibilityType.FULLY_COMPATIBLE_SCHEMA_UPDATES)
-                .setStructDef(InlineStructDef.newBuilder()
-                        .putFields("model", StructFieldDef.newBuilder()
-                                .setFieldType(TypeDefinition.newBuilder()
-                                        .setType(VariableType.STR))
-                                .build())
-                        .putFields("year", StructFieldDef.newBuilder()
-                                .setFieldType(TypeDefinition.newBuilder()
-                                        .setType(VariableType.INT))
-                                .build())
-                        .putFields("available", StructFieldDef.newBuilder()
-                                .setFieldType(TypeDefinition.newBuilder()
-                                        .setType(VariableType.BOOL))
-                                .build())
-                        .putFields("createdAt", StructFieldDef.newBuilder()
-                                .setFieldType(TypeDefinition.newBuilder()
-                                        .setType(VariableType.TIMESTAMP))
-                                .build())
-                        .putFields("ownerId", StructFieldDef.newBuilder()
-                                .setFieldType(TypeDefinition.newBuilder()
-                                        .setType(VariableType.INT))
-                                .setDefaultValue(VariableValue.newBuilder()
-                                        .setInt(0))
-                                .build()))
-                .build());
+        // New workflow
+        Workflow workflow = getWorkflow();
+
+        // New worker
+        LHTaskWorker worker = getTaskWorker(config);
+
+        // Register task
+        worker.registerTaskDef();
+
+        // Register a workflow
+        workflow.registerWfSpec(config.getBlockingStub());
+
+        // Run the worker
+        worker.start();
     }
 }
