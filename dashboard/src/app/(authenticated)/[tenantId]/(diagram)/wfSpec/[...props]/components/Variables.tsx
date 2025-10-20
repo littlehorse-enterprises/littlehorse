da22fa3d4b262f77317913d6dce752c48d868d8b
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2025-10-20 09:46:01 -0700

    refactor(dashboard): variable type display to TypeDisplay component

--- dashboard/src/app/(authenticated)/[tenantId]/(diagram)/wfSpec/[...props]/components/Variables.tsx
+++ dashboard/src/app/(authenticated)/[tenantId]/(diagram)/wfSpec/[...props]/components/Variables.tsx
@@ -1,8 +1,6 @@
-import LinkWithTenant from '@/app/(authenticated)/[tenantId]/components/LinkWithTenant'
-import { VARIABLE_TYPES } from '@/app/constants'
-import { getVariableDefType } from '@/app/utils/variables'
 import { ThreadSpec, WfRunVariableAccessLevel } from 'littlehorse-client/proto'
 import { FC } from 'react'
+import { TypeDisplay } from '../../../../components/TypeDisplay'
 
 type VariablesProps = Pick<ThreadSpec, 'variableDefs'>
 
@@ -21,18 +19,9 @@ export const Variables: FC<VariablesProps> = ({ variableDefs }) => {
       {variableDefs.map(variable => (
         <div key={variable.varDef?.name} className="mb-1 flex items-center gap-1">
           <span className="rounded	bg-gray-100 px-2 py-1 font-mono text-fuchsia-500">{variable.varDef?.name}</span>
-          <span className="rounded bg-yellow-100 p-1 text-xs">
-            {variable.varDef?.typeDef?.definedType?.$case === 'structDefId' ? (
-              <LinkWithTenant
-                className="flex underline"
-                href={`/structDef/${variable.varDef.typeDef.definedType.value.name}/${variable.varDef.typeDef.definedType.value.version}`}
-              >
-                Struct
-              </LinkWithTenant>
-            ) : (
-              VARIABLE_TYPES[getVariableDefType(variable.varDef!)]
-            )}
-          </span>
+          <TypeDisplay
+            definedType={variable.varDef?.typeDef?.definedType}
+          />
           {variable.required && <span className="rounded bg-orange-300 p-1 text-xs">Required</span>}
           {variable.searchable && <span className="rounded bg-blue-300 p-1 text-xs">Searchable</span>}
           <span className="rounded bg-green-300 p-1 text-xs">{accessLevels[variable.accessLevel]}</span>
--- dashboard/src/app/(authenticated)/[tenantId]/structDef/[name]/components/Fields.tsx
+++ dashboard/src/app/(authenticated)/[tenantId]/structDef/[name]/components/Fields.tsx
@@ -1,8 +1,7 @@
-import { VARIABLE_TYPES } from '@/app/constants'
-import { getVariableCaseFromType, getVariableValue } from '@/app/utils'
+import { getVariableValue } from '@/app/utils'
 import { InlineStructDef } from 'littlehorse-client/proto'
 import { FC } from 'react'
-import LinkWithTenant from '../../../components/LinkWithTenant'
+import { TypeDisplay } from '../../../components/TypeDisplay'
 
 type Props = {
   fields: InlineStructDef['fields']
@@ -18,23 +17,13 @@ export const Fields: FC<Props> = ({ fields }) => {
         const isRequired = !fieldDef.defaultValue
         const fieldType = fieldDef.fieldType.definedType
         if (!fieldType) return
-        console.log(fieldType)
 
         return (
           <div key={name} className="mb-1 flex items-center gap-1">
             <span className="rounded bg-gray-100 px-2 py-1 font-mono text-fuchsia-500">{name}</span>
-            <span className="rounded bg-yellow-100 p-1 text-xs">
-              {fieldType?.$case === 'structDefId' ? (
-                <LinkWithTenant
-                  className="flex underline"
-                  href={`/structDef/${fieldType.value.name}/${fieldType.value.version}`}
-                >
-                  Struct
-                </LinkWithTenant>
-              ) : (
-                VARIABLE_TYPES[getVariableCaseFromType(fieldType?.value)]
-              )}
-            </span>
+            <TypeDisplay
+              definedType={fieldType}
+            />
             {isRequired && <span className="rounded bg-orange-300 p-1 text-xs">Required</span>}
             {fieldDef.fieldType.masked && <span className="rounded bg-red-100 p-1 text-xs">Masked</span>}
             {fieldDef.defaultValue && (
