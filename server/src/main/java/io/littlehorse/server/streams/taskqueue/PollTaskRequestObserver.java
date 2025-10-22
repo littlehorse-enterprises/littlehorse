aee78b83e27cbf0ec91562320ff072d4d7d50d49
Colt McNealy <colt@littlehorse.io>
2025-10-21 21:18:30 -0700

    fix(server): properly handle case when task is already claimed

--- server/src/main/java/io/littlehorse/server/streams/taskqueue/PollTaskRequestObserver.java
+++ server/src/main/java/io/littlehorse/server/streams/taskqueue/PollTaskRequestObserver.java
@@ -116,7 +116,7 @@ public class PollTaskRequestObserver implements StreamObserver<PollTaskRequest> 
         if (toExecute == null) {
             // Tasks may be already claimed by other workers during rebalancing.
             // TODO: Add metrics to track the frequency of empty scheduled tasks to better monitor rebalancing impact
-            log.warn("Processing pollTaskRequest for task that was already claimed");
+            log.debug("Processing pollTaskRequest for task that was already claimed");
             PollTaskResponse emptyResponse = PollTaskResponse.newBuilder().build();
             responseObserver.onNext(emptyResponse);
         } else {
