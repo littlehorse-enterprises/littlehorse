f8d2be8ceb748e774400ff43bc52fca5cdb75f9d
miguel guano <miguelguano@miguels-MacBook-Pro.local>
2025-10-23 15:48:26 -0500

    feat(diagram): use tab to present sidebar information

--- dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/Sidebar/NodeInfo/FailureHandler.tsx
+++ dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/Sidebar/NodeInfo/FailureHandler.tsx
@@ -2,7 +2,7 @@ import { FailureHandlerDef } from 'littlehorse-client/proto'
 import { FC } from 'react'
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
 import { useDiagram } from '../../../hooks/useDiagram'
-import { ChevronRight } from 'lucide-react'
+import { ChevronRight, ScanIcon } from 'lucide-react'
 
 export const FailureHandler: FC<{ failureHandlers: FailureHandlerDef[] }> = ({ failureHandlers }) => {
   const { setThread } = useDiagram()
@@ -17,22 +17,22 @@ export const FailureHandler: FC<{ failureHandlers: FailureHandlerDef[] }> = ({ f
     })
   }
   return (
-    <div className='mt-2'>
+    <div className="mt-2">
       <Accordion type="single" collapsible>
         <AccordionItem value={'failure-handlers'}>
           <AccordionTrigger>
             <p className="text-[0.75em] text-slate-400">Failure Handlers</p>
           </AccordionTrigger>
-          <AccordionContent className='pb-0'>
+          <AccordionContent className="pb-0">
             {failureHandlers.map((handler, index) => (
               <div
-                className="flex  cursor-pointer font-mono text-blue-500  gap-2 "
+                className="flex  cursor-pointer gap-2 text-base font-medium "
                 key={index}
                 onClick={() => sendToFailureHandler(handler)}
               >
-                <div className="grow truncate">{handler.handlerSpecName}</div>
-                <div className="grow">
-                  <ChevronRight  size={16}/>
+                <div className="mt-1 grow truncate">{handler.handlerSpecName}</div>
+                <div className="mt-1 grow pt-1">
+                  <ScanIcon size={16} />
                 </div>
               </div>
             ))}
