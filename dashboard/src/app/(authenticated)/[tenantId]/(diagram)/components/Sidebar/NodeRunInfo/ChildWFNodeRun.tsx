55477a928a6bb1e5a37439775d28ddf8002a409a
¨miguel¨ <miguel@littlehorse.io>
2025-11-10 16:09:19 -0500

    feat(diagram): add node type to child work flow node

--- dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/Sidebar/NodeRunInfo/ChildWFNodeRun.tsx
+++ dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/Sidebar/NodeRunInfo/ChildWFNodeRun.tsx
@@ -5,6 +5,8 @@ import { LabelContent } from '../Components'
 export const ChildWFNodeRun: FC<{ node: RunChildWfNodeRunProto }> = ({ node }) => {
   return (
     <div>
+      <LabelContent label="Node Type" content="Child workflow node"></LabelContent>
+
       <LabelContent label="Work Flow Id" content={`${node.childWfRunId?.id}`}></LabelContent>
     </div>
   )
