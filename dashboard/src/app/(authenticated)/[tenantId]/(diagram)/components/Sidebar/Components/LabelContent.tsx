d385672ffd7496e175a9926976afa43e31071ee8
Miguel Guaño <miguel@littlehorse.io>
2025-11-11 10:41:18 -0500

    feat(dashboard): add child workflow node in spec and run wf (#1932)
    * feat(diagram): add child wf node spec and run
    
    * feat(diagram): add node type to child work flow node
    
    * feat(diagram): remove console log

--- dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/Sidebar/Components/LabelContent.tsx
+++ dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/Sidebar/Components/LabelContent.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 
 type Props = {
   label: string
@@ -9,7 +10,15 @@ export const LabelContent: FC<Props> = ({ label, content }) => {
   return (
     <div className="mb-2">
       <p className="text-[0.75em] text-slate-400">{label}</p>
-      {content && <p className="truncate text-lg font-medium">{content}</p>}
+
+      <TooltipProvider delayDuration={0}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            {content && <p className="truncate text-lg font-medium">{content}</p>}
+          </TooltipTrigger>
+          <TooltipContent>{content} </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </div>
   )
 }
