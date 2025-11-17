db886f1f2a817ac02d005065c906dc8ad9fc93c4
Snarr <jacobsnarr@gmail.com>
2025-11-17 12:08:16 -0500

    fix(server): use value field for LHStructDef annotation

--- server/src/test/java/e2e/Struct/UnknownStructDef.java
+++ server/src/test/java/e2e/Struct/UnknownStructDef.java
@@ -2,7 +2,7 @@ package e2e.Struct;
 
 import io.littlehorse.sdk.worker.LHStructDef;
 
-@LHStructDef(name = "unknown-struct-def")
+@LHStructDef("unknown-struct-def")
 public class UnknownStructDef {
     // This is a dummy StructDef used in shouldRejectPutWfSpecRequestUsingUnknownStructDef
 }
