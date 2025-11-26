1ebfd308e210a3423d4bd04236c1dadad4bb1d7f
¨miguel¨ <miguel@littlehorse.io>
2025-11-26 14:32:36 -0500

    feat(fix): format result modal

--- dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/Modals/OutputModal.tsx
+++ dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/Modals/OutputModal.tsx
@@ -1,18 +1,44 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Modal } from '../../context'
 import { useModal } from '../../hooks/useModal'
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { Check, Copy } from 'lucide-react'
 
 export const OutputModal: FC<Modal<{ message: string; label: string }>> = ({ data }) => {
   const { message, label } = data
   const { showModal, setShowModal } = useModal()
+  const [copied, setCopied] = useState(false)
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(message)
+      setCopied(true)
+      setTimeout(() => setCopied(false), 2000) // Reset after 2 seconds
+    } catch (err) {
+      console.error('Failed to copy text: ', err)
+    }
+  }
   return (
     <Dialog open={showModal} onOpenChange={open => setShowModal(open)}>
-      <DialogContent className=" max-w-ful">
+      <DialogContent className="min-w-[900px] ">
         <DialogHeader>
           <DialogTitle>{label}</DialogTitle>
         </DialogHeader>
-        <div className="mb-2 flex flex-col gap-2 overflow-y-auto border-b border-slate-200 pb-2">{message}</div>
+        <pre className="mb-2 overflow-auto rounded border-b border-slate-200 bg-slate-50 p-2 pb-2 font-mono">
+         <div className='absolute right-8'>
+           <button
+            onClick={handleCopy}
+            className={`ml-2 inline-flex h-6 w-6 items-center justify-center rounded transition-colors bg-gray-100  cursor-pointer hover:bg-gray-200`}
+          >
+            {copied ? (
+              <Check className="h-4 w-4 text-green-500" />
+            ) : (
+              <Copy className="h-4 w-4 text-gray-500 hover:text-gray-700" />
+            )}
+          </button>
+         </div>
+          <code>{message}</code>
+        </pre>
       </DialogContent>
     </Dialog>
   )
