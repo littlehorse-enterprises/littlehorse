d013a8817a713441a955c6a5ae7bf9025bcbda9f
Colt McNealy <colt@littlehorse.io>
2026-02-07 08:19:36 -0800

    adds javadoc

--- sdk-java/src/main/java/io/littlehorse/sdk/wfsdk/ExternalEventDefRegistration.java
+++ sdk-java/src/main/java/io/littlehorse/sdk/wfsdk/ExternalEventDefRegistration.java
@@ -2,8 +2,20 @@ package io.littlehorse.sdk.wfsdk;
 
 import io.littlehorse.sdk.common.proto.PutExternalEventDefRequest;
 
+/**
+ * Internal registration handle for ExternalEventDef creation.
+ *
+ * <p>Implementations provide the ExternalEventDef name and the corresponding
+ * {@link PutExternalEventDefRequest} used by {@link Workflow#getExternalEventDefsToRegister()}.
+ */
 public interface ExternalEventDefRegistration {
+    /**
+     * @return the ExternalEventDef name to register.
+     */
     String getExternalEventDefName();
 
+    /**
+     * @return the {@link PutExternalEventDefRequest} used to register this ExternalEventDef.
+     */
     PutExternalEventDefRequest toPutExtDefRequest();
 }
--- sdk-java/src/main/java/io/littlehorse/sdk/wfsdk/InterruptHandler.java
+++ sdk-java/src/main/java/io/littlehorse/sdk/wfsdk/InterruptHandler.java
@@ -2,5 +2,13 @@ package io.littlehorse.sdk.wfsdk;
 
 public interface InterruptHandler {
 
+    /**
+     * Registers an ExternalEventDef for this interrupt handler with the given payload type.
+     *
+     * <p>Allowed classes are String, Integer, Double, and Boolean. If {@code eventType} is
+     * {@code null}, then the ExternalEventDef will have a null payload.
+     *
+     * @param eventType the class of the interrupt event payload.
+     */
     void withEventType(Class<?> eventType);
 }
