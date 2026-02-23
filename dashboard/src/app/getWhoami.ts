f96f313ce310b417d64a380aeb6800d3bcef6ef4
Bryson G <114206517+bryson-g@users.noreply.github.com>
2026-02-23 13:40:42 -0800

    fix(dashboard): tenant selector

--- dashboard/src/app/getWhoami.ts
+++ dashboard/src/app/getWhoami.ts
@@ -1,5 +1,4 @@
 import lhConfig from '@/lhConfig'
-import { ACLAction, ACLResource, Principal, ServerACLs } from 'littlehorse-client/proto'
 import { getServerSession } from 'next-auth'
 import { WhoAmI } from '../types'
 import { authOptions } from './api/auth/[...nextauth]/authOptions'
@@ -8,29 +7,14 @@ const getWhoAmI = async (): Promise<WhoAmI> => {
   const session = await getServerSession(authOptions)
   const client = lhConfig.getClient(session?.accessToken)
 
-  const { id, perTenantAcls, globalAcls } = await client.whoami({})
-
-  const tenants = getTenants({ perTenantAcls, globalAcls })
+  const { id } = await client.whoami({})
+  const { results } = await client.searchTenant({})
+  const tenants = results.map(t => t.id).filter((id): id is string => id !== undefined)
 
   return {
     user: session?.user || { name: id?.id },
     tenants,
   }
-}
-
-const getTenants = ({ perTenantAcls, globalAcls }: Pick<Principal, 'globalAcls' | 'perTenantAcls'>): string[] => {
-  let tenants: string[] = []
-  if (globalAcls && hasDefaultAccess(globalAcls)) {
-    tenants = ['default']
-  }
-  return [...tenants, ...Object.keys(perTenantAcls)]
-}
-
-const hasDefaultAccess = ({ acls }: ServerACLs): boolean => {
-  const result = acls.filter(({ resources, allowedActions }) => {
-    return resources.includes(ACLResource.ACL_ALL_RESOURCES) && allowedActions.includes(ACLAction.ALL_ACTIONS)
-  })
-  return result.length > 0
 }
 
 export default getWhoAmI
