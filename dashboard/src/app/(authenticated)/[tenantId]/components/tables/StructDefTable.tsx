2ff8a20617fbb7625e5ca86a75d626f8e278f99a
Snarr <jacobsnarr@gmail.com>
2026-02-27 11:04:11 -0500

    feat(dashboard): add versioning to StructDef list and page

--- dashboard/src/app/(authenticated)/[tenantId]/components/tables/StructDefTable.tsx
+++ dashboard/src/app/(authenticated)/[tenantId]/components/tables/StructDefTable.tsx
@@ -2,6 +2,7 @@ import { StructDefId } from 'littlehorse-client/proto'
 import { FC } from 'react'
 import { SearchResultProps } from '.'
 import { SelectionLink } from '../SelectionLink'
+import VersionTag from '../VersionTag'
 
 export const StructDefTable: FC<SearchResultProps> = ({ pages = [] }) => {
   const allResults = pages.flatMap(page => page.results) as StructDefId[]
@@ -9,8 +10,9 @@ export const StructDefTable: FC<SearchResultProps> = ({ pages = [] }) => {
   return (
     <div className="py-4">
       {allResults.map(({ name, version }: StructDefId) => (
-        <SelectionLink key={name} href={`/structDef/${name}/${version}`}>
+        <SelectionLink key={`${name}.${version}`} href={`/structDef/${name}/${version}`}>
           <p className="group">{name}</p>
+          <VersionTag label={`v${version}`} />
         </SelectionLink>
       ))}
     </div>
