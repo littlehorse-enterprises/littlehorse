fed7a2fca070cb70738f28e3e3dc670cde72b91d
Snarr <jacobsnarr@gmail.com>
2026-03-05 12:56:53 -0500

    remove comment

--- server/src/main/java/io/littlehorse/server/LHServerListener.java
+++ server/src/main/java/io/littlehorse/server/LHServerListener.java
@@ -338,7 +338,6 @@ public class LHServerListener extends LittleHorseImplBase implements Closeable {
             ctx.onNext(td.toProto().build());
             ctx.onCompleted();
         }
-        // trigger workflows
     }
 
     @Override
