8efe64c14b2831f5d13208d0e4b3bf3b4ffa4070
Snarr <jacobsnarr@gmail.com>
2026-03-18 12:32:00 -0400

    fix(server): add accept nulls clause to typing method overload

---
+++ server/src/test/java/e2e/TaskReturnNullTest.java
@@ -0,0 +1,42 @@
+package e2e;
+
+import io.littlehorse.sdk.common.proto.LHStatus;
+import io.littlehorse.sdk.common.util.Arg;
+import io.littlehorse.sdk.wfsdk.WfRunVariable;
+import io.littlehorse.sdk.wfsdk.Workflow;
+import io.littlehorse.sdk.worker.LHTaskMethod;
+import io.littlehorse.test.LHTest;
+import io.littlehorse.test.LHWorkflow;
+import io.littlehorse.test.WorkflowVerifier;
+import org.junit.jupiter.api.Test;
+
+@LHTest
+public class TaskReturnNullTest {
+
+    private WorkflowVerifier verifier;
+
+    @LHWorkflow("task-null-return-wf")
+    private Workflow wf;
+
+    @LHWorkflow("task-null-return-wf")
+    public Workflow getWorkflow() {
+        return Workflow.newWorkflow("task-null-return-wf", wf -> {
+            WfRunVariable in = wf.declareStr("in").required();
+            wf.execute("maybe-return-int", in);
+            wf.complete();
+        });
+    }
+
+    @Test
+    public void taskReturningNullIsAccepted() {
+        verifier.prepareRun(getWorkflow(), Arg.of("in", "whatever"))
+                .waitForStatus(LHStatus.COMPLETED)
+                .start();
+    }
+
+    @LHTaskMethod("maybe-return-int")
+    public Integer maybeReturnInt(String input) {
+        // Simulate a lookup that finds nothing
+        return null;
+    }
+}
