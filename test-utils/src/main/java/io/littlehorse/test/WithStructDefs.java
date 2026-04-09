e07b7eef931f528bdb6b2d8d1c5b532b51a08eb3
Snarr <jacobsnarr@gmail.com>
2026-04-09 11:01:10 -0400

    docs(test-utils): add comment for WithStructDefs annotation

--- test-utils/src/main/java/io/littlehorse/test/WithStructDefs.java
+++ test-utils/src/main/java/io/littlehorse/test/WithStructDefs.java
@@ -5,6 +5,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to specify that a class annotated with
+ * {@link io.littlehorse.test.LHTest} should have the specified StructDefs
+ * registered on the LH server before tests are run.
+ *
+ * This will register StructDefs in the order they are specified in the
+ * annotation.
+ *
+ * This doesn't allow for StructDef evolution testing since the
+ * StructDefs are only registered once before all tests are run. For tests that
+ * require more fine-grained control over when StructDefs are registered, test
+ * authors can use the client API to register StructDefs directly within their
+ * test methods.
+ */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface WithStructDefs {
