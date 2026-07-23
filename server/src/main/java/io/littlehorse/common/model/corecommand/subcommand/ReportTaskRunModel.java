8ad6c8d36e2ece49c858e9ff3cbfd28585c16f5f
Eduwer Camacaro <eduwerc@gmail.com>
2026-07-23 11:46:41 -0500

    unused import

--- server/src/main/java/io/littlehorse/common/model/corecommand/subcommand/ReportTaskRunModel.java
+++ server/src/main/java/io/littlehorse/common/model/corecommand/subcommand/ReportTaskRunModel.java
@@ -21,11 +21,9 @@ import io.littlehorse.server.streams.topology.core.ExecutionContext;
 import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Setter
-@Slf4j
 public class ReportTaskRunModel extends CoreSubCommand<ReportTaskRun> {
 
     private TaskRunIdModel taskRunId;
