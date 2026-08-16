05836763786232833f1d1e494880a7272c0566a2
Colt McNealy <colt@littlehorse.io>
2026-08-16 10:13:42 -0700

    bolder workflow icon

--- dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/NodeTypes/RunChildWf/RunChildWf.tsx
+++ dashboard/src/app/(authenticated)/[tenantId]/(diagram)/components/NodeTypes/RunChildWf/RunChildWf.tsx
@@ -23,7 +23,7 @@ const Node: FC<NodeProps<'runChildWf', RunChildWfNode>> = ({ data }) => {
       <Fade fade={fade} status={nodeRun?.status}>
         <div className="flex w-40 cursor-pointer flex-col items-center rounded-md border-[1px] border-orange-500 bg-orange-200 px-2 pt-1 text-center text-xs">
           <Handle type="target" position={Position.Left} id="target-0" className="bg-transparent" />
-          <Workflow className="h-4 w-4 stroke-orange-500" strokeWidth={1.5} />
+          <Workflow className="h-4 w-4 stroke-orange-500" strokeWidth={2} />
           <span className="truncate">{wfSpecName}</span>
           <Handle type="source" position={Position.Right} id="source-0" className="bg-transparent" />
         </div>
