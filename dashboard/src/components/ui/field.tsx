7e2be13c4f3d83876df40f1f3d742037360b19f1
Snarr <jacobsnarr@gmail.com>
2026-08-21 15:58:51 -0500

    restore shadcn component

--- dashboard/src/components/ui/field.tsx
+++ dashboard/src/components/ui/field.tsx
@@ -11,7 +11,7 @@ function FieldGroup({ className, ...props }: React.ComponentProps<'div'>) {
     <div
       data-slot="field-group"
       className={cn(
-        'group/field-group @container/field-group flex w-full flex-col gap-2 data-[slot=checkbox-group]:gap-3 [&>[data-slot=field-group]]:gap-4',
+        'group/field-group @container/field-group flex w-full flex-col gap-7 data-[slot=checkbox-group]:gap-3 [&>[data-slot=field-group]]:gap-4',
         className
       )}
       {...props}
